34feab09d8dad4e8b5937864b61f3010515e393e
David Kircos <davidkircos@gmail.com>
2021-11-11 11:34:06 -0700

    Draw a 10x larger grid vertically

--- src/core/drawGrid.ts
+++ src/core/drawGrid.ts
@@ -16,13 +16,16 @@ const drawGrid = function (viewport: Viewport) {
   // Draw vertical lines
   for (var i = 0; i < GRID_SIZE; i++) {
     grid.moveTo(xoffset + i * CELL_WIDTH, yoffset);
-    grid.lineTo(xoffset + i * CELL_WIDTH, CELL_HEIGHT * GRID_SIZE);
+    grid.lineTo(
+      xoffset + i * CELL_WIDTH,
+      yoffset + CELL_HEIGHT * GRID_SIZE * 10
+    );
   }
 
-  // Draw vertical LINES
-  for (var j = 0; j < GRID_SIZE; j++) {
+  // Draw horizontal LINES
+  for (var j = 0; j < GRID_SIZE * 10; j++) {
     grid.moveTo(xoffset, yoffset + j * CELL_HEIGHT);
-    grid.lineTo(CELL_WIDTH * GRID_SIZE, yoffset + j * CELL_HEIGHT);
+    grid.lineTo(xoffset + CELL_WIDTH * GRID_SIZE, yoffset + j * CELL_HEIGHT);
   }
 
   viewport.addChild(grid);
