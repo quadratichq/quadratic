fb4d9552e4b7a6da8bdc2f4217a56a63f1670902
David Kircos <davidkircos@gmail.com>
2021-11-11 11:44:00 -0700

    Improve text quality

--- src/core/cells/fillCell.ts
+++ src/core/cells/fillCell.ts
@@ -8,22 +8,24 @@ import colors from "../../utils/colors.js";
 
 BitmapFont.from("CellFont", {
   fill: colors.cellFontColor,
-  fontSize: 18,
+  fontSize: 28,
 });
 
 const fillCell = (viewport: Viewport, cell: CellReference, text: string) => {
   // Calculate X and Y positions
   const x_pos = cell.x * CELL_WIDTH;
   const y_pos = cell.y * CELL_HEIGHT;
-  const margin = 2;
+  const margin_left = 2;
+  const margin_top = 2;
 
   // render text
   let bitmapText = new BitmapText(text, {
     fontName: "CellFont",
-    fontSize: 18,
+    fontSize: 28,
     align: "right",
   });
-  bitmapText.position.set(x_pos + margin, y_pos);
+  bitmapText.position.set(x_pos + margin_left, y_pos + margin_top);
+  bitmapText.scale.set(0.5);
 
   // highlight cell
   let cell_outline = new Graphics();
