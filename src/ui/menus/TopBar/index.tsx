5243b170172de1ae58ad07fb2e4724f29220533c
David Kircos <davidkircos@gmail.com>
2021-12-09 16:51:47 -0700

    Fix All TypeCheck Warnings

--- src/ui/menus/TopBar/index.tsx
+++ src/ui/menus/TopBar/index.tsx
@@ -1,15 +1,10 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuList from "@mui/material/MenuList";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
