54eb558d27f2dfa85f14b341f988122d0af8972c
David Kircos <davidkircos@gmail.com>
2022-02-09 22:31:50 -0700

    Remove unused code files

--- src/core/functions/DeleteCellsQF.ts
+++
@@ -1,7 +0,0 @@
-import CellReference from "../gridGL/types/cellReference";
-import { DeleteCellsDB } from "../gridDB/DeleteCellsDB";
-
-export const DeleteCellsQF = (cells: CellReference[]) => {
-  //DeleteCellSGM(cells);
-  DeleteCellsDB(cells);
-};
--- src/core/functions/EditCellQF.ts
+++
@@ -1,4 +0,0 @@
-import { Cell } from "../gridDB/db";
-
-// TODO should take single cell
-export const EditCellQF = (cells: Cell[]) => {};
