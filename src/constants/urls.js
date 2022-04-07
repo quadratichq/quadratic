9e5f6669cba61cd67239b37e4e5f5d52e37e8a12
David Kircos <davidkircos@gmail.com>
2022-04-07 11:41:08 -0600

    update documentation URL

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8000";
-export const DOCUMENTATION_URL = "https://docs.quadratic.to";
-export const BUG_REPORT_URL = "https://docs.quadratic.to";
+export const API_URL = 'http://localhost:8000';
+export const DOCUMENTATION_URL = 'https://docs.quadratic.to';
+export const BUG_REPORT_URL = 'https://github.com/quadratichq/quadratic/issues';
