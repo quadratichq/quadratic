bea083ef706de33387b7d6b97f222289e444bb94
David Kircos <davidkircos@gmail.com>
2022-04-07 11:40:58 -0600

    lint

--- public/electron.js
+++ public/electron.js
@@ -1,14 +1,14 @@
-const path = require("path");
+const path = require('path');
 
-const { app, BrowserWindow } = require("electron");
-const isDev = require("electron-is-dev");
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
 
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 600,
     height: 600,
-    titleBarStyle: "hidden",
+    titleBarStyle: 'hidden',
     trafficLightPosition: { x: 10, y: 10 },
     webPreferences: {
       nodeIntegration: true,
@@ -21,12 +21,12 @@ function createWindow() {
   // win.loadFile("index.html");
   win.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
   );
   // Open the DevTools.
   if (isDev) {
-    win.webContents.openDevTools({ mode: "detach" });
+    win.webContents.openDevTools({ mode: 'detach' });
   }
 }
 
@@ -38,13 +38,13 @@ app.whenReady().then(createWindow);
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
