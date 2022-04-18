8d71ed817b57141f7c87c788fa9fa4f2d8542852
David Kircos <davidkircos@gmail.com>
2022-04-18 17:41:46 -0600

    method that doesn't exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import * as React from "react";
+import * as React from 'react';
 
-import "./styles.css";
+import './styles.css';
 
-import { LoadingProvider } from "./contexts/LoadingContext";
+import { LoadingProvider } from './contexts/LoadingContext';
 
-import QuadraticApp from "./quadratic/QuadraticApp";
+import QuadraticApp from './quadratic/QuadraticApp';
 
 export default function App() {
   return (
     <LoadingProvider>
       {/* Provider of QuadraticApp */}
       <QuadraticApp></QuadraticApp>
+      <button onClick={methodDoesNotExist}>Break the world</button>;
     </LoadingProvider>
   );
 }
