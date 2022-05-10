8260eef122ffcb68f3a97c50561f29950ba7e5f6
David Kircos <davidkircos@gmail.com>
2022-05-10 16:00:31 -0600

    check if wasm is supported, otherwise show error

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import './styles.css';
 import { LoadingProvider } from './contexts/LoadingContext';
 
 import QuadraticApp from './quadratic/QuadraticApp';
+import { isWASMSupported } from './utils/isWASMSupported';
+import { BrowserNotSupported } from './ui/overlays/BrowserNotSupported';
 
 export default function App() {
+  // TODO: also check client WebGL support
+  if (!isWASMSupported) return <BrowserNotSupported></BrowserNotSupported>;
+
   return (
     <LoadingProvider>
       {/* Provider of QuadraticApp */}
