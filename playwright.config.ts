0c2a534c4b1327714b674bc7924ab9aa3abe973b
David Kircos <davidkircos@gmail.com>
2022-06-23 09:33:38 -0600

    increase test timeouts on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,20 +13,20 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: process.env.CI ? 60 * 1000 : 30 * 1000,
+  timeout: process.env.CI ? 100 * 1000 : 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: process.env.CI ? 30 * 1000 : 5 * 1000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -104,7 +104,7 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run start',
     port: 3000,
-    timeout: 120 * 1000,
+    timeout: 150 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 };
