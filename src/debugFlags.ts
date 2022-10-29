1ede86780d5b51c64c58e901d708da3f49bda21c
David Figatner <david@yopeyopey.com>
2022-10-29 07:16:33 -0700

    cleaned up debug flags

--- src/debugFlags.ts
+++ src/debugFlags.ts
@@ -1,7 +1,14 @@
-// set this to false to turn off all flags for release
-export const debug = true;
+// set this in .env (if set to false then all debug flags are turned off)
+export const debug = process.env.REACT_APP_DEBUG ?? false;
 
+// shows renderer light
 export const debugShowRenderer = true;
+
+// shows FPS meter
 export const debugShowFPS = true;
-export const debugShowTime = true;
+
+// shows rendering time for components
+export const debugShowTime = false;
+
+// counts number of children and which are visible in the viewport
 export const debugShowCountRenderedObjects = false;
