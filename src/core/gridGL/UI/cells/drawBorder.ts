95adac275985f671efc9d010f56467a7e41f2657
David Figatner <david@yopeyopey.com>
2022-11-13 03:52:03 -0800

    double border

--- src/core/gridGL/UI/cells/drawBorder.ts
+++ src/core/gridGL/UI/cells/drawBorder.ts
@@ -20,6 +20,7 @@ export function drawBorder(options: {
   const lineWidth = borderType === BorderType.line2 ? 2 : borderType === BorderType.line3 ? 3 : 1;
 
   const tiling = borderType === BorderType.dashed || borderType === BorderType.dotted;
+  const doubleDistance = borderType === BorderType.double ? lineWidth * 2 : 0;
 
   const setTexture = (sprite: Sprite | TilingSprite, horizontal: boolean): void => {
     if (borderType === BorderType.dashed) {
@@ -39,6 +40,16 @@ export function drawBorder(options: {
     top.width = options.width + lineWidth;
     top.height = lineWidth;
     top.position.set(options.x - lineWidth / 2, options.y - lineWidth / 2);
+
+    if (doubleDistance) {
+      const top = options.getSprite(tiling);
+      setTexture(top, true);
+      top.tint = options.tint;
+      top.alpha = options.alpha;
+      top.width = options.width + lineWidth - ((options.left ? 1 : 0) + (options.right ? 1 : 0)) * doubleDistance;
+      top.height = lineWidth;
+      top.position.set(options.x - lineWidth / 2 + (options.left ? doubleDistance : 0), options.y + doubleDistance - lineWidth / 2);
+    }
   }
 
   if (options.bottom) {
@@ -49,6 +60,16 @@ export function drawBorder(options: {
     bottom.width = options.width + lineWidth;
     bottom.height = lineWidth;
     bottom.position.set(options.x - lineWidth / 2, options.y + options.height - lineWidth / 2);
+
+    if (doubleDistance) {
+      const bottom = options.getSprite(tiling);
+      setTexture(bottom, true);
+      bottom.tint = options.tint;
+      bottom.alpha = options.alpha;
+      bottom.width = options.width + lineWidth - ((options.left ? 1 : 0) + (options.right ? 1 : 0)) * doubleDistance;
+      bottom.height = lineWidth;
+      bottom.position.set(options.x - lineWidth / 2 + (options.left ? doubleDistance : 0), options.y - doubleDistance + options.height - lineWidth / 2);
+    }
   }
 
   if (options.left) {
@@ -59,6 +80,16 @@ export function drawBorder(options: {
     left.width = lineWidth;
     left.height = options.height + lineWidth;
     left.position.set(options.x - lineWidth / 2, options.y - lineWidth / 2);
+
+    if (doubleDistance) {
+      const left = options.getSprite(tiling);
+      setTexture(left, false);
+      left.tint = options.tint;
+      left.alpha = options.alpha;
+      left.width = lineWidth;
+      left.height = options.height + lineWidth - ((options.top ? 1 : 0) + (options.bottom ? 1 : 0)) * doubleDistance;
+      left.position.set(options.x - lineWidth / 2 + doubleDistance, options.y - lineWidth / 2 + (options.top ? doubleDistance : 0));
+    }
   }
 
   if (options.right) {
@@ -69,5 +100,15 @@ export function drawBorder(options: {
     right.width = lineWidth;
     right.height = options.height + lineWidth;
     right.position.set(options.x + options.width - lineWidth / 2, options.y - lineWidth / 2);
+
+    if (doubleDistance) {
+      const right = options.getSprite(tiling);
+      setTexture(right, false);
+      right.tint = options.tint;
+      right.alpha = options.alpha;
+      right.width = lineWidth;
+      right.height = options.height + lineWidth - ((options.top ? 1 : 0) + (options.bottom ? 1 : 0)) * doubleDistance;
+      right.position.set(options.x + options.width - lineWidth / 2 - doubleDistance, options.y - lineWidth / 2 + (options.bottom ? doubleDistance : 0));
+    }
   }
 }
--- src/core/gridGL/dashedTextures.ts
+++ src/core/gridGL/dashedTextures.ts
@@ -3,16 +3,12 @@ import { SCALE_MODES, Texture, WRAP_MODES } from 'pixi.js';
 const THICKNESS = 2;
 const DASHED = 8;
 const DOTTED = 4;
-const DOUBLE_LINE = 2;
-const DOUBLE_LINE_THICKNESS = 4;
 
 interface DashedTexture {
   dottedVertical: Texture;
   dottedHorizontal: Texture;
   dashedVertical: Texture;
   dashedHorizontal: Texture;
-  doubleVertical: Texture;
-  doubleHorizontal: Texture;
 }
 
 // fallback to a white texture
@@ -21,8 +17,6 @@ export const dashedTextures: DashedTexture = {
   dottedHorizontal: Texture.WHITE,
   dashedVertical: Texture.WHITE,
   dashedHorizontal: Texture.WHITE,
-  doubleVertical: Texture.WHITE,
-  doubleHorizontal: Texture.WHITE,
 };
 
 function createDashedLine(horizontal: boolean): Texture {
@@ -71,34 +65,9 @@ function createDottedLine(horizontal: boolean): Texture {
   return Texture.from(canvas, { wrapMode: WRAP_MODES.REPEAT, scaleMode: SCALE_MODES.NEAREST });
 }
 
-function createDoubleLine(horizontal: boolean): Texture {
-  const canvas = document.createElement('canvas');
-  canvas.width = (horizontal ? DOUBLE_LINE : DOUBLE_LINE_THICKNESS);
-  canvas.height = (horizontal ? DOUBLE_LINE_THICKNESS : DOUBLE_LINE);
-  const context = canvas.getContext('2d');
-  if (!context) {
-    console.warn('Expected context to be defined in createBorderType');
-    return Texture.WHITE;
-  }
-  context.lineWidth = THICKNESS;
-  context.strokeStyle = 'white';
-
-  if (horizontal) {
-    context.moveTo(0, THICKNESS / 2);
-    context.lineTo(DOUBLE_LINE, THICKNESS / 2);
-  } else {
-    context.moveTo(THICKNESS / 2, 0);
-    context.lineTo(THICKNESS / 2, DOTTED / 2 );
-  }
-  context.stroke();
-  return Texture.from(canvas, { wrapMode: WRAP_MODES.REPEAT, scaleMode: SCALE_MODES.NEAREST });
-}
-
 export function createBorderTypes(): void {
   dashedTextures.dashedHorizontal = createDashedLine(true);
   dashedTextures.dashedVertical = createDashedLine(false);
   dashedTextures.dottedHorizontal = createDottedLine(true);
   dashedTextures.dottedVertical = createDottedLine(false);
-  dashedTextures.doubleHorizontal = createDoubleLine(true);
-  dashedTextures.doubleVertical = createDoubleLine(false);
 }
