21eeba243225af89205c94b50549c503247a5217
David Kircos <davidkircos@gmail.com>
2022-11-30 09:31:01 -0700

    add min height to FileNewCard

--- src/ui/fileBrowser/FileNewCard.tsx
+++ src/ui/fileBrowser/FileNewCard.tsx
@@ -3,7 +3,7 @@ import { AddCircleOutline } from '@mui/icons-material';
 
 export const FileNewCard = () => {
   return (
-    <Button variant="outlined" sx={{ width: '275px' }} startIcon={<AddCircleOutline />}>
+    <Button variant="outlined" sx={{ width: '275px', minHeight: '260px' }} startIcon={<AddCircleOutline />}>
       Create New File
     </Button>
   );
