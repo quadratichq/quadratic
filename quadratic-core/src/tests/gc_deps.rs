4362500dadb494d3fe60f3806e36dbb6830bf351
David Kircos <davidkircos@gmail.com>
2022-12-01 12:48:53 -0700

    split commands AddCellDependencies, RemoveCellDependencies

--- quadratic-core/src/tests/gc_deps.rs
+++ quadratic-core/src/tests/gc_deps.rs
@@ -6,19 +6,43 @@ fn test_gc_set_cell_deps() {
 
     // Manually create a transaction.
     grid.transact(|t| {
-        t.exec(Command::SetCellDependencies(
+        t.exec(Command::AddCellDependencies(
             Pos { x: 1, y: 3 },
             vec![Pos { x: 1, y: 2 }, Pos { x: 1, y: 1 }],
         ))?;
-        t.exec(Command::SetCellDependencies(
+
+        Ok(())
+    });
+
+    assert_eq!(
+        grid.get_dependent_cells(Pos { x: 1, y: 2 }),
+        vec![Pos { x: 1, y: 3 }]
+    );
+    assert_eq!(
+        grid.get_dependent_cells(Pos { x: 1, y: 1 }),
+        vec![Pos { x: 1, y: 3 }]
+    );
+
+    grid.undo();
+
+    assert_eq!(grid.get_dependent_cells(Pos { x: 1, y: 2 }), vec![]);
+    assert_eq!(grid.get_dependent_cells(Pos { x: 1, y: 1 }), vec![]);
+
+    // Manually create a transaction.
+    grid.transact(|t| {
+        t.exec(Command::AddCellDependencies(
+            Pos { x: 1, y: 3 },
+            vec![Pos { x: 1, y: 2 }, Pos { x: 1, y: 1 }],
+        ))?;
+        t.exec(Command::AddCellDependencies(
             Pos { x: 1, y: 4 },
             vec![Pos { x: 1, y: 3 }],
         ))?;
-        t.exec(Command::SetCellDependencies(
+        t.exec(Command::AddCellDependencies(
             Pos { x: 2, y: 3 },
             vec![Pos { x: 1, y: 2 }, Pos { x: 1, y: 1 }],
         ))?;
-        t.exec(Command::SetCellDependencies(
+        t.exec(Command::AddCellDependencies(
             Pos { x: 3, y: 3 },
             vec![Pos { x: 2, y: 3 }, Pos { x: 1, y: 4 }],
         ))?;
@@ -26,15 +50,19 @@ fn test_gc_set_cell_deps() {
         Ok(())
     });
 
+    assert_eq!(grid.get_dependent_cells(Pos { x: 3, y: 3 }), vec![]);
     assert_eq!(
-        grid.get_dependent_cells(Pos { x: 3, y: 3 }),
+        grid.get_dependent_cells(Pos { x: 1, y: 2 }),
         vec![
+            Pos { x: 1, y: 3 },
             Pos { x: 2, y: 3 },
             Pos { x: 1, y: 4 },
-            Pos { x: 1, y: 2 },
-            Pos { x: 1, y: 1 },
-            Pos { x: 1, y: 3 },
+            Pos { x: 3, y: 3 }
         ]
+    );
+    assert_eq!(
+        grid.get_dependent_cells(Pos { x: 1, y: 3 }),
+        vec![Pos { x: 1, y: 4 }, Pos { x: 3, y: 3 }]
     );
 
     assert!(grid.has_undo());
@@ -42,21 +70,25 @@ fn test_gc_set_cell_deps() {
 
     grid.undo();
 
-    assert_eq!(grid.get_dependent_cells(Pos { x: 3, y: 3 }), vec![]);
+    assert_eq!(grid.get_dependent_cells(Pos { x: 1, y: 2 }), vec![]);
 
     assert!(!grid.has_undo());
     assert!(grid.has_redo());
 
     grid.redo();
 
+    assert_eq!(grid.get_dependent_cells(Pos { x: 3, y: 3 }), vec![]);
     assert_eq!(
-        grid.get_dependent_cells(Pos { x: 3, y: 3 }),
+        grid.get_dependent_cells(Pos { x: 1, y: 2 }),
         vec![
+            Pos { x: 1, y: 3 },
             Pos { x: 2, y: 3 },
             Pos { x: 1, y: 4 },
-            Pos { x: 1, y: 2 },
-            Pos { x: 1, y: 1 },
-            Pos { x: 1, y: 3 },
+            Pos { x: 3, y: 3 }
         ]
+    );
+    assert_eq!(
+        grid.get_dependent_cells(Pos { x: 1, y: 3 }),
+        vec![Pos { x: 1, y: 4 }, Pos { x: 3, y: 3 }]
     );
 }
