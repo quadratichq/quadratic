1526de576fd1666150f8e4358371636a67466e3f
David Kircos <davidkircos@gmail.com>
2022-12-02 09:51:30 -0700

    add comment explaining why RemoveCellDependencies may not be necessary

--- quadratic-core/src/grid/command.rs
+++ quadratic-core/src/grid/command.rs
@@ -7,5 +7,8 @@ pub enum Command {
     /// Sets a single cell in the grid.
     SetCell(Pos, Cell),
     AddCellDependencies(Pos, Vec<Pos>),
+    // NOTE: We may be able to accomplish the same thing by changing AddCellDependencies to
+    // SetCellDependencies which would return the previous dependencies, delete them from the graph,
+    // and then set only the new ones.
     RemoveCellDependencies(Pos, Vec<Pos>),
 }
