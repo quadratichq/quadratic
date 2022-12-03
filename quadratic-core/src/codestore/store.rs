471af5d804ad4eeb7d28599696d3cbaa27b0d305
David Kircos <davidkircos@gmail.com>
2022-12-03 11:00:12 -0700

    CodeStore, Command::SetCellCode, tests

--- quadratic-core/src/codestore/store.rs
+++ quadratic-core/src/codestore/store.rs
@@ -1,11 +1,25 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt;
 
 use crate::Pos;
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum CodeType {
+    Formula,
+    Python,
+    SQL,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CodeCell {
+    pub code_type: CodeType,
+    pub code: String,
+}
+
 #[derive(Default, Clone, Debug)]
 pub struct CodeStore {
-    pub code: HashMap<Pos, String>,
+    pub code: HashMap<Pos, CodeCell>,
 }
 
 impl fmt::Display for CodeStore {
@@ -20,8 +34,16 @@ impl CodeStore {
         Self::default()
     }
 
-    pub fn set_code(&mut self, pos: Pos, code: String) -> Option<String> {
-        // sets code at pos and returns the old code or null if there was no code at pos
-        self.code.insert(pos, code)
+    pub fn set_cell_code(
+        &mut self,
+        pos: Pos,
+        code_cell_or_none: Option<CodeCell>,
+    ) -> Option<CodeCell> {
+        // sets code at pos and returns the old code or None if there was no code at pos
+        if code_cell_or_none.is_some() {
+            self.code.insert(pos, code_cell_or_none.unwrap())
+        } else {
+            self.code.remove(&pos)
+        }
     }
 }
