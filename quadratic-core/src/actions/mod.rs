1b070137d6730a8c912ca8960b89c24b3bd6b500
David Kircos <davidkircos@gmail.com>
2022-12-03 13:23:53 -0700

    add SetCodeValue action stub

--- quadratic-core/src/actions/mod.rs
+++ quadratic-core/src/actions/mod.rs
@@ -1,13 +1,14 @@
 use serde::{Deserialize, Serialize};
 
 use super::Pos;
+use crate::codestore::CodeCell;
 use crate::grid::{Cell, Command, GridController};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum UserActions {
     RunCell(Pos),
     SetCellValue(Pos, String),
-    // SetCodeValue(Pos, CodeCell),
+    SetCodeValue(Pos, Option<CodeCell>),
     // PasteCells(Pos),
     // CopyCells(Rect), // Does not add anything to the undo stack...
     // CutCells(Rect),
@@ -36,6 +37,8 @@ impl UserGridController {
             // Recomputes all dependent cells in a single transaction
             UserActions::RunCell(pos) => {
                 self.controller.transact(|t| {
+                    // TODO: Updating all dependent cells should be broken out into a separate function
+                    // this needs to be reused in other UserActions.
                     let dependent_cells = t.grid().get_graph().get_dependent_cells(pos);
                     for _cell in dependent_cells {
                         // recompute _cell
@@ -49,6 +52,23 @@ impl UserGridController {
             UserActions::SetCellValue(pos, value) => {
                 self.controller.transact(|t| {
                     t.exec(Command::SetCell(pos, Cell::Text(value)))?;
+
+                    let dependent_cells = t.grid().get_graph().get_dependent_cells(pos);
+                    for _cell in dependent_cells {
+                        // recompute _cell
+                        // call Command::SetCell(_cell, result)
+                    }
+
+                    Ok(())
+                });
+            }
+
+            // Sets Cell Value and recomputes all dependent cells in a single transaction
+            UserActions::SetCodeValue(pos, code_cell) => {
+                self.controller.transact(|t| {
+                    t.exec(Command::SetCellCode(pos, code_cell))?;
+                    // run cell
+                    t.exec(Command::SetCell(pos, Cell::Text("result".to_string())))?;
 
                     let dependent_cells = t.grid().get_graph().get_dependent_cells(pos);
                     for _cell in dependent_cells {
