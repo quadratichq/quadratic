3d43502e842fd8a0b22d4f09f47c930d7b6b5415
Jim Nielsen <jimniels@gmail.com>
2023-01-19 15:38:02 -0700

    fix: default param value

--- src/ui/menus/KeyboardShortcut.tsx
+++ src/ui/menus/KeyboardShortcut.tsx
@@ -4,14 +4,11 @@ export interface IKeyboardShortcut {
   modifier?: string;
 }
 
-export const KeyboardShortcut = ({ modifier, shortcut, text }: IKeyboardShortcut): JSX.Element => {
+export const KeyboardShortcut = ({ modifier = '', shortcut, text }: IKeyboardShortcut): JSX.Element => {
   return (
     <div style={{ display: 'flex', width: '175px' }}>
       <div style={{ width: '140px' }}>{text}</div>
-      <div style={{ fontSize: '14px', color: '#aaaaaa' }}>
-        {modifier ? modifier : ''}
-        {shortcut}
-      </div>
+      <div style={{ fontSize: '14px', color: '#aaaaaa' }}>{modifier + shortcut}</div>
     </div>
   );
 };
