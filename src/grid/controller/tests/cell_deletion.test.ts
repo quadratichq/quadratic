6bf591b8f238b5fb31f26d10df4a993f98bd7297
David Kircos <davidkircos@gmail.com>
2023-02-14 12:46:38 -0700

    add test delete cell and array cells together, undo, redo

--- src/grid/controller/tests/cell_deletion.test.ts
+++ src/grid/controller/tests/cell_deletion.test.ts
@@ -3,6 +3,7 @@ import { Cell } from '../../sheet/gridTypes';
 import { setupPython } from '../../computations/python/loadPython';
 import { updateCellAndDCells } from '../../actions/updateCellAndDCells';
 import { GetCellsDBSetSheet } from '../../sheet/Cells/GetCellsDB';
+import { DeleteCells } from '../../sheet/Cells/DeleteCells';
 
 // Setup Pyodide before tests
 let pyodide: any;
@@ -135,5 +136,67 @@ test('SheetController - cell bulk update when deleting a range of cells', async 
   expect(cells_after_delete_dependency.length).toBe(4);
   cells_after_delete_dependency.forEach((cell) => {
     expect(cell.value).toBe('');
+  });
+});
+
+test('SheetController - delete cell and array cells', async () => {
+  const sc = new SheetController();
+  GetCellsDBSetSheet(sc.sheet); // makes sheet available to Python
+
+  const cell = {
+    x: 0,
+    y: 0,
+    value: '',
+    type: 'PYTHON',
+    python_code: '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]',
+    last_modified: '2023-01-19T19:12:21.745Z',
+  } as Cell;
+
+  await updateCellAndDCells({ starting_cells: [cell], sheetController: sc, pyodide });
+
+  const code_cell = sc.sheet.grid.getNakedCells(0, 0, 0, 0);
+  expect(code_cell[0]?.value).toBe('1');
+  expect(code_cell[0]?.python_code).toBe('[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]');
+  expect(code_cell[0]?.evaluation_result?.std_out).toBe('');
+  expect(code_cell[0]?.last_modified).toBeDefined();
+  expect(code_cell[0]?.type).toBe('PYTHON');
+  expect(code_cell[0]?.array_cells).toBeDefined();
+
+  const after_code_run_cells = sc.sheet.grid.getNakedCells(0, 1, 0, 0);
+  after_code_run_cells.forEach((cell, index) => {
+    expect(cell.value).toEqual((cell.y + 1).toString());
+    expect(cell.type).toEqual('COMPUTED');
+  });
+
+  // delete cells
+  DeleteCells({
+    x0: 0,
+    y0: 0,
+    x1: 0,
+    y1: 10,
+    sheetController: sc,
+    pyodide,
+    create_transaction: true,
+  });
+
+  const after_delete = sc.sheet.grid.getNakedCells(0, 0, 0, 10);
+  expect(after_delete.length).toBe(0);
+
+  // undo
+
+  sc.undo();
+
+  const after_undo_code_cell = sc.sheet.grid.getNakedCells(0, 0, 0, 0);
+  expect(after_undo_code_cell[0]?.value).toBe('1');
+  expect(after_undo_code_cell[0]?.python_code).toBe('[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]');
+  expect(after_undo_code_cell[0]?.evaluation_result?.std_out).toBe('');
+  expect(after_undo_code_cell[0]?.last_modified).toBeDefined();
+  expect(after_undo_code_cell[0]?.type).toBe('PYTHON');
+  expect(after_undo_code_cell[0]?.array_cells).toBeDefined();
+
+  const after_undo_cells = sc.sheet.grid.getNakedCells(0, 1, 0, 0);
+  after_undo_cells.forEach((cell, index) => {
+    expect(cell.value).toEqual((cell.y + 1).toString());
+    expect(cell.type).toEqual('COMPUTED');
   });
 });
