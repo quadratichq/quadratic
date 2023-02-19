9a145e34e4849ef354320ad8f80b3d0e10fb72ae
David Kircos <davidkircos@gmail.com>
2023-02-19 09:04:46 -0700

    capture sentry event when browser isn't supported

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,18 @@ import { QuadraticAuth } from './quadratic/QuadraticAuth';
 import { isWASMSupported } from './utils/isWASMSupported';
 import { isWebGLSupported } from '@pixi/utils';
 import { BrowserNotSupported } from './ui/overlays/BrowserNotSupported';
+import * as Sentry from '@sentry/browser';
 
 export default function App() {
   // Check if browser supports WebGL and WASM
-  if (!isWASMSupported || !isWebGLSupported()) return <BrowserNotSupported></BrowserNotSupported>;
+  if (!isWASMSupported || !isWebGLSupported()) {
+    Sentry.captureEvent({
+      message: 'Browser does not support WebGL or WASM',
+      level: Sentry.Severity.Info,
+    });
+
+    return <BrowserNotSupported></BrowserNotSupported>;
+  }
 
   return (
     <LoadingProvider>
