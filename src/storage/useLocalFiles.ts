b1d67efed50e0549a006f76ed1613fd49c7810f9
David Figatner <david@yopeyopey.com>
2023-03-04 10:06:05 -0800

    file loads

--- src/storage/useLocalFiles.ts
+++ src/storage/useLocalFiles.ts
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import localforage from 'localforage';
-import { GridFileSchemaV1 } from './GridFileSchema';
+import { GridFileData, GridFileSchemaV1 } from './GridFileSchema';
 import { debugShowFileIO } from '../debugFlags';
 import { v4 as uuid } from 'uuid';
 import { getURLParameter } from '../helpers/getURL';
@@ -28,8 +28,8 @@ interface LocalFiles {
   loadSample: (sample: string) => void;
 }
 
-function log(s: string): void {
-  console.log(`[useLocalFiles] ${s}`)
+function log(...s: string[]): void {
+  console.log(`[useLocalFiles] ${s[0]}`, ...s.slice(1));
 }
 
 let firstLoad = false;
@@ -40,6 +40,13 @@ export const useLocalFiles = (sheetController: SheetController): LocalFiles => {
   const [index, setIndex] = useState<LocalFile[] | undefined>();
   const [lastLoaded, setLastLoaded] = useState<string | undefined>();
   const [lastFileContents, setLastFileContents] = useState<GridFileSchemaV1 | undefined>();
+
+  const afterLoad = useCallback((grid: GridFileSchemaV1) => {
+    sheetController.sheet.load_file(grid);
+    sheetController.clear();
+    sheetController.app?.rebuild();
+  }, [sheetController]);
+
 
   const load = useCallback(async (id: string): Promise<GridFileSchemaV1 | undefined> => {
     if (!index || !index.find(entry => entry.id === id)) {
@@ -57,9 +64,10 @@ export const useLocalFiles = (sheetController: SheetController): LocalFiles => {
     log(`loaded ${file.filename}`);
 
     setLastLoaded(id);
+    afterLoad(file);
 
     return file;
-  }, [index]);
+  }, [index, afterLoad]);
 
   const validateFile = useCallback((file: GridFileSchemaV1): boolean => {
     const expected = ['cells', 'formats', 'columns', 'rows', 'borders', 'cell_dependency', 'version', 'modified', 'created', 'id', 'filename'];
@@ -77,15 +85,13 @@ export const useLocalFiles = (sheetController: SheetController): LocalFiles => {
    * @param overwrite false = creates a new version of the imported file with a different id; true = overwrite; undefined = return 'overwrite'
    */
   const loadQuadraticFile = useCallback(async (url: string, overwrite?: boolean): Promise<boolean | 'overwrite'> => {
-    if (!index) return false;
-
     try {
       const file = await fetch(url);
       const gridFileJSON = (await file.json()) as GridFileSchemaV1;
       if (validateFile(gridFileJSON)) {
 
         // don't overwrite existing file unless user
-        if (overwrite === undefined && index.find(entry => entry.id === gridFileJSON.id)) {
+        if (overwrite === undefined && index && index.find(entry => entry.id === gridFileJSON.id)) {
           return 'overwrite';
         }
         const id = overwrite === false ? uuid() : gridFileJSON.id;
@@ -95,31 +101,40 @@ export const useLocalFiles = (sheetController: SheetController): LocalFiles => {
         setLastLoaded(id);
 
         // update index
-        const newIndex = [...index, { filename: gridFileJSON.filename, id, modified: Date.now() }];
+        const newIndex = [...(index ?? []), { filename: gridFileJSON.filename, id, modified: Date.now() }];
         setIndex(newIndex);
         await localforage.setItem(INDEX, newIndex);
+        log('setting index with 1 file');
 
         setLastLoaded(id);
+        afterLoad(gridFileJSON);
         return true;
       } else {
         log('attempted to import an invalid Quadratic file')
         return false;
       }
     } catch (e) {
-      log('error while fetching file');
+      log('error while fetching file', e as string);
       return false;
     }
-  }, [index, validateFile]);
+  }, [index, validateFile, afterLoad]);
 
   const loadSample = useCallback((sample: string): void => {
     loadQuadraticFile(`/examples/${sample}`);
   }, [loadQuadraticFile]);
 
   useEffect(() => {
-
     // ensure this only runs once
     if (firstLoad) return;
     firstLoad = true;
+
+    if (getURLParameter('clear') === '1') {
+      localforage.clear();
+      setIndex([]);
+      log('clear requested. Loading example file');
+      loadSample('default.grid');
+      return;
+    }
 
     localforage.config({ name: 'Quadratic', version: 1 });
     if (debugShowFileIO) log('initialized localForage');
@@ -148,6 +163,8 @@ export const useLocalFiles = (sheetController: SheetController): LocalFiles => {
         return;
       }
       if (!hasIndex) {
+        setIndex([]);
+
         log('loading example file');
         loadSample('default.grid');
       }
@@ -183,11 +200,21 @@ export const useLocalFiles = (sheetController: SheetController): LocalFiles => {
     if (!index) {
       throw new Error("Expected index to be defined in newFile");
     }
-    sheet.newFile();
-
     // create file
+    const grid: GridFileData = {
+      cells: [],
+      formats: [],
+      columns: [],
+      rows: [],
+      borders: [],
+      cell_dependency: '',
+
+      // todo: this goes away when alignment branch is merged
+      render_dependency: [],
+    };
+
     const created = Date.now();
-    const newFile: GridFileSchemaV1 = { ...sheet.export_file(), id: uuid(), created, modified: created, version: VERSION, filename: filename || 'Untitled' };
+    const newFile: GridFileSchemaV1 = { ...grid, id: uuid(), created, version: VERSION, modified: created, filename: filename || 'Untitled' };
     setLastFileContents(newFile);
     await localforage.setItem(newFile.id, newFile);
 
@@ -204,7 +231,7 @@ export const useLocalFiles = (sheetController: SheetController): LocalFiles => {
     setIndex(newIndex);
     await localforage.setItem(INDEX, newIndex);
 
-  }, [index, lastLoaded, sheet]);
+  }, [index, lastLoaded]);
 
   const saveQuadraticFile = useCallback((autoDownload: boolean): GridFileSchemaV1 | undefined => {
     if (!lastFileContents) return;
