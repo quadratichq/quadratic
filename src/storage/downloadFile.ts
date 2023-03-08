d4f6b481ebf557aa3e4f720182325d60129e1ff6
Jim Nielsen <jimniels@gmail.com>
2023-03-08 15:15:07 -0700

    fix: application/json + .grid

--- src/storage/downloadFile.ts
+++ src/storage/downloadFile.ts
@@ -1,5 +1,5 @@
 export function downloadFile(filename: string, data: string) {
-  const blob = new Blob([data], { type: 'text/csv' });
+  const blob = new Blob([data], { type: 'application/json' });
   //@ts-expect-error
   if (window.navigator.msSaveOrOpenBlob) {
     //@ts-expect-error
@@ -7,7 +7,7 @@ export function downloadFile(filename: string, data: string) {
   } else {
     const elem = window.document.createElement('a');
     elem.href = window.URL.createObjectURL(blob);
-    elem.download = filename;
+    elem.download = filename + '.grid';
     document.body.appendChild(elem);
     elem.click();
     document.body.removeChild(elem);
