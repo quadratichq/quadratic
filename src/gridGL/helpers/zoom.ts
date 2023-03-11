ee352c8f3a5af9367f419ccc85ac796059d98eda
David Figatner <david@yopeyopey.com>
2023-03-11 06:21:28 -0800

    add zoom to one cell selected

--- src/gridGL/helpers/zoom.ts
+++ src/gridGL/helpers/zoom.ts
@@ -1,4 +1,4 @@
-import * as PIXI from 'pixi.js';
+import { Rectangle, Point } from 'pixi.js';
 import { Viewport } from 'pixi-viewport';
 import { Sheet } from '../../grid/sheet/Sheet';
 import { ZOOM_ANIMATION_TIME_MS, ZOOM_BUFFER } from '../../constants/gridConstants';
@@ -22,7 +22,7 @@ export function zoomToFit(sheet: Sheet, viewport: Viewport): void {
 
     viewport.animate({
       time: ZOOM_ANIMATION_TIME_MS,
-      position: new PIXI.Point(
+      position: new Point(
         screenRectangle.x + screenRectangle.width / 2,
         screenRectangle.y + screenRectangle.height / 2
       ),
@@ -31,7 +31,7 @@ export function zoomToFit(sheet: Sheet, viewport: Viewport): void {
   } else {
     viewport.animate({
       time: ZOOM_ANIMATION_TIME_MS,
-      position: new PIXI.Point(0, 0),
+      position: new Point(0, 0),
       scale: 1,
     });
   }
@@ -57,29 +57,28 @@ export function zoomTo100(viewport: Viewport) {
 }
 
 export function zoomToSelection(interactionState: GridInteractionState, sheet: Sheet, viewport: Viewport): void {
+  let screenRectangle: Rectangle;
   if (interactionState.showMultiCursor) {
     const cursor = interactionState.multiCursorPosition;
-    const screenRectangle = sheet.gridOffsets.getScreenRectangle(
+    screenRectangle = sheet.gridOffsets.getScreenRectangle(
       cursor.originPosition.x,
       cursor.originPosition.y,
       cursor.terminalPosition.x - cursor.originPosition.x,
       cursor.terminalPosition.y - cursor.originPosition.y
     );
-
-    // calc scale, and leave a little room on the top and sides
-    let scale = viewport.findFit(screenRectangle.width * ZOOM_BUFFER, screenRectangle.height * ZOOM_BUFFER);
-
-    // Don't zoom in more than a factor of 2
-    if (scale > 2) scale = 2;
-
-    viewport.animate({
-      time: ZOOM_ANIMATION_TIME_MS,
-      position: new PIXI.Point(
-        screenRectangle.x + screenRectangle.width / 2,
-        screenRectangle.y + screenRectangle.height / 2
-      ),
-      scale,
-    });
   } else {
+    const cursor = interactionState.cursorPosition;
+    screenRectangle = sheet.gridOffsets.getScreenRectangle(cursor.x, cursor.y, 1, 1);
   }
+  // calc scale, and leave a little room on the top and sides
+  let scale = viewport.findFit(screenRectangle.width * ZOOM_BUFFER, screenRectangle.height * ZOOM_BUFFER);
+
+  // Don't zoom in more than a factor of 2
+  if (scale > 2) scale = 2;
+
+  viewport.animate({
+    time: ZOOM_ANIMATION_TIME_MS,
+    position: new Point(screenRectangle.x + screenRectangle.width / 2, screenRectangle.y + screenRectangle.height / 2),
+    scale,
+  });
 }
