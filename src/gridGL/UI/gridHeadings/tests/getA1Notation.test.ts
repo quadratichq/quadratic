2bb27483e3c269a6f14ea531554af3e4e4e3cf05
David Kircos <davidkircos@gmail.com>
2023-03-14 12:01:05 -0600

    add A1 Notation tests from Rust to JS (failing)

--- src/gridGL/UI/gridHeadings/tests/getA1Notation.test.ts
+++ src/gridGL/UI/gridHeadings/tests/getA1Notation.test.ts
@@ -1,19 +1,60 @@
 import { getColumnA1Notation, getRowA1Notation } from '../getA1Notation';
 
 describe('A1 notation translation', () => {
-  it('gets column (positive)', () => {
+  it('gets column (based on tests in quadratic-core)', () => {
+    // Test near 0
     expect(getColumnA1Notation(0)).toBe('A');
     expect(getColumnA1Notation(1)).toBe('B');
+    expect(getColumnA1Notation(2)).toBe('C');
+    expect(getColumnA1Notation(3)).toBe('D');
+    expect(getColumnA1Notation(4)).toBe('E');
+    expect(getColumnA1Notation(5)).toBe('F');
+
+    expect(getColumnA1Notation(-1)).toBe('nA');
+    expect(getColumnA1Notation(-2)).toBe('nB');
+    expect(getColumnA1Notation(-3)).toBe('nC');
+    expect(getColumnA1Notation(-4)).toBe('nD');
+    expect(getColumnA1Notation(-5)).toBe('nE');
+    expect(getColumnA1Notation(-6)).toBe('nF');
+
+    // Test near ±26
+    expect(getColumnA1Notation(24)).toBe('Y');
     expect(getColumnA1Notation(25)).toBe('Z');
     expect(getColumnA1Notation(26)).toBe('AA');
-    expect(getColumnA1Notation(100)).toBe('CW');
-  });
-
-  it('gets column name (negative)', () => {
-    expect(getColumnA1Notation(-1)).toBe('nA');
+    expect(getColumnA1Notation(27)).toBe('AB');
+    expect(getColumnA1Notation(-25)).toBe('nY');
     expect(getColumnA1Notation(-26)).toBe('nZ');
     expect(getColumnA1Notation(-27)).toBe('nAA');
-    expect(getColumnA1Notation(-100)).toBe('nCV');
+    expect(getColumnA1Notation(-28)).toBe('nAB');
+
+    // Test near ±52
+    expect(getColumnA1Notation(50)).toBe('AY');
+    expect(getColumnA1Notation(51)).toBe('AZ');
+    expect(getColumnA1Notation(52)).toBe('BA');
+    expect(getColumnA1Notation(53)).toBe('BB');
+    expect(getColumnA1Notation(-51)).toBe('nAY');
+    expect(getColumnA1Notation(-52)).toBe('nAZ');
+    expect(getColumnA1Notation(-53)).toBe('nBA');
+    expect(getColumnA1Notation(-54)).toBe('nBB');
+
+    // Test near ±702
+    expect(getColumnA1Notation(700)).toBe('ZY');
+    expect(getColumnA1Notation(701)).toBe('ZZ');
+    expect(getColumnA1Notation(702)).toBe('AAA');
+    expect(getColumnA1Notation(703)).toBe('AAB');
+    expect(getColumnA1Notation(-701)).toBe('nZY');
+    expect(getColumnA1Notation(-702)).toBe('nZZ');
+    expect(getColumnA1Notation(-703)).toBe('nAAA');
+    expect(getColumnA1Notation(-704)).toBe('nAAB');
+
+    // // Test 64 bit integer limits (±9,223,372,036,854,775,807)
+    expect(getColumnA1Notation(9223372036854775807)).toBe('CRPXNLSKVLJFHH');
+    expect(getColumnA1Notation(-9223372036854775808)).toBe('nCRPXNLSKVLJFHH');
+
+    // test fun stuff
+    expect(getColumnA1Notation(3719092809668)).toBe('QUADRATIC');
+    expect(getColumnA1Notation(-3719092809669)).toBe('nQUADRATIC');
+    expect(getColumnA1Notation(1700658608758053877)).toBe('QUICKBROWNFOX');
   });
 
   it('gets row (positive)', () => {
