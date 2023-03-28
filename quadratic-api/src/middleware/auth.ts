fc97662cbed22c27ceab98db20db2271e1b21736
David Kircos <davidkircos@gmail.com>
2023-03-28 15:29:13 -0600

    update error msg

--- quadratic-api/src/middleware/auth.ts
+++ quadratic-api/src/middleware/auth.ts
@@ -4,7 +4,7 @@ import jwksRsa from 'jwks-rsa';
 // based on implementation from https://github.com/auth0-developer-hub/api_express_typescript_hello-world/blob/main/src/middleware/auth0.middleware.ts
 
 if (!process.env.AUTH0_JWKS_URI || !process.env.AUTH0_AUDIENCE || !process.env.AUTH0_ISSUER) {
-  throw new Error('AUTH0_JWKS_URI, AUTH0_AUDIENCE, or AUTH0_ISSUER are not defined');
+  throw new Error('AUTH0_JWKS_URI, AUTH0_AUDIENCE, or AUTH0_ISSUER need to be defined');
 }
 
 export const validateAccessToken = expressjwt({
