43790550ba423c60389f281034ff125e8bf903e5
David Kircos <davidkircos@gmail.com>
2023-04-13 10:56:51 -0600

    Add MixPanel Analytics (#423)
    * add MixPanel analytics
    
    * reduce events fired on load
    
    * recognize 'none' env var as undefined

--- src/quadratic/AnalyticsProvider.tsx
+++ src/quadratic/AnalyticsProvider.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import * as amplitude from '@amplitude/analytics-browser';
 import { useAuth0, User } from '@auth0/auth0-react';
+import mixpanel from 'mixpanel-browser';
 
 // Quadratic only shares analytics on the QuadraticHQ.com hosted version where the environment variables are set.
 
 const loadGoogleAnalytics = async () => {
-  if (!process.env.REACT_APP_GOOGLE_ANALYTICS_GTAG) {
+  if (!process.env.REACT_APP_GOOGLE_ANALYTICS_GTAG && process.env.REACT_APP_GOOGLE_ANALYTICS_GTAG !== 'none') {
     return;
   }
 
@@ -32,7 +33,10 @@ const loadGoogleAnalytics = async () => {
 };
 
 const loadAmplitudeAnalytics = async (user: User | undefined) => {
-  if (!process.env.REACT_APP_AMPLITUDE_ANALYTICS_API_KEY) {
+  if (
+    !process.env.REACT_APP_AMPLITUDE_ANALYTICS_API_KEY &&
+    process.env.REACT_APP_AMPLITUDE_ANALYTICS_API_KEY !== 'none'
+  ) {
     return;
   }
 
@@ -41,6 +45,22 @@ const loadAmplitudeAnalytics = async (user: User | undefined) => {
   });
 
   console.log('[Analytics] Amplitude activated');
+};
+
+const loadMixPanelAnalytics = async (user: User | undefined) => {
+  if (!process.env.REACT_APP_MIXPANEL_ANALYTICS_KEY && process.env.REACT_APP_MIXPANEL_ANALYTICS_KEY !== 'none') {
+    return;
+  }
+
+  mixpanel.init(process.env.REACT_APP_MIXPANEL_ANALYTICS_KEY);
+
+  mixpanel.register({
+    email: user?.email,
+  });
+
+  mixpanel.identify(user?.sub);
+
+  console.log('[Analytics] Mixpanel activated');
 };
 
 export const AnalyticsProvider = () => {
@@ -54,6 +74,7 @@ export const AnalyticsProvider = () => {
 
     loadGoogleAnalytics();
     loadAmplitudeAnalytics(user);
+    loadMixPanelAnalytics(user);
   }, [loaded, setLoaded, user]);
 
   return null;
