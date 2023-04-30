d49ba9bcca0c6574bdb4545c4e97156d78a286cb
David Figatner <david@yopeyopey.com>
2023-04-30 06:00:14 -0700

    clean up a bit

--- src/gridGL/interaction/CellInput.tsx
+++ src/gridGL/interaction/CellInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { GridInteractionState } from '../../atoms/gridInteractionStateAtom';
 import { Coordinate } from '../types/size';
 import { focusGrid } from '../../helpers/focusGrid';
@@ -21,26 +21,47 @@ export const CellInput = (props: CellInputProps) => {
   const { interactionState, editorInteractionState, setInteractionState, app, container, sheetController } = props;
   const viewport = app?.viewport;
 
-  const [value, setValue] = useState<string | undefined>(undefined);
+  const cellLocation = interactionState.cursorPosition;
 
-  const cellLocation = useRef(interactionState.cursorPosition);
-  const textInput = useRef<HTMLInputElement>(null);
+  const text = useRef('');
+  const handleChange = useCallback((e) => {
+    console.log(e.target.value);
+    text.current = e.target.value;
+  }, []);
+
   // Effect for sizing the input width to the length of the value
+  const [textInput, setTextInput] = useState<HTMLDivElement>();
+  const textInputRef = useCallback((node) => {
+    node?.focus();
+    setTextInput(node);
+  }, []);
+
+  const cell_offsets = sheetController.sheet.gridOffsets.getCell(cellLocation.x, cellLocation.y);
+  const copy = sheetController.sheet.getCellAndFormatCopy(cellLocation.x, cellLocation.y);
+  const cell = copy?.cell;
+  const format = copy?.format;
+
+  const fontFamily = useMemo(() => {
+    if (!format) return 'OpenSans';
+    if (format.italic && format.bold) return 'OpenSans-BoldItalic';
+    if (format.italic) return 'OpenSans-Italic';
+    if (format.bold) return 'OpenSans-Bold';
+  }, [format]);
+
   useEffect(() => {
-    if (textInput.current) textInput.current.size = value?.length || 0 + 1;
-  }, [value, textInput]);
+    if (cell?.value) {
+      text.current = cell.value;
+    }
+  }, [cell?.value]);
 
   // If we don't have a viewport, we can't continue.
   if (!viewport || !container) return null;
 
-  const cell_offsets = sheetController.sheet.gridOffsets.getCell(cellLocation.current.x, cellLocation.current.y);
-  const cell = sheetController.sheet.getCellCopy(cellLocation.current.x, cellLocation.current.y);
-
   // If the cell is open in the code editor, don't show the input
   if (
     editorInteractionState.showCodeEditor &&
-    editorInteractionState.selectedCell.x === cellLocation.current.x &&
-    editorInteractionState.selectedCell.y === cellLocation.current.y
+    editorInteractionState.selectedCell.x === cellLocation.x &&
+    editorInteractionState.selectedCell.y === cellLocation.y
   )
     return null;
 
@@ -51,11 +72,8 @@ export const CellInput = (props: CellInputProps) => {
     // Get world transform matrix
     let worldTransform = viewport.worldTransform;
 
-    // Calculate position of input based on cell
-    let cell_offset_scaled = viewport.toScreen(
-      cell_offsets.x,
-      cell_offsets.y - 0.66 // magic number via experimentation
-    );
+    // Calculate position of input based on cell (magic number via experimentation)
+    let cell_offset_scaled = viewport.toScreen(cell_offsets.x + 2, cell_offsets.y + 3);
 
     // Generate transform CSS
     const transform =
@@ -71,7 +89,7 @@ export const CellInput = (props: CellInputProps) => {
       ')';
 
     // Update input css matrix
-    if (textInput.current) textInput.current.style.transform = transform;
+    if (textInput) textInput.style.transform = transform;
 
     // return transform
     return transform;
@@ -87,8 +105,10 @@ export const CellInput = (props: CellInputProps) => {
 
   // When done editing with the input
   const closeInput = async (transpose = { x: 0, y: 0 } as Coordinate, cancel = false) => {
-    if (closed) return;
+    if (closed || !textInput) return;
     closed = true;
+
+    const value = textInput.innerText;
 
     if (!cancel) {
       // Update Cell and dependent cells
@@ -96,10 +116,10 @@ export const CellInput = (props: CellInputProps) => {
         // delete cell if input is empty, and wasn't empty before
         if (cell !== undefined)
           DeleteCells({
-            x0: cellLocation.current.x,
-            y0: cellLocation.current.y,
-            x1: cellLocation.current.x,
-            y1: cellLocation.current.y,
+            x0: cellLocation.x,
+            y0: cellLocation.y,
+            x1: cellLocation.x,
+            y1: cellLocation.y,
             sheetController,
             app,
           });
@@ -108,8 +128,8 @@ export const CellInput = (props: CellInputProps) => {
         await updateCellAndDCells({
           starting_cells: [
             {
-              x: cellLocation.current.x,
-              y: cellLocation.current.y,
+              x: cellLocation.x,
+              y: cellLocation.y,
               type: 'TEXT',
               value: value || '',
             },
@@ -118,7 +138,7 @@ export const CellInput = (props: CellInputProps) => {
           app,
         });
       }
-      app.quadrants.quadrantChanged({ cells: [cellLocation.current] });
+      app.quadrants.quadrantChanged({ cells: [cellLocation] });
     }
 
     // Update Grid Interaction state, reset input value state
@@ -135,7 +155,7 @@ export const CellInput = (props: CellInputProps) => {
       showInput: false,
       inputInitialValue: '',
     });
-    setValue(undefined);
+    // setValue(undefined);
 
     // Set focus back to Grid
     focusGrid();
@@ -145,24 +165,18 @@ export const CellInput = (props: CellInputProps) => {
     viewport.off('moved', updateInputCSSTransform);
   };
 
-  // Happens when a cell is being edited
-  if (value === undefined && value !== interactionState.inputInitialValue) {
-    // Set initial value and remember this cells position.
-    setValue(interactionState.inputInitialValue);
-    cellLocation.current = interactionState.cursorPosition;
-
-    // Register lister for when grid moves to resize and move input with CSS
-    viewport.on('moved', updateInputCSSTransform);
-    viewport.on('moved-end', updateInputCSSTransform);
-  }
+  // Register lister for when grid moves to resize and move input with CSS
+  viewport.on('moved', updateInputCSSTransform);
+  viewport.on('moved-end', updateInputCSSTransform);
 
   // set input's initial position correctly
   const transform = updateInputCSSTransform();
 
   return (
-    <input
-      autoFocus
-      ref={textInput}
+    <div
+      contentEditable={true}
+      suppressContentEditableWarning={true}
+      ref={textInputRef}
       spellCheck={false}
       style={{
         display: 'block',
@@ -171,19 +185,20 @@ export const CellInput = (props: CellInputProps) => {
         left: 0,
         minWidth: 100,
         border: 'none',
-        outline: 'none',
+        outline: 'solid',
+        outlineColor: format?.fillColor ?? 'transparent',
+        color: format?.textColor ?? 'black',
+        padding: 0,
+        margin: 0,
         lineHeight: '1',
-        background: 'none',
+        background: format?.fillColor ?? 'transparent',
         transformOrigin: '0 0',
         transform,
-        fontFamily: 'OpenSans',
+        fontFamily,
         fontSize: '14px',
-        letterSpacing: '0.07px',
+        // letterSpacing: '0.07px',
       }}
-      value={value}
-      onChange={(event) => {
-        setValue(event.target.value);
-      }}
+      onChange={handleChange}
       onBlur={() => {
         closeInput();
       }}
@@ -208,6 +223,8 @@ export const CellInput = (props: CellInputProps) => {
           event.stopPropagation();
         }
       }}
-    ></input>
+    >
+      {text.current}
+    </div>
   );
 };
