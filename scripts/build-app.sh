06bb3922efe117afba846ee22cfbfa899a96f46e
Jim Nielsen <jimniels@gmail.com>
2023-05-02 22:09:25 -0600

    Update build-app.sh

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -1,7 +1,7 @@
 echo 'Installing npm depdencies...'
 npm install
 
-echo 'Installing Rustup...'
+echo 'Installing rustup...'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
