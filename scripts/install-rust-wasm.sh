769cb6032c032172396dd453d7bb0c4b122b7ab0
Jim Nielsen <jimniels@gmail.com>
2023-05-02 22:57:39 -0600

    tweaks

---
+++ scripts/install-rust-wasm.sh
@@ -0,0 +1,6 @@
+echo 'Installing rustup...'
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
+echo 'Installing wasm-pack...'
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
