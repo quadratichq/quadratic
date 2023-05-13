d4f03dfb83c2ceeb1b1c811c78547b002db44422
David Figatner <david@yopeyopey.com>
2023-05-13 08:02:37 -0700

    fix delete empty cells

--- src/grid/actions/DeleteCells.ts
+++ src/grid/actions/DeleteCells.ts
@@ -16,21 +16,19 @@ interface DeleteCellsArgs {
 export const DeleteCells = async (args: DeleteCellsArgs) => {
   const { x0, y0, x1, y1, sheetController, pyodide, app, create_transaction } = args;
 
+  const cells_to_delete = sheetController.sheet.grid.getNakedCells(x0, y0, x1, y1);
+  if (!cells_to_delete.length) return;
+
   if (create_transaction ?? true) sheetController.start_transaction();
 
-  // delete cells row by row
-  for (var current_row = y0; current_row <= y1; current_row++) {
-    const cells_to_delete = sheetController.sheet.grid.getNakedCells(x0, current_row, x1, current_row);
-    // delete cells
-    await updateCellAndDCells({
-      starting_cells: cells_to_delete,
-      sheetController,
-      pyodide,
-      delete_starting_cells: true,
-      app,
-      create_transaction: false,
-    });
-  }
+  await updateCellAndDCells({
+    starting_cells: cells_to_delete,
+    sheetController,
+    pyodide,
+    delete_starting_cells: true,
+    app,
+    create_transaction: false,
+  });
 
   if (create_transaction ?? true) sheetController.end_transaction();
 };
