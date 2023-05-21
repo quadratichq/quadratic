1b68a854c541449f7e41a081a831bd6234d1a18e
David Figatner <david@yopeyopey.com>
2023-05-21 05:53:41 -0700

    fixed undo stack for delete

--- src/grid/actions/DeleteCells.ts
+++ src/grid/actions/DeleteCells.ts
@@ -16,19 +16,34 @@ interface DeleteCellsArgs {
 export const DeleteCells = async (args: DeleteCellsArgs) => {
   const { x0, y0, x1, y1, sheetController, pyodide, app, create_transaction } = args;
 
-  const cells_to_delete = sheetController.sheet.grid.getNakedCells(x0, y0, x1, y1);
-  if (!cells_to_delete.length) return;
-
   if (create_transaction ?? true) sheetController.start_transaction();
 
-  await updateCellAndDCells({
-    starting_cells: cells_to_delete,
-    sheetController,
-    pyodide,
-    delete_starting_cells: true,
-    app,
-    create_transaction: false,
-  });
+  // cancel transaction if there are no cells to delete
+  let deletingCells = false;
 
-  if (create_transaction ?? true) sheetController.end_transaction();
+  // delete cells row by row
+  for (var current_row = y0; current_row <= y1; current_row++) {
+    const cells_to_delete = sheetController.sheet.grid.getNakedCells(x0, current_row, x1, current_row);
+
+    // delete cells
+    if (cells_to_delete.length !== 0) {
+      deletingCells = true;
+      await updateCellAndDCells({
+        starting_cells: cells_to_delete,
+        sheetController,
+        pyodide,
+        delete_starting_cells: true,
+        app,
+        create_transaction: false,
+      });
+    }
+  }
+
+  if (create_transaction ?? true) {
+    if (!deletingCells) {
+      sheetController.cancel_transaction();
+    } else {
+      sheetController.end_transaction();
+    }
+  }
 };
