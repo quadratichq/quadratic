0232a5c89575895636c3aa679d91edb11af8696a
Jim Nielsen <jimniels@gmail.com>
2023-05-31 12:00:32 -0600

    feat: add a feedback widget (#450)
    * initial commit
    
    * start
    
    * tweaks
    
    * Update feedback.ts
    
    * Update debugFlags.ts
    
    * move logic into api layer
    
    * add v0 prefix for feedback
    
    * remove email
    
    * add minrows
    
    * don't show feedback on mobile
    
    ---------
    
    Co-authored-by: David Kircos <davidkircos@gmail.com>

---
+++ src/ui/menus/FeedbackMenu/FeedbackMenu.tsx
@@ -0,0 +1,127 @@
+import { useState } from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  TextField,
+} from '@mui/material';
+import { useTheme } from '@mui/system';
+import { useRecoilState } from 'recoil';
+import { editorInteractionStateAtom } from '../../../atoms/editorInteractionStateAtom';
+import { BUG_REPORT_URL, DISCORD, TWITTER } from '../../../constants/urls';
+import useLocalStorage from '../../../hooks/useLocalStorage';
+import { useGlobalSnackbar } from '../../contexts/GlobalSnackbar';
+import apiClientSingleton from '../../../api-client/apiClientSingleton';
+import { SocialDiscord, SocialGithub, SocialTwitter } from '../../icons';
+
+export const FeedbackMenu = () => {
+  const [editorInteractionState, setEditorInteractionState] = useRecoilState(editorInteractionStateAtom);
+  const { showFeedbackMenu } = editorInteractionState;
+  // We'll keep the user's state around unless they explicitly cancel or get a successful submit
+  const [value, setValue] = useLocalStorage('feedback-message', '');
+  const [loadState, setLoadState] = useState<'INITIAL' | 'LOADING' | 'LOAD_ERROR'>('INITIAL');
+  const theme = useTheme();
+  const { addGlobalSnackbar } = useGlobalSnackbar();
+
+  const closeMenu = () => {
+    setEditorInteractionState((state) => ({
+      ...state,
+      showFeedbackMenu: false,
+    }));
+  };
+
+  const onSubmit = async () => {
+    setLoadState('LOADING');
+    const success = await apiClientSingleton.postFeedback(value);
+    if (success) {
+      setValue('');
+      closeMenu();
+      addGlobalSnackbar('Feedback submitted! Thank you.');
+    } else {
+      setLoadState('LOAD_ERROR');
+    }
+  };
+
+  const isLoading = loadState === 'LOADING';
+  const hasError = loadState === 'LOAD_ERROR';
+
+  return (
+    <Dialog open={showFeedbackMenu} onClose={closeMenu} fullWidth maxWidth={'sm'} BackdropProps={{ invisible: true }}>
+      <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <span>Provide feedback</span>
+        <span style={{ display: 'flex', alignItems: 'center', gap: theme.spacing(1) }}>
+          <IconButton href={BUG_REPORT_URL} target="_blank" color="inherit">
+            <SocialGithub />
+          </IconButton>
+          <IconButton href={TWITTER} target="_blank" color="inherit">
+            <SocialTwitter />
+          </IconButton>
+          <IconButton href={DISCORD} target="_blank" color="inherit">
+            <SocialDiscord />
+          </IconButton>
+        </span>
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>How can we make Quadratic better? Reach out, or let us know below:</DialogContentText>
+
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          id="feedback"
+          variant="outlined"
+          disabled={isLoading}
+          fullWidth
+          minRows={4}
+          multiline
+          sx={{ mt: theme.spacing(2) }}
+          autoFocus
+          value={value}
+          onFocus={(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+            // If an error exists, reset
+            if (loadState === 'LOAD_ERROR') {
+              setLoadState('INITIAL');
+            }
+            // Ensure cursor position to the end on focus
+            if (value.length > 0) {
+              event.target.setSelectionRange(value.length, value.length);
+            }
+          }}
+          // Allow submit via keyboard CMD + Enter
+          onKeyDown={(event) => {
+            if ((event.metaKey || event.ctrlKey) && event.key === 'Enter') {
+              onSubmit();
+            }
+          }}
+          onChange={(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+            setValue(event.target.value);
+          }}
+          {...(hasError ? { error: true, helperText: 'Failed to send. Try again.' } : {})}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant="text"
+          color="inherit"
+          onClick={() => {
+            setValue('');
+            closeMenu();
+          }}
+          disabled={isLoading}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="text"
+          sx={{ mr: theme.spacing(1) }}
+          onClick={onSubmit}
+          disabled={value.length === 0 || isLoading}
+        >
+          {isLoading ? 'Submitting…' : 'Submit'}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
--- src/ui/menus/TopBar/SubMenus/QuadraticMenu.tsx
+++ src/ui/menus/TopBar/SubMenus/QuadraticMenu.tsx
@@ -7,7 +7,7 @@ import { IS_READONLY_MODE } from '../../../../constants/app';
 import { useGridSettings } from './useGridSettings';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Tooltip } from '@mui/material';
-import { DOCUMENTATION_URL, BUG_REPORT_URL } from '../../../../constants/urls';
+import { DOCUMENTATION_URL } from '../../../../constants/urls';
 import { SheetController } from '../../../../grid/controller/sheetController';
 import { MenuLineItem } from '../MenuLineItem';
 import { KeyboardSymbols } from '../../../../helpers/keyboardSymbols';
@@ -190,7 +190,16 @@ export const QuadraticMenu = (props: Props) => {
 
         <SubMenu label="Help">
           <MenuItem onClick={() => window.open(DOCUMENTATION_URL, '_blank')}>Read the docs</MenuItem>
-          <MenuItem onClick={() => window.open(BUG_REPORT_URL, '_blank')}>Report a problem</MenuItem>
+          <MenuItem
+            onClick={() =>
+              setEditorInteractionState((prevState) => ({
+                ...prevState,
+                showFeedbackMenu: true,
+              }))
+            }
+          >
+            Provide feedback
+          </MenuItem>
         </SubMenu>
       </Menu>
     </>
