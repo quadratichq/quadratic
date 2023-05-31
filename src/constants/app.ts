2e388a16096213ee29fcc71de230adffbe6d64f9
David Kircos <davidkircos@gmail.com>
2023-05-31 10:45:21 -0600

    Update app.ts

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -25,9 +25,9 @@ export const EXAMPLE_FILES = [
     description: 'Example of pulling data from the Mercury API.',
   },
   {
-    name: 'Open AI (example)',
+    name: 'OpenAI (example)',
     file: 'open_ai.grid',
-    description: 'Example prompt querying the Open AI API.',
+    description: 'Example prompt querying the OpenAI API.',
   },
   // Leaving this one out, as it has nothing useful for users
   // { name: 'Airports large (example)', file: 'airports_large.grid', description: 'Lorem ipsum santa dolor.' },
