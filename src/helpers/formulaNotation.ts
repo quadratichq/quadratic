dde949e9098edbc35829208ee5641a600f19655d
Juan <jmbercoff@gmail.com>
2023-06-16 10:57:46 -0300

    remove parser and add return type

--- src/helpers/formulaNotation.ts
+++ src/helpers/formulaNotation.ts
@@ -1,58 +1,79 @@
+import { getCoordinatesFromStringId } from '../grid/actions/updateCellAndDCells';
 import { GridOffsets } from '../grid/sheet/GridOffsets';
 import { Coordinate } from '../gridGL/types/size';
+import { CursorCell } from '../gridGL/UI/Cursor';
+import { CellRefId } from '../hooks/useEditorCellHighlights';
+import { StringId } from './getKey';
 
-export function getColumnFromFormulaNotation(formulaNotation: string) {
-  const { formulaNotation: parsedFormulaNotation, isNegative } = parseNegative(formulaNotation);
-  let columnNumber = 0;
+type Position = {
+  Relative: number;
+  Absolute?: number;
+};
 
-  for (let i = 0; i < parsedFormulaNotation.length; i++) {
-    columnNumber = parsedFormulaNotation[i].charCodeAt(0) - 64 + columnNumber * 26;
-  }
-  columnNumber--;
-  return columnNumber * (isNegative ? -1 : 1) + (isNegative ? -1 : 0);
-}
+type CoordiantePositionType = {
+  x: Position;
+  y: Position;
+};
+
+export type Span = { start: number; end: number };
+
+export type CellRef = { Cell: CoordiantePositionType } | { CellRange: CoordiantePositionType[] };
+
+export type ParseFormulaReturnType = {
+  parse_error_msg: string;
+  parse_error_span: { start: number; end: number };
+  cell_refs: {
+    cell_ref: CellRef;
+    span: Span;
+  }[];
+};
 
 export function getCellFromFormulaNotation(
-  formulaNotation: string,
+  cellRefId: CellRefId,
   gridOffsets: GridOffsets,
   editorCursorPosition: Coordinate
 ) {
-  const match = formulaNotation.match(/\$?(n?[A-Z]+)\$?(n?\d+)/);
-  if (!match) return false;
-  const columnNumber = getColumnFromFormulaNotation(match[1]);
-  const { formulaNotation: rowFormulaNotation, isNegative } = parseNegative(match[2]);
-  const rowNumber = parseInt(rowFormulaNotation) * (isNegative ? -1 : 1);
+  const isSimpleCell = !cellRefId.includes(':');
 
+  if (isSimpleCell) {
+    const [x, y] = getCoordinatesFromStringId(cellRefId);
+    return getCellWithLimit(gridOffsets, editorCursorPosition, y, x);
+  }
+  const [startCell, endCell] = cellRefId.split(':') as [StringId, StringId];
+  const [startCellX, startCellY] = getCoordinatesFromStringId(startCell);
+  const [endCellX, endCellY] = getCoordinatesFromStringId(endCell);
+
+  return {
+    startCell: getCellWithLimit(gridOffsets, editorCursorPosition, startCellY, startCellX),
+    endCell: getCellWithLimit(gridOffsets, editorCursorPosition, endCellY, endCellX),
+  };
+}
+
+function getCellWithLimit(
+  gridOffsets: GridOffsets,
+  editorCursorPosition: Coordinate,
+  row: number,
+  column: number,
+  offset = 20000
+): CursorCell {
   // getCell is slow with more than 9 digits, so limit if column or row is > editorCursorPosition + an offset
   // If it's a single cell to be highlighted, it won't be visible anyway, and if it's a range
   // It will highlight beyond the what's visible in the viewport
   return gridOffsets.getCell(
-    Math.min(columnNumber, editorCursorPosition.x + 20000),
-    Math.min(rowNumber, editorCursorPosition.y + 20000)
+    Math.min(column, editorCursorPosition.x + offset),
+    Math.min(row, editorCursorPosition.y + offset)
   );
 }
 
-export function parseMulticursorFormulaNotation(
-  multicursorFormulaNotation: string,
-  gridOffsets: GridOffsets,
-  editorCursorPosition: Coordinate
-) {
-  const [startCellLetter, endCellLetter] = multicursorFormulaNotation.split(':');
-  const startCell = getCellFromFormulaNotation(startCellLetter, gridOffsets, editorCursorPosition);
-  const endCell = getCellFromFormulaNotation(endCellLetter, gridOffsets, editorCursorPosition);
-
-  if (!startCell || !endCell) return false;
-  return {
-    startCell,
-    endCell,
-  };
-}
-
-function parseNegative(formulaNotation: string) {
-  const isNegative = formulaNotation[0] === 'n';
-  formulaNotation = isNegative ? formulaNotation.substring(1) : formulaNotation;
-  return {
-    isNegative,
-    formulaNotation,
-  };
+export function isCellRangeTypeGuard(obj: any): obj is { startCell: CursorCell; endCell: CursorCell } {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'startCell' in obj &&
+    typeof obj.startCell === 'object' &&
+    obj.startCell !== null &&
+    'endCell' in obj &&
+    typeof obj.endCell === 'object' &&
+    obj.endCell !== null
+  );
 }
