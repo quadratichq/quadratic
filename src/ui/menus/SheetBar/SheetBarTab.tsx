4eac1501aacfab5bc4bca77725300cb98fa3943c
David Figatner <david@yopeyopey.com>
2023-06-17 09:34:06 -0700

    handle blur when sheet name is not unique

--- src/ui/menus/SheetBar/SheetBarTab.tsx
+++ src/ui/menus/SheetBar/SheetBarTab.tsx
@@ -1,6 +1,6 @@
 import './SheetBarTab.css';
 
-import { MouseEvent, PointerEvent, useCallback, useEffect, useState } from 'react';
+import { MouseEvent, PointerEvent, useCallback, useEffect, useRef, useState } from 'react';
 import { Sheet } from '../../../grid/sheet/Sheet';
 import { useLocalFiles } from '../../contexts/LocalFiles';
 import { SheetController } from '../../../grid/controller/sheetController';
@@ -29,21 +29,20 @@ export const SheetBarTab = (props: Props): JSX.Element => {
     }
   }, [forceRename]);
 
-  const [input, setInput] = useState<HTMLInputElement | undefined>();
+  const divRef = useRef<HTMLDivElement | null>(null);
+
   const inputRef = useCallback(
     (node: HTMLInputElement) => {
       if (node) {
         node.value = sheet.name;
-        setInput(node);
-        node.focus();
       }
     },
     [sheet.name]
   );
 
-  if (isRenaming) {
-    return (
-      <>
+  return (
+    <div ref={divRef}>
+      {isRenaming && (
         <input
           ref={inputRef}
           className="sheet-tab-input"
@@ -81,52 +80,50 @@ export const SheetBarTab = (props: Props): JSX.Element => {
           onBlur={(event) => {
             const input = event.currentTarget as HTMLInputElement;
             if (!input) return false;
-            setIsRenaming((isRenaming: boolean) => {
-              if (!isRenaming) return false;
-              if (input.value !== sheet.name) {
-                if (sheetController.sheetNameExists(input.value)) {
-                  setNameExists(true);
-                  window.setTimeout(() => input.focus(), 0);
-                  return true;
-                }
+            setIsRenaming(false);
+            if (input.value !== sheet.name) {
+              if (!sheetController.sheetNameExists(input.value)) {
                 sheetController.sheet.rename(input.value);
                 localFiles.save();
+              } else {
+                setNameExists(true);
+                setTimeout(() => setNameExists(false), 1500);
               }
-              setNameExists(false);
-              clearRename();
-              focusGrid();
-              return false;
-            });
+            }
+            clearRename();
+            focusGrid();
+            return false;
           }}
         />
-        <Popper open={isRenaming && nameExists} anchorEl={input} transition>
-          {({ TransitionProps }) => (
-            <Fade {...TransitionProps} timeout={350}>
-              <Box sx={{ border: 1, p: 0.5, marginBottom: 1, backgroundColor: 'rgba(255, 0, 0, 0.25)' }}>
-                Sheet name must be unique
-              </Box>
-            </Fade>
-          )}
-        </Popper>
-      </>
-    );
-  }
+      )}
 
-  return (
-    <div
-      className={active ? 'sheet-tab-active' : 'sheet-tab'}
-      data-order={sheet.order * 2}
-      data-id={sheet.id}
-      style={{
-        borderBottomColor: sheet.color,
-        // * 2 is needed so there's a space next to each tab
-        order: sheet.order * 2,
-      }}
-      onPointerDown={(event) => onPointerDown({ event, sheet })}
-      onDoubleClick={() => setIsRenaming(true)}
-      onContextMenu={(e) => onContextMenu(e, sheet)}
-    >
-      {sheet.name}
+      {!isRenaming && (
+        <div
+          className={active ? 'sheet-tab-active' : 'sheet-tab'}
+          data-order={sheet.order * 2}
+          data-id={sheet.id}
+          style={{
+            borderBottomColor: sheet.color,
+            // * 2 is needed so there's a space next to each tab
+            order: sheet.order * 2,
+          }}
+          onPointerDown={(event) => onPointerDown({ event, sheet })}
+          onDoubleClick={() => setIsRenaming(true)}
+          onContextMenu={(e) => onContextMenu(e, sheet)}
+        >
+          {sheet.name}
+        </div>
+      )}
+
+      <Popper open={nameExists} anchorEl={divRef.current} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Box sx={{ border: 1, p: 0.5, marginBottom: 1, backgroundColor: 'rgba(255, 0, 0, 0.25)' }}>
+              Sheet name must be unique
+            </Box>
+          </Fade>
+        )}
+      </Popper>
     </div>
   );
 };
