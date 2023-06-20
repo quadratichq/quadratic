ed4d33eaf37c577fbf03bf6bf87f13319a84689f
Jim Nielsen <jimniels@gmail.com>
2023-06-20 09:32:22 -0600

    initial commit

--- quadratic-api/src/routes/feedback.ts
+++ quadratic-api/src/routes/feedback.ts
@@ -10,13 +10,15 @@ const prisma = new PrismaClient();
 
 const RequestBodySchema = z.object({
   feedback: z.string(),
+  userEmail: z.string().optional(),
 });
 type RequestBody = z.infer<typeof RequestBodySchema>;
 
 files_router.post('/', validateAccessToken, async (request: JWTRequest, response) => {
-  const { feedback }: RequestBody = RequestBodySchema.parse(request.body);
+  const { feedback, userEmail }: RequestBody = RequestBodySchema.parse(request.body);
   const user = await get_user(request);
 
+  // Add to DB
   await prisma.qFeedback.create({
     data: {
       feedback,
@@ -24,6 +26,28 @@ files_router.post('/', validateAccessToken, async (request: JWTRequest, response
       created_date: new Date(),
     },
   });
+
+  // Post to Slack
+  if (process.env.SLACK_FEEDBACK_URL) {
+    const payload = {
+      text: [
+        `📣 ${process.env.NODE_ENV === 'production' ? '' : '[STAGING]'} New product feedback`,
+        `*From:* ${userEmail ? userEmail : `[no email]`} (${user.auth0_user_id})`,
+        '*Message*:',
+        feedback,
+      ].join('\n\n'),
+    };
+    // @ts-expect-error we're on node18 so global fetch is ok
+    fetch(process.env.SLACK_FEEDBACK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    }).catch((e: Error) => {
+      console.log('Failed to post feedback to Slack', e);
+    });
+  }
 
   response.status(200).end();
 });
