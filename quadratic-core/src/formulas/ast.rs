434f9c10d501b1faea94f00bf4a33ce9bb196041
Hactar <6060305+HactarCE@users.noreply.github.com>
2023-06-22 17:37:06 -0400

    Add boolean parsing in formulas (#572)
    * Add boolean parsing in formulas
    * Test bool parsing
    * Omit parens in autocomplete for `TRUE` and `FALSE`

--- quadratic-core/src/formulas/ast.rs
+++ quadratic-core/src/formulas/ast.rs
@@ -30,6 +30,7 @@ pub enum AstNodeContents {
     CellRef(CellRef),
     String(String),
     Number(f64),
+    Bool(bool),
 }
 impl fmt::Display for AstNodeContents {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -55,6 +56,8 @@ impl fmt::Display for AstNodeContents {
             AstNodeContents::CellRef(cellref) => write!(f, "{cellref}"),
             AstNodeContents::String(s) => write!(f, "{s:?}"),
             AstNodeContents::Number(n) => write!(f, "{n:?}"),
+            AstNodeContents::Bool(false) => write!(f, "FALSE"),
+            AstNodeContents::Bool(true) => write!(f, "TRUE"),
         }
     }
 }
@@ -72,6 +75,7 @@ impl AstNodeContents {
             AstNodeContents::CellRef(_) => "cell reference",
             AstNodeContents::String(_) => "string literal",
             AstNodeContents::Number(_) => "numeric literal",
+            AstNodeContents::Bool(_) => "boolean literal",
         }
     }
 }
@@ -203,8 +207,8 @@ impl AstNode {
             }
 
             AstNodeContents::String(s) => Value::from(s.to_string()),
-
             AstNodeContents::Number(n) => Value::from(*n),
+            AstNodeContents::Bool(b) => Value::from(*b),
         };
 
         Ok(Spanned {
--- quadratic-core/src/formulas/functions/macros.rs
+++ quadratic-core/src/formulas/functions/macros.rs
@@ -107,7 +107,7 @@ macro_rules! formula_fn {
     ) => {
         $crate::formulas::functions::FormulaFunction {
             name: $fn_name,
-            arg_completion: "",
+            arg_completion: None,
             usage: "",
             examples: &[],
             doc: "",
@@ -122,14 +122,21 @@ macro_rules! formula_fn {
     (
         #[doc = $doc:expr]
         $(#[doc = $additional_doc:expr])*
+        $(#[include_args_in_completion($include_args_in_completion:expr)])?
         #[examples($($example_str:expr),+ $(,)?)]
         $(#[$($attr:tt)*])*
         fn $fn_name:ident( $($params:tt)* ) { $($body:tt)* }
     ) => {{
         let params_list = params_list!($($params)*);
+
+        // Default to `true`
+        let include_args_in_completion = [$($include_args_in_completion, )? true][0];
+
         $crate::formulas::functions::FormulaFunction {
             name: stringify!($fn_name),
-            arg_completion: $crate::formulas::params::arg_completion_string(&params_list),
+            arg_completion: include_args_in_completion.then(|| {
+                $crate::formulas::params::arg_completion_string(&params_list)
+            }),
             usage: $crate::formulas::params::usage_string(&params_list),
             examples: &[$($example_str),+],
             doc: concat!($doc $(, "\n", $additional_doc)*),
