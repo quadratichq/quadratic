5fd422fef2c0addab18cfc9a61be1c06c9db47d8
Jim Nielsen <jimniels@gmail.com>
2023-07-03 22:53:13 -0600

    Update BottomBar.tsx

--- src/ui/menus/BottomBar/BottomBar.tsx
+++ src/ui/menus/BottomBar/BottomBar.tsx
@@ -11,19 +11,71 @@ import { debugShowCacheFlag, debugShowFPS, debugShowRenderer, debugShowCacheCoun
 import { Sheet } from '../../../grid/sheet/Sheet';
 import { editorInteractionStateAtom } from '../../../atoms/editorInteractionStateAtom';
 import { ChatBubbleOutline } from '@mui/icons-material';
+import { runFormula } from '../../../grid/computations/formulas/runFormula';
+import { getColumnA1Notation, getRowA1Notation } from '../../../gridGL/UI/gridHeadings/getA1Notation';
 
 interface Props {
   sheet: Sheet;
+}
+
+interface Output {
+  sum: string;
+  avg: string;
+  count: string;
 }
 
 export const BottomBar = (props: Props) => {
   const [interactionState] = useRecoilState(gridInteractionStateAtom);
   const [editorInteractionState, setEditorInteractionState] = useRecoilState(editorInteractionStateAtom);
   const [selectedCell, setSelectedCell] = useState<Cell | undefined>();
+  const [output, setOutput] = useState<Output | null>(null);
+  const { sheet } = props;
 
   // Generate string describing cursor location
   const cursorPositionString = `(${interactionState.cursorPosition.x}, ${interactionState.cursorPosition.y})`;
   const multiCursorPositionString = `(${interactionState.multiCursorPosition.originPosition.x}, ${interactionState.multiCursorPosition.originPosition.y}), (${interactionState.multiCursorPosition.terminalPosition.x}, ${interactionState.multiCursorPosition.terminalPosition.y})`;
+
+  useEffect(() => {
+    const { showMultiCursor, multiCursorPosition } = interactionState;
+    if (showMultiCursor) {
+      const runCalculationOnActiveSelection = async () => {
+        const { originPosition, terminalPosition } = multiCursorPosition;
+
+        // Don't bother if we don't have at least two cells with values
+        const cells = sheet.grid.getNakedCells(
+          originPosition.x,
+          originPosition.y,
+          terminalPosition.x,
+          terminalPosition.y
+        );
+        if (cells.length < 2) {
+          setOutput(null);
+          return;
+        }
+
+        // Run a formula on the selected cells
+        const colStart = getColumnA1Notation(originPosition.x);
+        const rowStart = getRowA1Notation(originPosition.y);
+        const colEnd = getColumnA1Notation(terminalPosition.x);
+        const rowEnd = getRowA1Notation(terminalPosition.y);
+        const range = `${colStart}${rowStart}:${colEnd}${rowEnd}`;
+
+        // TODO set to nearby but not selected cell
+        const pos = { x: 0, y: 0 };
+        const sum = await runFormula(`SUM(${range})`, pos);
+        const avg = await runFormula(`AVERAGE(${range})`, pos);
+        const count = await runFormula(`COUNT(${range})`, pos);
+        if (sum.success && avg.success && count.success) {
+          // @ts-expect-error
+          setOutput({ sum: sum.output_value, avg: avg.output_value, count: count.output_value });
+        } else {
+          // TODO log it?
+          setOutput(null);
+        }
+      };
+      runCalculationOnActiveSelection();
+    }
+  }, [interactionState, sheet.grid]);
 
   useEffect(() => {
     const updateCellData = async () => {
@@ -92,9 +144,18 @@ export const BottomBar = (props: Props) => {
           Cursor: {cursorPositionString}
         </span>
         {interactionState.showMultiCursor && (
-          <span style={{ cursor: 'pointer' }} onClick={handleShowGoToMenu}>
-            Selection: {multiCursorPositionString}
-          </span>
+          <>
+            <span style={{ cursor: 'pointer' }} onClick={handleShowGoToMenu}>
+              Selection: {multiCursorPositionString}
+            </span>
+            {output && (
+              <>
+                <span>Sum: {output.sum}</span>
+                <span>Avg.: {output.avg}</span>
+                <span>Count: {output.count}</span>
+              </>
+            )}
+          </>
         )}
         {selectedCell?.last_modified && (
           <span>You, {formatDistance(Date.parse(selectedCell.last_modified), new Date(), { addSuffix: true })}</span>
