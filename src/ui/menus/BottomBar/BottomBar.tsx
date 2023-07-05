de7861f694ef9afbb908b0a65c0bf3e7dc6c3b85
Jim Nielsen <jimniels@gmail.com>
2023-07-05 17:03:40 -0600

    Update BottomBar.tsx

--- src/ui/menus/BottomBar/BottomBar.tsx
+++ src/ui/menus/BottomBar/BottomBar.tsx
@@ -21,37 +21,43 @@ interface Props {
 interface Output {
   sum: string;
   avg: string;
-  count: string;
 }
 
 export const BottomBar = (props: Props) => {
   const [interactionState] = useRecoilState(gridInteractionStateAtom);
   const [editorInteractionState, setEditorInteractionState] = useRecoilState(editorInteractionStateAtom);
   const [selectedCell, setSelectedCell] = useState<Cell | undefined>();
-  const [output, setOutput] = useState<Output | null>(null);
+  const [output, setOutput] = useState<Output | undefined>();
   const { sheet } = props;
+  const {
+    showMultiCursor,
+    cursorPosition,
+    multiCursorPosition: { originPosition, terminalPosition },
+  } = interactionState;
 
   // Generate string describing cursor location
-  const cursorPositionString = `(${interactionState.cursorPosition.x}, ${interactionState.cursorPosition.y})`;
-  const multiCursorPositionString = `(${interactionState.multiCursorPosition.originPosition.x}, ${interactionState.multiCursorPosition.originPosition.y}), (${interactionState.multiCursorPosition.terminalPosition.x}, ${interactionState.multiCursorPosition.terminalPosition.y})`;
+  const cursorPositionString = `(${cursorPosition.x}, ${cursorPosition.y})`;
+  const multiCursorPositionString = `(${originPosition.x}, ${originPosition.y}), (${terminalPosition.x}, ${terminalPosition.y})`;
+
+  // Get the number of cells with values
+  const count = sheet.grid.getNakedCells(
+    originPosition.x,
+    originPosition.y,
+    terminalPosition.x,
+    terminalPosition.y
+  ).length;
 
   useEffect(() => {
-    const { showMultiCursor, multiCursorPosition } = interactionState;
     if (showMultiCursor) {
       const runCalculationOnActiveSelection = async () => {
-        const { originPosition, terminalPosition } = multiCursorPosition;
-
         // Don't bother if we don't have at least two cells with values
-        const cells = sheet.grid.getNakedCells(
-          originPosition.x,
-          originPosition.y,
-          terminalPosition.x,
-          terminalPosition.y
-        );
-        if (cells.length < 2) {
-          setOutput(null);
+        if (count < 2) {
+          setOutput(undefined);
           return;
         }
+
+        // TODO only run the formulas if there are at least two with numbers
+        // at which point we can expect there to be a result
 
         // Run a formula on the selected cells
         const colStart = getColumnA1Notation(originPosition.x);
@@ -59,23 +65,21 @@ export const BottomBar = (props: Props) => {
         const colEnd = getColumnA1Notation(terminalPosition.x);
         const rowEnd = getRowA1Notation(terminalPosition.y);
         const range = `${colStart}${rowStart}:${colEnd}${rowEnd}`;
+        const pos = { x: originPosition.x - 1, y: originPosition.y - 1 };
+        const sum = await runFormula(`IF(COUNT(${range}) >= 1, SUM(${range}), "")`, pos);
+        const avg = await runFormula(`IF(COUNT(${range}) >= 1, AVERAGE(${range}), "")`, pos);
 
-        // TODO set to nearby but not selected cell
-        const pos = { x: 0, y: 0 };
-        const sum = await runFormula(`SUM(${range})`, pos);
-        const avg = await runFormula(`AVERAGE(${range})`, pos);
-        const count = await runFormula(`COUNT(${range})`, pos);
-        if (sum.success && avg.success && count.success) {
-          // @ts-expect-error
-          setOutput({ sum: sum.output_value, avg: avg.output_value, count: count.output_value });
+        if (sum.success && sum.output_value && avg.success && avg.output_value) {
+          setOutput({ sum: sum.output_value, avg: avg.output_value });
         } else {
-          // TODO log it?
-          setOutput(null);
+          // TODO This shouldn't trigger, it's an unexpected error
+          console.log('formula message. sum: %s, avg: %s', sum.error_msg, avg.error_msg);
+          setOutput(undefined);
         }
       };
       runCalculationOnActiveSelection();
     }
-  }, [interactionState, sheet.grid]);
+  }, [count, originPosition, showMultiCursor, terminalPosition]);
 
   useEffect(() => {
     const updateCellData = async () => {
@@ -148,11 +152,11 @@ export const BottomBar = (props: Props) => {
             <span style={{ cursor: 'pointer' }} onClick={handleShowGoToMenu}>
               Selection: {multiCursorPositionString}
             </span>
-            {output && (
+            {count >= 2 && (
               <>
-                <span>Sum: {output.sum}</span>
-                <span>Avg: {output.avg}</span>
-                <span>Count: {output.count}</span>
+                <span>Count: {count}</span>
+                {output && output.sum && <span>Sum: {output.sum}</span>}
+                {output && output.avg && <span>Avg: {output.avg}</span>}
               </>
             )}
           </>
