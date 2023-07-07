28e96b68c14be65d873be667597ccf8a025ea863
Jim Nielsen <jimniels@gmail.com>
2023-07-07 11:56:49 -0600

    Update AnalyticsProvider.tsx

--- src/quadratic/AnalyticsProvider.tsx
+++ src/quadratic/AnalyticsProvider.tsx
@@ -56,7 +56,7 @@ const loadMixPanelAnalytics = async (user: User | undefined) => {
     return;
   }
 
-  mixpanel.init(process.env.REACT_APP_MIXPANEL_ANALYTICS_KEY, { api_host: "https://mixpanel-proxy.quadratichq.com" });
+  mixpanel.init(process.env.REACT_APP_MIXPANEL_ANALYTICS_KEY, { api_host: 'https://mixpanel-proxy.quadratichq.com' });
 
   mixpanel.register({
     email: user?.email,
