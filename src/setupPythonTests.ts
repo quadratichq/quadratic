38f87d1d97e549993ffccfaa58882c138350ba22
David Figatner <david@yopeyopey.com>
2023-07-09 15:57:03 +0800

    tweak

--- src/setupPythonTests.ts
+++ src/setupPythonTests.ts
@@ -4,7 +4,7 @@ import define_run_python from './web-workers/pythonWebWorker/run_python.py';
 
 export async function setupPython() {
   const pyodide = await loadPyodide();
-  pyodide.registerJsModule('GetCellsDB', GetCellsDB);
+  await pyodide.registerJsModule('GetCellsDB', GetCellsDB);
   await pyodide.loadPackage(['numpy', 'pandas', 'micropip']);
 
   // define a global py function called run_python used to run code from cells
