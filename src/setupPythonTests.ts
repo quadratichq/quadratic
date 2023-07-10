bdb35487772b5422cad44b5d85779c87ff19cafe
David Figatner <david@yopeyopey.com>
2023-07-11 06:19:42 +0800

    adding debug flags

--- src/setupPythonTests.ts
+++ src/setupPythonTests.ts
@@ -3,11 +3,16 @@ import { GetCellsDB } from './grid/sheet/Cells/GetCellsDB';
 import define_run_python from './web-workers/pythonWebWorker/run_python.py';
 
 export async function setupPython() {
-  const pyodide = await loadPyodide();
-  await pyodide.registerJsModule('GetCellsDB', GetCellsDB);
+  console.log('**** starting load of pyodide');
+  const pyodide = (global as any).pyodide ?? (await loadPyodide());
+  (global as any).pyodide = pyodide;
+  console.log('**** registering getcells');
+  pyodide.registerJsModule('GetCellsDB', GetCellsDB);
+  console.log('*** loading packages....');
   await pyodide.loadPackage(['numpy', 'pandas', 'micropip']);
 
   // define a global py function called run_python used to run code from cells
   await pyodide.runPython(define_run_python);
+  console.log('*** after runpython');
   return pyodide;
 }
--- src/web-workers/pythonWebWorker/__mocks__/PythonWebWorker.ts
+++ src/web-workers/pythonWebWorker/__mocks__/PythonWebWorker.ts
@@ -9,7 +9,9 @@ export class PythonWebWorker {
   }
 
   async run(python: string): Promise<PythonReturnType> {
+    console.log('*** before run_python');
     const output = await this.pyodide.globals.get('run_python')(python);
+    console.log('*** after run_python');
     return Object.fromEntries(output.toJs()) as PythonReturnType;
   }
 }
