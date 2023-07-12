0878291c69c3796300806172d98ceaf7391c5b71
David Figatner <david@yopeyopey.com>
2023-07-12 13:07:27 +0800

    trying with require

--- src/setupPythonTests.ts
+++ src/setupPythonTests.ts
@@ -1,13 +1,13 @@
-import { loadPyodide } from 'pyodide';
 import { GetCellsDB } from './grid/sheet/Cells/GetCellsDB';
 import define_run_python from './web-workers/pythonWebWorker/run_python.py';
 
 export async function setupPython() {
+  const { loadPyodide } = require('pyodide');
   const pyodide = await loadPyodide();
   pyodide.registerJsModule('GetCellsDB', GetCellsDB);
   await pyodide.loadPackage(['numpy', 'pandas', 'micropip']);
 
-  // define a global py function called run_python used to run code from cells
+  // // define a global py function called run_python used to run code from cells
   await pyodide.runPython(define_run_python);
   return pyodide;
 }
