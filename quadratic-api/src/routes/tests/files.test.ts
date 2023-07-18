a2b1d85fb1e9324fbc8c6001ed0e613ef45cbaf7
David Kircos <davidkircos@gmail.com>
2023-07-18 17:39:20 -0600

    add multiple users to mocked testing, test second user with no files

--- quadratic-api/src/routes/tests/files.test.ts
+++ quadratic-api/src/routes/tests/files.test.ts
@@ -29,12 +29,15 @@ afterAll(async () => {
   await dbClient.$transaction([deleteFiles, deleteUsers]);
 });
 
+// For auth we expect the following Authorization header format:
+// Bearer ValidToken {user.sub}
 jest.mock('../../middleware/auth', () => {
   return {
     validateAccessToken: jest.fn().mockImplementation(async (req: JWTRequest, res: Response, next: NextFunction) => {
-      if (req.headers.authorization === 'Bearer ValidToken') {
+      // expected format is `Bearer ValidToken {user.sub}`
+      if (req.headers.authorization?.substring(0, 17) === 'Bearer ValidToken') {
         req.auth = {
-          sub: 'test_user_1',
+          sub: req.headers.authorization?.substring(18), // Extract user.sub from the Authorization header
         };
         return next();
       } else {
@@ -56,15 +59,29 @@ describe('GET /v0/files/ no auth', () => {
   });
 });
 
-describe('GET /v0/files/ with auth', () => {
+describe('GET /v0/files/ with auth and files', () => {
   it('responds with json', async () => {
     const res = await request(app)
       .get('/v0/files/')
       .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken`)
+      .set('Authorization', `Bearer ValidToken test_user_1`)
       .expect('Content-Type', /json/)
       .expect(200); // OK
 
     expect(res.body.length).toEqual(1);
+    expect(res.body[0]).toMatchObject({ name: 'test_file_1' });
+  });
+});
+
+describe('GET /v0/files/ with auth and no files', () => {
+  it('responds with json', async () => {
+    const res = await request(app)
+      .get('/v0/files/')
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ValidToken test_user_2`)
+      .expect('Content-Type', /json/)
+      .expect(200); // OK
+
+    expect(res.body.length).toEqual(0);
   });
 });
