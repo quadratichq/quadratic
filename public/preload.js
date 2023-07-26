8ef76093007d09018c5d0d99bf57b337ae152b6c
Juan <jmbercoff@gmail.com>
2023-07-26 19:00:19 -0300

    send message

--- public/preload.js
+++ public/preload.js
@@ -2,4 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   maximizeCurrentWindow: () => ipcRenderer.send('maximize-current-window'),
+  editorHasUnsavedChanges: (hasUnsavedChanges, where) =>
+    ipcRenderer.send('has-unsaved-changes', hasUnsavedChanges, where),
 });
