c183307ae536e1322cdd5d20f236f20515408bd5
Juan <jmbercoff@gmail.com>
2023-07-26 18:59:56 -0300

    sendToElectron

--- src/hooks/useAlertOnUnsavedChanges.ts
+++ src/hooks/useAlertOnUnsavedChanges.ts
@@ -5,13 +5,23 @@ function beforeUnloadListener(event: Event) {
   event.returnValue = false;
 }
 
-export default function useAlertOnUnsavedChanges(hasUnsavedChanges: boolean) {
+function sendToElectronHasUnsavedChanges(hasUnsavedChanges: boolean) {
+  //@ts-expect-error
+  if (window.electronAPI) window.electronAPI.editorHasUnsavedChanges(hasUnsavedChanges);
+}
+
+export default function useAlertOnUnsavedChanges(hasUnsavedChanges: boolean | undefined) {
   useEffect(() => {
     if (hasUnsavedChanges) {
       window.addEventListener('beforeunload', beforeUnloadListener);
+      sendToElectronHasUnsavedChanges(true);
       return;
     }
     window.removeEventListener('beforeunload', beforeUnloadListener);
-    return () => window.removeEventListener('beforeunload', beforeUnloadListener);
+    sendToElectronHasUnsavedChanges(false);
+    return () => {
+      window.removeEventListener('beforeunload', beforeUnloadListener);
+      sendToElectronHasUnsavedChanges(false);
+    };
   }, [hasUnsavedChanges]);
 }
