9cb84c27bd42054e058095f55b30fc16c57fb9f4
Juan <jmbercoff@gmail.com>
2023-07-26 19:02:34 -0300

    add listener for unsavedchanges

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const url = require('url');
 
-const { app, BrowserWindow, ipcMain } = require('electron');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+require('update-electron-app')();
+
+let editorHasUnsavedChanges = false;
 
 function createWindow() {
   // Create the browser window.
@@ -20,9 +23,29 @@ function createWindow() {
 
   // and load the index.html of the app.
   // win.loadFile("index.html");
-  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  const startUrl = !app.isPackaged
+    ? 'http://localhost:3000'
+    : url.format({
+        pathname: path.join(__dirname, '../build/index.html'),
+        protocol: 'file',
+        slashes: true,
+      });
+  win.loadURL(startUrl);
+
+  win.webContents.on('will-prevent-unload', (event) => {
+    if (!editorHasUnsavedChanges) return;
+    const options = {
+      type: 'question',
+      buttons: ['Cancel', 'Leave'],
+      message: 'Leave Site?',
+      detail: 'Changes that you made may not be saved.',
+    };
+    const response = dialog.showMessageBoxSync(null, options);
+    if (response === 1) event.preventDefault();
+  });
+
   // Open the DevTools.
-  if (isDev) {
+  if (!app.isPackaged) {
     win.webContents.openDevTools({ mode: 'detach' });
   }
 }
@@ -30,7 +53,15 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -38,12 +69,6 @@ app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
   }
 });
 
@@ -58,4 +83,8 @@ ipcMain.on('maximize-current-window', (event) => {
   } else {
     win.unmaximize();
   }
+});
+
+ipcMain.on('has-unsaved-changes', (_, hasUnsavedChanges) => {
+  editorHasUnsavedChanges = hasUnsavedChanges;
 });
