597753540e0c0d64d0017878390b4219372fae86
David Kircos <davidkircos@gmail.com>
2023-07-27 13:13:17 -0600

    add websocket server to api

--- quadratic-api/src/server.ts
+++ quadratic-api/src/server.ts
@@ -1,7 +1,32 @@
+import { createServer, Server as HTTPServer } from 'http';
+import { Server as WebSocketServer } from 'ws';
 import { app } from './app';
+const server: HTTPServer = createServer(app);
+const wss: WebSocketServer = new WebSocketServer({ server });
+const setupWSConnection = require('./websocket/utils').setupWSConnection;
+
+// Handle WebSocket connections here
+wss.on('connection', setupWSConnection);
+server.on('upgrade', (request, socket, head) => {
+  // You may check auth of request here..
+  // See https://github.com/websockets/ws#client-authentication
+
+  // const handleAuth = (ws: any) => {
+  //   wss.emit('connection', ws, request);
+  // };
+  // wss.handleUpgrade(request, socket, head, handleAuth);
+
+  if (request.url === '/ws') {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
+});
 
 // Start the server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- src/presence/y.ts
+++ src/presence/y.ts
@@ -1,11 +1,14 @@
 import { WebsocketProvider } from 'y-websocket';
 import * as Y from 'yjs';
 
+// replace http(s) with ws(s) in process.env.REACT_APP_QUADRATIC_API_URL
+const WS_URL = process.env.REACT_APP_QUADRATIC_API_URL?.replace('http', 'ws');
+
 // Create the shared doc
 export const doc = new Y.Doc();
 
 // Create a websocket provider
-export const provider = new WebsocketProvider('wss://demos.yjs.dev', 'demo-room-3', doc);
+export const provider = new WebsocketProvider(`${WS_URL}/ws`, 'demo-room-4', doc);
 
 // Export the provider's awareness API
 export const awareness = provider.awareness;
