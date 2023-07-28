a102626578300f9f6f87ea2adf7fe4629ff264e3
Juan <jmbercoff@gmail.com>
2023-07-28 16:00:01 -0300

    add new types

--- src/web-workers/pythonWebWorker/pythonTypes.ts
+++ src/web-workers/pythonWebWorker/pythonTypes.ts
@@ -6,12 +6,25 @@ export interface PythonReturnType {
   input_python_stack_trace: string;
   input_python_std_out: string;
   output_value: string | null;
+  output_type: string | null | undefined;
   array_output: ArrayOutput;
   formatted_code: string;
 }
 
+export type InspectPythonReturnType =
+  | {
+      lineno: number;
+      col_offset: number;
+      end_lineno: number;
+      end_col_offset: number;
+      value_type?: string;
+    }
+  | {
+      error: string;
+    };
+
 export interface PythonMessage {
-  type: 'results' | 'execute' | 'not-loaded' | 'get-cells' | 'python-loaded' | 'python-error';
+  type: 'results' | 'execute' | 'not-loaded' | 'get-cells' | 'python-loaded' | 'python-error' | 'inspect-python';
   python?: string;
   results?: PythonReturnType;
   error?: string;
