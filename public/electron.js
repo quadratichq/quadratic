b08e5c57b0f592f1e33b897f2bdf70ea91c4f9f3
Juan <jmbercoff@gmail.com>
2023-08-02 18:54:17 -0300

    add events

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,21 @@
 const path = require('path');
 const url = require('url');
 
-const { app, BrowserWindow, dialog, ipcMain } = require('electron');
-require('update-electron-app')();
+const { app, BrowserWindow, dialog, ipcMain, autoUpdater } = require('electron');
+
+const server = 'https://your-deployment-url.com';
+const feedUrl = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+autoUpdater.setFeedURL({ feedUrl });
+
+const CHECK_FOR_UPDATES_FROM_FEED_INTERVAL = // in milliseconds
+  1000 * // milliseconds per second
+  60 * // seconds per minute
+  10; // check every 10 minutes
+
+setInterval(() => {
+  autoUpdater.checkForUpdates();
+}, CHECK_FOR_UPDATES_FROM_FEED_INTERVAL);
 
 let editorHasUnsavedChanges = false;
 
@@ -87,4 +100,24 @@ ipcMain.on('maximize-current-window', (event) => {
 
 ipcMain.on('has-unsaved-changes', (_, hasUnsavedChanges) => {
   editorHasUnsavedChanges = hasUnsavedChanges;
+});
+
+// autoUpdater events
+autoUpdater.on('update-downloaded', (_, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.',
+  };
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
+});
+
+autoUpdater.on('error', (message) => {
+  console.error('There was a problem updating the application');
+  console.error(message);
 });
