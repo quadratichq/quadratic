95a6793a69604a32a75c2826b1f2537b8b808927
Jim Nielsen <jimniels@gmail.com>
2023-08-04 22:35:30 -0600

    ShowAfter component

--- src/ui/menus/BottomBar/SyncState.tsx
+++ src/ui/menus/BottomBar/SyncState.tsx
@@ -1,29 +1,30 @@
 import { ErrorOutline } from '@mui/icons-material';
 import { CircularProgress, useTheme } from '@mui/material';
+import ShowAfter from 'shared/ShowAfter';
 import { useFile } from 'ui/contexts/File';
 
 export default function SyncState() {
   const theme = useTheme();
   const { syncState } = useFile();
 
-  // TODO only render if the state for this has changed for longer than X milliseconds
-
   if (syncState === 'idle') {
     return null;
   }
 
   return (
-    <span style={{ display: 'flex', alignItems: 'center', gap: theme.spacing(0.5) }}>
-      {syncState === 'error' && (
-        <>
-          <ErrorOutline style={{ color: 'red' }} fontSize="inherit" /> Sync error
-        </>
-      )}
-      {syncState === 'syncing' && (
-        <>
-          <CircularProgress size="0.5rem" /> Syncing…
-        </>
-      )}
-    </span>
+    <ShowAfter delay={300}>
+      <span style={{ display: 'flex', alignItems: 'center', gap: theme.spacing(0.5) }}>
+        {syncState === 'error' && (
+          <>
+            <ErrorOutline style={{ color: 'red' }} fontSize="inherit" /> Sync error
+          </>
+        )}
+        {syncState === 'syncing' && (
+          <>
+            <CircularProgress size="0.5rem" /> Syncing…
+          </>
+        )}
+      </span>
+    </ShowAfter>
   );
 }
