ffbe1847fe310bd90dfe492ed64fb1513519f3f1
David Figatner <david@yopeyopey.com>
2023-08-08 07:59:22 -0700

    reverting to performance.now() control

--- src/gridGL/cells/cellsLabels/createCellsSheet.ts
+++ src/gridGL/cells/cellsLabels/createCellsSheet.ts
@@ -4,7 +4,9 @@ import { CellsSheet } from '../CellsSheet';
 import { CellsHashBounds, sheetHashHeight, sheetHashWidth } from '../CellsTypes';
 
 // number of meshes to measure per frame (since MAXIMUM_FRAME_TIME is limited by the coarseness in performance.now())
-const meshesPerFrame = 3;
+// const meshesPerFrame = 3;
+
+const MAXIMUM_FRAME_TIME = 1000 / 15;
 
 // async populating of a CellsSheet
 class CreateCellsSheet {
@@ -41,7 +43,8 @@ class CreateCellsSheet {
     return this.cellsSheet.sheet;
   }
 
-  private nextHash = (): void => {
+  private nextHash = (time?: number): void => {
+    time = time || performance.now();
     if (
       this.cellsSheet === undefined ||
       this.x === undefined ||
@@ -50,31 +53,34 @@ class CreateCellsSheet {
     ) {
       throw new Error('Expected variables to be defined in createCellsSheet.next');
     }
-    for (let i = 0; i < meshesPerFrame; i++) {
-      const rect = new Rectangle(
-        this.x * sheetHashWidth,
-        this.y * sheetHashHeight,
-        sheetHashWidth - 1,
-        sheetHashHeight - 1
-      );
-      const cells = this.sheet.grid.getCellList(rect);
-      const background = this.sheet.grid.getCellBackground(rect);
-      if (cells.length || background.length) {
-        this.cellsSheet.addHash(this.x, this.y, cells, background);
-      }
-      this.x++;
-      if (this.x > this.hashBounds.xEnd) {
-        this.x = this.hashBounds.xStart;
-        this.y++;
+    const rect = new Rectangle(
+      this.x * sheetHashWidth,
+      this.y * sheetHashHeight,
+      sheetHashWidth - 1,
+      sheetHashHeight - 1
+    );
+    const cells = this.sheet.grid.getCellList(rect);
+    const background = this.sheet.grid.getCellBackground(rect);
+    if (cells.length || background.length) {
+      this.cellsSheet.addHash(this.x, this.y, cells, background);
+    }
+    this.x++;
+    if (this.x > this.hashBounds.xEnd) {
+      this.x = this.hashBounds.xStart;
+      this.y++;
 
-        // start clipping when we've populated all cellHashes
-        if (this.y > this.hashBounds.yEnd) {
-          this.timeout = window.setTimeout(this.clip);
-          return;
-        }
+      // start clipping when we've populated all cellHashes
+      if (this.y > this.hashBounds.yEnd) {
+        this.timeout = window.setTimeout(this.clip);
+        return;
       }
     }
-    this.timeout = window.setTimeout(this.nextHash);
+    const now = performance.now();
+    if (now - time < MAXIMUM_FRAME_TIME) {
+      this.nextHash(time);
+    } else {
+      this.timeout = window.setTimeout(this.nextHash, 0);
+    }
   };
 
   private clip = () => {
