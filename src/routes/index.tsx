ae825b2574a4bc3dae4437098a0d67d4be290f53
Jim Nielsen <jimniels@gmail.com>
2023-08-10 15:26:31 -0600

    small fix to login/logout history

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -131,11 +131,17 @@ const router = createBrowserRouter(
       <Route
         path={ROUTES.LOGIN_RESULT}
         loader={async () => {
-          await authClient.handleSigninRedirect();
-          let isAuthenticated = await authClient.isAuthenticated();
-          if (isAuthenticated) {
-            let redirectTo = new URLSearchParams(window.location.search).get('redirectTo') || '/';
-            return redirect(redirectTo);
+          // try/catch here handles case where this _could_ error out and we
+          // have no errorElement so we just redirect back to home
+          try {
+            await authClient.handleSigninRedirect();
+            let isAuthenticated = await authClient.isAuthenticated();
+            if (isAuthenticated) {
+              let redirectTo = new URLSearchParams(window.location.search).get('redirectTo') || '/';
+              return redirect(redirectTo);
+            }
+          } catch (e) {
+            console.error(e);
           }
           return redirect('/');
         }}
