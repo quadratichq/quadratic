fb0d9c6cbcae7d63ee3c12f8610b37c461129b35
David Figatner <david@yopeyopey.com>
2023-08-13 07:38:27 -0700

    added cheapCull to markers and array

--- src/gridGL/cells/CellsMarker.ts
+++ src/gridGL/cells/CellsMarker.ts
@@ -1,30 +1,44 @@
-import { Container, Sprite, Texture } from 'pixi.js';
+import { Container, Rectangle, Sprite, Texture } from 'pixi.js';
 import { CodeCellLanguage } from '../../quadratic-core/types';
 import { colors } from '../../theme/colors';
+import { intersects } from '../helpers/intersects';
 
 export type CellsMarkerTypes = 'CodeIcon' | 'FormulaIcon' | 'AIIcon' | 'ErrorIcon';
 
+interface Marker {
+  sprite: Sprite;
+  rectangle: Rectangle;
+}
+
 export class CellsMarkers extends Container {
+  private markers: Marker[] = [];
+
   clear() {
     this.removeChildren();
+    this.markers = [];
+  }
+
+  cheapCull(bounds: Rectangle): void {
+    this.markers.forEach((marker) => (marker.sprite.visible = intersects.rectangleRectangle(bounds, marker.rectangle)));
   }
 
   add(x: number, y: number, type: CodeCellLanguage, error?: boolean): void {
     const child = this.addChild(new Sprite());
     child.height = 4;
     child.width = 4;
-
+    child.position.set(x + 1.25, y + 1.25);
     if (type === 'Python') {
-      child.position.set(x + 1.25, y + 1.25);
       child.texture = Texture.from('images/python-icon.png');
       child.tint = colors.cellColorUserPython;
-      child.width = child.height = 4;
     } else if (type === 'Formula') {
-      child.position.set(x + 1.25, y + 1.25);
       child.texture = Texture.from('images/formula-fx-icon.png');
       child.tint = colors.cellColorUserFormula;
-      child.width = child.height = 4;
     }
+
+    this.markers.push({
+      sprite: child,
+      rectangle: new Rectangle(child.x, child.y, 4, 4),
+    });
 
     // todo
     // else if (type === 'AIIcon') {
