bb3c2ae40ae6a79b6752209a2bb403ff97b81b0c
David Figatner <david@yopeyopey.com>
2023-08-13 09:16:03 -0700

    moving all npm run commands to npm for build:ci

--- scripts/run-ci-build.sh
+++ scripts/run-ci-build.sh
@@ -4,9 +4,3 @@ source "$HOME/.cargo/env"
 
 echo 'Installing wasm-pack...'
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-
-echo 'Building wasm...'
-npm run build:wasm
-
-echo 'Building front-end...'
-npm run build
