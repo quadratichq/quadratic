f41048ed27170dcd01a2e08b45d99f879662c227
David Figatner <david@yopeyopey.com>
2023-08-14 03:58:55 -0700

    changing order of compilation :(

--- scripts/run-ci-build.sh
+++ scripts/run-ci-build.sh
@@ -5,16 +5,15 @@ source "$HOME/.cargo/env"
 echo 'Installing wasm-pack...'
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
-echo 'Exporting TS/Rust types...'
 cd quadratic-core
-cargo run --bin export_types
 
 echo 'Building wasm...'
 wasm-pack build --target web --out-dir ../src/quadratic-core
-wasm-pack build --target nodejs --out-dir ../src/quadratic-core/__mocks__
-ls ../src/quadratic-core
+
+echo 'Exporting TS/Rust types...'
+cargo run --bin export_types
+
+cd ..
 
 echo 'Building front-end...'
-cd ..
-ls
 npm run build
