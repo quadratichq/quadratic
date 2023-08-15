2b9be1b0b663c0c7715f4303323e4c2d182890e0
David Kircos <davidkircos@gmail.com>
2023-08-15 13:12:15 -0600

    fix typo

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -43,5 +43,5 @@ export const EXAMPLE_FILES: { [id: string]: { name: string; description: string 
     description: 'Example with calculations from formulas and Python.',
   },
 };
-export const CSV_IMPORT_MESSAGE = 'Drag and drop a CSV id on the grid to import it.';
+export const CSV_IMPORT_MESSAGE = 'Drag and drop a CSV file on the grid to import it.';
 export const PNG_MESSAGE = 'Copied selection as PNG to clipboard';
