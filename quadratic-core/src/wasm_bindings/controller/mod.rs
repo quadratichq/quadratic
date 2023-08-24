9ea963c1b3bb9d87c05966fc746daa1e5c99e308
HactarCE <6060305+HactarCE@users.noreply.github.com>
2023-08-24 10:06:57 -0400

    Refactor `GridController` wasm bindings

---
+++ quadratic-core/src/wasm_bindings/controller/mod.rs
@@ -0,0 +1,134 @@
+use super::*;
+use crate::{controller::TransactionSummary, grid::js_types::*};
+use std::str::FromStr;
+
+pub mod formatting;
+pub mod sheets;
+
+#[wasm_bindgen]
+impl GridController {
+    /// Imports a [`GridController`] from a file (that fits the schema defined in JS).
+    #[wasm_bindgen(js_name = "newFromFile")]
+    pub fn js_new_from_file(file: JsValue) -> Result<GridController, JsValue> {
+        Ok(GridController::from_grid(Grid::from_legacy(
+            &serde_wasm_bindgen::from_value(file)?,
+        )?))
+    }
+
+    /// Exports a [`GridController`] to a file. Returns a `GridFile` (fits JS schema).
+    #[wasm_bindgen(js_name = "exportToFile")]
+    pub fn js_export_to_file(&self) -> Result<JsValue, JsValue> {
+        Ok(serde_wasm_bindgen::to_value(
+            &self.grid().to_legacy_file_format(),
+        )?)
+    }
+
+    /// Constructs a new empty grid.
+    #[wasm_bindgen(constructor)]
+    pub fn js_new() -> Self {
+        Self::new()
+    }
+
+    /// Returns whether there is a transaction to undo.
+    #[wasm_bindgen(js_name = "hasUndo")]
+    pub fn js_has_undo(&self) -> bool {
+        self.has_undo()
+    }
+    /// Returns whether there is a transaction to redo.
+    #[wasm_bindgen(js_name = "hasRedo")]
+    pub fn js_has_redo(&self) -> bool {
+        self.has_redo()
+    }
+
+    /// Undoes one transaction. Returns a [`TransactionSummary`], or `null` if
+    /// there was nothing to undo.
+    #[wasm_bindgen(js_name = "undo")]
+    pub fn js_undo(&mut self, cursor: Option<String>) -> Result<JsValue, JsValue> {
+        Ok(serde_wasm_bindgen::to_value(&self.undo(cursor))?)
+    }
+    /// Redoes one transaction. Returns a [`TransactionSummary`], or `null` if
+    /// there was nothing to redo.
+    #[wasm_bindgen(js_name = "redo")]
+    pub fn js_redo(&mut self, cursor: Option<String>) -> Result<JsValue, JsValue> {
+        Ok(serde_wasm_bindgen::to_value(&self.redo(cursor))?)
+    }
+
+    /// Populates a portion of a sheet with random float values.
+    ///
+    /// Returns a [`TransactionSummary`].
+    #[wasm_bindgen(js_name = "populateWithRandomFloats")]
+    pub fn js_populate_with_random_floats(
+        &mut self,
+        sheet_id: String,
+        region: &Rect,
+    ) -> Result<JsValue, JsValue> {
+        let sheet_id = SheetId::from_str(&sheet_id).unwrap();
+        self.populate_with_random_floats(sheet_id, region);
+        Ok(serde_wasm_bindgen::to_value(&TransactionSummary {
+            cell_regions_modified: vec![(sheet_id, *region)],
+            fill_sheets_modified: vec![],
+            border_sheets_modified: vec![],
+            code_cells_modified: vec![],
+            sheet_list_modified: false,
+            cursor: None,
+        })?)
+    }
+
+    /// Returns a sheet's bounds.
+    #[wasm_bindgen(js_name = "getGridBounds")]
+    pub fn get_grid_bounds(
+        &self,
+        sheet_id: String,
+        ignore_formatting: bool,
+    ) -> Result<JsValue, JsValue> {
+        let sheet_id = SheetId::from_str(&sheet_id).unwrap();
+
+        Ok(serde_wasm_bindgen::to_value(
+            &self.sheet(sheet_id).bounds(ignore_formatting),
+        )?)
+    }
+
+    /// Sets a cell value given as a [`CellValue`].
+    ///
+    /// Returns a [`TransactionSummary`].
+    #[wasm_bindgen(js_name = "setCellValue")]
+    pub fn js_set_cell_value(
+        &mut self,
+        sheet_id: String,
+        pos: &Pos,
+        cell_value: JsValue,
+        cursor: Option<String>,
+    ) -> Result<JsValue, JsValue> {
+        let sheet_id = SheetId::from_str(&sheet_id).unwrap();
+        let cell_value: CellValue = serde_wasm_bindgen::from_value(cell_value)?;
+        Ok(serde_wasm_bindgen::to_value(
+            &self.set_cell_value(sheet_id, *pos, cell_value, cursor),
+        )?)
+    }
+
+    /// Deletes a region of cells.
+    ///
+    /// Returns a [`TransactionSummary`].
+    #[wasm_bindgen(js_name = "deleteCellValues")]
+    pub fn js_delete_cell_values(
+        &mut self,
+        sheet_id: String,
+        region: &Rect,
+        cursor: Option<String>,
+    ) -> Result<JsValue, JsValue> {
+        let sheet_id = SheetId::from_str(&sheet_id).unwrap();
+        Ok(serde_wasm_bindgen::to_value(
+            &self.delete_cell_values(sheet_id, *region, cursor),
+        )?)
+    }
+
+    /// Returns a code cell as a [`CodeCellValue`].
+    #[wasm_bindgen(js_name = "getCodeCellValue")]
+    pub fn get_code_cell_value(&mut self, sheet_id: String, pos: &Pos) -> Result<JsValue, JsValue> {
+        let sheet_id = SheetId::from_str(&sheet_id).unwrap();
+        match self.sheet(sheet_id).get_code_cell(*pos) {
+            Some(code_cell) => Ok(serde_wasm_bindgen::to_value(&code_cell)?),
+            None => Ok(JsValue::UNDEFINED),
+        }
+    }
+}
