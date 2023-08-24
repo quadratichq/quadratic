fb5e66d3d6e2a8f224872ff0e642d043bdde90d0
David Kircos <davidkircos@gmail.com>
2023-08-24 09:22:40 -0600

    load fonts in loadAssets file

--- src/gridGL/loadAssets.ts
+++ src/gridGL/loadAssets.ts
@@ -1,13 +1,26 @@
+import FontFaceObserver from 'fontfaceobserver';
 import { Loader } from 'pixi.js';
 import { createBorderTypes } from './dashedTextures';
+
+function loadFont(fontName: string): void {
+  const font = new FontFaceObserver(fontName);
+  font.load();
+}
 
 export function loadAssets(): Promise<void> {
   return new Promise((resolve) => {
     createBorderTypes();
+    // Load HTML fonts for Input
+    loadFont('OpenSans');
+    loadFont('OpenSans-Bold');
+    loadFont('OpenSans-Italic');
+    loadFont('OpenSans-BoldItalic');
+    // Load PixiJS fonts for canvas
     Loader.shared.add('OpenSans', '/fonts/opensans/OpenSans.fnt');
     Loader.shared.add('OpenSans-Bold', '/fonts/opensans/OpenSans-Bold.fnt');
     Loader.shared.add('OpenSans-Italic', '/fonts/opensans/OpenSans-Italic.fnt');
     Loader.shared.add('OpenSans-BoldItalic', '/fonts/opensans/OpenSans-BoldItalic.fnt');
+    // Wait until pixi fonts are loaded before resolving
     Loader.shared.load(() => resolve());
   });
 }
