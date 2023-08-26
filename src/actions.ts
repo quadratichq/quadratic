9392598487baf771d3cce053b580aca62a98fd14
Jim Nielsen <jimniels@gmail.com>
2023-08-26 17:46:55 -0600

    Update actions.ts

--- src/actions.ts
+++ src/actions.ts
@@ -1,15 +1,77 @@
 /*
 
-Usage:
+Option 1: only share label + permissions and leave actions to individual spots in code
 
-import { renameFile } from "./actions";
-
-const MyComponent() {
-  <div>
-    {renameFile.permissions.includes(permissions) && 
-      <Button onClick={() => setState()}>{renameFile.label}</Button>}
-  </div>
+```
+// actions.ts
+const deleteFile = {
+  label: "Delete",
+  permissions: ["OWNER"]
 }
+
+// Component.tsx
+import { deleteFile } from "./actions";
+const MyComponent = () => {
+  const { permission } = useRecoilValue(editorInteractionStateAtom);
+  return (
+    <div>
+      {deleteFile.permissions.includes(permissions) && 
+        <Button onClick={() => {}}>{deleteFile.label}</Button>}
+    </div>
+  );
+}
+```
+
+Option 2: share label + permissions + action via a context
+
+```
+// ActionProvider.ts
+const ActionContext = createContext({});
+const ActionProvider = (props) => {
+  const { permission } = props.initialFile;
+
+  const deleteFile = useMemo(() => ({
+    label: 'Delete',
+    isAvailable: permission === 'OWNER,
+    run: (uuid) => {
+      if (window.confirm("Please confirm you want to delete this file")) {
+        try {
+          await apiClient.deleteFile();
+          window.location.href = "/files/mine"
+        } catch(e) {
+          addGlobalSnackbar("Failed to delete file. Try again.", { severity: 'error' });
+        }
+      }
+    } 
+  }), [permission]);
+
+  // more actions defined here
+
+  return (
+    <ActionContext.Provider value={{ renameFile }}>
+      {props.children}
+    </ActionContext.Provider>
+  );
+}
+export const useActionContext = () => useContext(ActionContext);
+
+
+// Then in whatever component you want to use these actions.
+// Note: using these necessitate being inside a component, since you need `useActionContext`
+const MyComponent = () => {
+  const { deleteFile } = useActionContext();
+  const { uuid } = useParams();
+
+  return (
+    <div>
+      {deleteFile.isAvailable && 
+        <Button onClick={() => deleteFile.run(uuid)}>
+          {deleteFile.label}
+        </Button>
+      }
+  );
+}
+```
 
 */
 
