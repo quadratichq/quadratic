71976649e1619e4fff01e9c406113569e65ae319
David Kircos <davidkircos@gmail.com>
2023-08-30 11:57:38 -0600

    merge

--- quadratic-api/src/routes/files/getFilePermissions.ts
+++ quadratic-api/src/routes/files/getFilePermissions.ts
@@ -1,8 +1,12 @@
 import { File, User } from '@prisma/client';
 
-export type FILE_PERMISSION = 'OWNER' | 'VIEWER' | 'EDITOR';
+export type FILE_PERMISSION = 'OWNER' | 'VIEWER' | 'EDITOR' | 'ANONYMOUS';
 
 export const getFilePermissions = (user: User | undefined, file: File): FILE_PERMISSION => {
+  if (!user) {
+    return 'ANONYMOUS';
+  }
+
   if (file.ownerUserId === user?.id) {
     return 'OWNER';
   }
