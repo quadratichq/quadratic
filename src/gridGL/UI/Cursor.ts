304621b4598bf0555c81c9e46d298b626179585d
Jim Nielsen <jimniels@gmail.com>
2023-09-01 11:07:05 -0600

    fix: hide autocomplete indicator for non-editors (#678)

--- src/gridGL/UI/Cursor.ts
+++ src/gridGL/UI/Cursor.ts
@@ -1,5 +1,6 @@
 import { Graphics, Rectangle } from 'pixi.js';
 import { isMobile } from 'react-device-detect';
+import { isEditorOrAbove } from '../../actions';
 import { convertColorStringToTint } from '../../helpers/convertColor';
 import { getCellFromFormulaNotation, isCellRangeTypeGuard } from '../../helpers/formulaNotation';
 import { colors } from '../../theme/colors';
@@ -52,7 +53,9 @@ export class Cursor extends Graphics {
     const editor_selected_cell = editorInteractionState.selectedCell;
 
     // draw cursor but leave room for cursor indicator if needed
-    const indicatorSize = Math.max(INDICATOR_SIZE / viewport.scale.x, 4);
+    const indicatorSize = isEditorOrAbove(this.app.settings.editorInteractionState.permission)
+      ? Math.max(INDICATOR_SIZE / viewport.scale.x, 4)
+      : 0;
     this.indicator.width = this.indicator.height = indicatorSize;
     const indicatorPadding = Math.max(INDICATOR_PADDING / viewport.scale.x, 1);
     const terminalPosition = settings.interactionState.multiCursorPosition.terminalPosition;
