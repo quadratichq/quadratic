c10f34ec729c78eca83e6c29eb3f7b3dec100df1
David Figatner <david@yopeyopey.com>
2023-09-11 06:21:43 -0700

    second attempt at correct dir for export_types

--- quadratic-core/src/bin/export_types.rs
+++ quadratic-core/src/bin/export_types.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use quadratic_core::{controller::transactions, *};
 use ts_rs::TS;
 
@@ -60,5 +62,11 @@ fn main() {
         Span,
     );
 
-    std::fs::write("../src/quadratic-core/types.d.ts", s).expect("failed to write types file");
+    let Ok(mut dir) = env::current_dir() else {
+        panic!("export_types did not find the correct path.");
+    };
+    dir.push("..");
+    dir.push("quadratic-core");
+    dir.push("types.d.ts");
+    std::fs::write(dir, s).expect("failed to write types file");
 }
