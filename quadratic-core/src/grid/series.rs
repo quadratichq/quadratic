6de40d02a70c78a41d8e5011b57e13435d8198ed
David DiMaria <info@collectivesessions.com>
2023-09-20 18:08:33 -0600

    Ugly conversion of text series functions, with most tests passing

--- quadratic-core/src/grid/series.rs
+++ quadratic-core/src/grid/series.rs
@@ -3,21 +3,21 @@ use bigdecimal::{BigDecimal, Zero};
 
 use crate::CellValue;
 
-const ALPHABET_LOWER: [&str; 26] = [
+const ALPHABET_LOWER: [&'static str; 26] = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
     "t", "u", "v", "w", "x", "y", "z",
 ];
 
-const ALPHABET_UPPER: [&str; 26] = [
+const ALPHABET_UPPER: [&'static str; 26] = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
     "T", "U", "V", "W", "X", "Y", "Z",
 ];
 
-const MONTHS_SHORT: [&str; 12] = [
+const MONTHS_SHORT: [&'static str; 12] = [
     "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
 ];
 
-const MONTHS_FULL: [&str; 12] = [
+const MONTHS_FULL: [&'static str; 12] = [
     "January",
     "February",
     "March",
@@ -32,7 +32,7 @@ const MONTHS_FULL: [&str; 12] = [
     "December",
 ];
 
-const MONTHS_FULL_UPPER: [&str; 12] = [
+const MONTHS_FULL_UPPER: [&'static str; 12] = [
     "JANUARY",
     "FEBRUARY",
     "MARCH",
@@ -47,10 +47,10 @@ const MONTHS_FULL_UPPER: [&str; 12] = [
     "DECEMBER",
 ];
 
-const DAYS_SHORT: [&str; 7] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-const DAYS_SHORT_UPPER: [&str; 7] = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+const DAYS_SHORT: [&'static str; 7] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const DAYS_SHORT_UPPER: [&'static str; 7] = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
 
-const DAYS_FULL: [&str; 7] = [
+const DAYS_FULL: [&'static str; 7] = [
     "Monday",
     "Tuesday",
     "Wednesday",
@@ -60,7 +60,7 @@ const DAYS_FULL: [&str; 7] = [
     "Sunday",
 ];
 
-const DAYS_FULL_UPPER: [&str; 7] = [
+const DAYS_FULL_UPPER: [&'static str; 7] = [
     "MONDAY",
     "TUESDAY",
     "WEDNESDAY",
@@ -210,10 +210,115 @@ pub fn find_number_series(options: SeriesOptions) -> Vec<CellValue> {
     return results;
 }
 
+pub fn find_string_series(options: SeriesOptions) -> Vec<CellValue> {
+    let mut results: Vec<CellValue> = vec![];
+    let SeriesOptions {
+        ref series,
+        spaces,
+        negative,
+    } = options;
+    let text_series: Vec<Vec<&str>> = vec![
+        ALPHABET_LOWER.into(),
+        ALPHABET_UPPER.into(),
+        MONTHS_SHORT.into(),
+        MONTHS_FULL.into(),
+        MONTHS_FULL_UPPER.into(),
+        DAYS_SHORT.into(),
+        DAYS_SHORT_UPPER.into(),
+        DAYS_FULL.into(),
+        DAYS_FULL_UPPER.into(),
+    ];
+
+    let mut possible_text_series = text_series
+        .iter()
+        .map(|_| vec![])
+        .collect::<Vec<Vec<CellValue>>>();
+
+    series.iter().for_each(|cell| {
+        text_series.iter().enumerate().for_each(|(i, text_series)| {
+            // TODO(ddimaria): change to if let Some
+            let cell_value = match cell {
+                CellValue::Text(s) => s,
+                _ => "",
+            };
+
+            // if possible_text_series[i].is_some() {
+            if !is_series_key(cell_value, text_series) {
+                possible_text_series[i] = vec![];
+            } else if possible_text_series[i].len() == 0 {
+                possible_text_series[i] = vec![cell.to_owned()];
+            } else if is_series_next_key(
+                cell_value,
+                &possible_text_series[i]
+                    .iter()
+                    .map(|s| match s {
+                        CellValue::Text(s) => s,
+                        _ => "",
+                    })
+                    .collect(),
+                text_series,
+            )
+            .unwrap()
+            {
+                possible_text_series[i].push(cell.to_owned());
+            } else {
+                possible_text_series[i] = vec![];
+            }
+            // }
+        });
+    });
+
+    possible_text_series.iter().enumerate().for_each(|(i, s)| {
+        let entry = possible_text_series[i].clone();
+        if entry.len() > 0 {
+            if negative {
+                let string_list = entry;
+                let current = string_list[0].clone();
+                let cell_value = match current {
+                    CellValue::Text(current) => current,
+                    _ => "".into(),
+                };
+
+                (0..spaces).for_each(|_| {
+                    let next =
+                        get_series_next_key(&cell_value, text_series[i].clone(), true).unwrap();
+                    results.push(CellValue::Text(next));
+                });
+
+                results.reverse();
+            } else {
+                let string_list = entry;
+                let current = string_list[string_list.len() - 1].clone();
+                let mut cell_value = match current {
+                    CellValue::Text(current) => current,
+                    _ => "".into(),
+                };
+
+                (0..spaces).for_each(|_| {
+                    let next = get_series_next_key(&cell_value, text_series[i].clone(), false)
+                        .unwrap_or_else(|e| {
+                            println!("next: {:?}", e);
+                            "".into()
+                        });
+
+                    results.push(CellValue::Text(next.clone()));
+                    cell_value = next;
+                });
+            }
+        }
+    });
+
+    if results.len() > 0 {
+        return results;
+    }
+
+    // no case found
+    copy_series(options)
+}
+
 pub fn find_auto_complete(options: SeriesOptions) -> Vec<CellValue> {
     // if cells are missing, just copy series
     if options.series.iter().all(|s| *s == CellValue::Blank) {
-        println!("blank");
         return copy_series(options);
     }
 
@@ -225,49 +330,54 @@ pub fn find_auto_complete(options: SeriesOptions) -> Vec<CellValue> {
         return find_number_series(options);
     }
 
-    // string series
-    // return findStringSeries(options);
-    vec![]
+    find_string_series(options)
 }
 
-pub fn is_series_key(key: &str, keys: Vec<&str>) -> bool {
+pub fn is_series_key(key: &str, keys: &Vec<&str>) -> bool {
     keys.contains(&key)
 }
 
 pub fn is_series_next_key(
     key: &str,
-    existing_keys: Vec<&str>,
-    all_keys: Vec<&str>,
+    existing_keys: &Vec<&str>,
+    all_keys: &Vec<&str>,
 ) -> Result<bool> {
     let last_key = existing_keys[existing_keys.len() - 1];
     let index = all_keys
         .iter()
         .position(|val| val == &last_key)
-        .ok_or_else(|| anyhow!("Expected to find last_key in all_keys"))?;
+        .ok_or_else(|| anyhow!("Expected to find last_key in all_keys in is_series_next_key"))?;
 
     // find index of the key
     let index_next_key = all_keys
         .iter()
         .position(|val| val == &key)
         .ok_or_else(|| anyhow!("Expected to find key in all_keys"))?;
-
+    dbg!(index);
+    dbg!(all_keys.len());
+    dbg!(index_next_key);
+    dbg!((index + 1) % all_keys.len() == index_next_key);
     Ok((index + 1) % all_keys.len() == index_next_key)
+}
+
+pub fn checked_mod(n: usize, m: usize) -> usize {
+    ((n % m) + m) % m
 }
 
 pub fn get_series_next_key(last_key: &str, all_keys: Vec<&str>, negative: bool) -> Result<String> {
     let index = all_keys
         .iter()
-        .position(|val| val == &last_key)
-        .ok_or_else(|| anyhow!("Expected to find last_key in all_keys"))?;
+        .position(|val| last_key.to_string() == val.to_string())
+        .ok_or_else(|| anyhow!("Expected to find '{}' in all_keys", last_key))?;
 
     let key = match negative {
-        true => index - 1 % all_keys.len(),
-        false => index + 1 % all_keys.len(),
+        true => checked_mod(index.checked_sub(1).unwrap_or(0), all_keys.len()),
+        false => checked_mod(index + 1, all_keys.len()),
     };
 
     let next_key = all_keys
         .get(key)
-        .ok_or_else(|| anyhow!("Expected to find last_key in all_keys"))?;
+        .ok_or_else(|| anyhow!("Expected to find '{}' in all_keys", key))?;
 
     Ok(next_key.to_string())
 }
@@ -276,142 +386,194 @@ pub fn get_series_next_key(last_key: &str, all_keys: Vec<&str>, negative: bool) 
 mod tests {
     use super::*;
 
-    fn to_cell_values(values: Vec<i32>) -> Vec<CellValue> {
+    fn cell_value_number(values: Vec<i32>) -> Vec<CellValue> {
         values
             .iter()
             .map(|s| CellValue::Number(BigDecimal::from(s)))
             .collect::<Vec<CellValue>>()
     }
 
+    fn cell_value_text(values: Vec<&str>) -> Vec<CellValue> {
+        values
+            .iter()
+            .map(|s| CellValue::Text(s.to_string()))
+            .collect::<Vec<CellValue>>()
+    }
+
     #[test]
     fn find_a_number_series_positive_addition_by_one() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![1, 2, 3]),
+            series: cell_value_number(vec![1, 2, 3]),
             spaces: 4,
             negative: false,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![4, 5, 6, 7]));
+        assert_eq!(results, cell_value_number(vec![4, 5, 6, 7]));
     }
 
     #[test]
     fn find_a_number_series_positive_addition_by_two() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![2, 4, 6]),
+            series: cell_value_number(vec![2, 4, 6]),
             spaces: 4,
             negative: false,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![8, 10, 12, 14]));
+        assert_eq!(results, cell_value_number(vec![8, 10, 12, 14]));
     }
 
     #[test]
     fn find_a_number_series_positive_addition_by_minus_one() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![6, 5, 4]),
+            series: cell_value_number(vec![6, 5, 4]),
             spaces: 4,
             negative: false,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![3, 2, 1, 0]));
+        assert_eq!(results, cell_value_number(vec![3, 2, 1, 0]));
     }
 
     #[test]
     fn find_a_number_series_positive_addition_by_minus_two() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![6, 4, 2]),
+            series: cell_value_number(vec![6, 4, 2]),
             spaces: 4,
             negative: false,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![0, -2, -4, -6]));
+        assert_eq!(results, cell_value_number(vec![0, -2, -4, -6]));
     }
 
     #[test]
     fn find_a_number_series_positive_addition_by_add_one_negative() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![1, 2, 3]),
+            series: cell_value_number(vec![1, 2, 3]),
             spaces: 4,
             negative: true,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![-3, -2, -1, 0]));
+        assert_eq!(results, cell_value_number(vec![-3, -2, -1, 0]));
     }
 
     #[test]
     fn find_a_number_series_positive_addition_by_add_two_negative() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![2, 4, 6]),
+            series: cell_value_number(vec![2, 4, 6]),
             spaces: 4,
             negative: true,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![-6, -4, -2, 0]));
+        assert_eq!(results, cell_value_number(vec![-6, -4, -2, 0]));
     }
 
     #[test]
     fn find_a_number_series_positive_addition_by_minus_one_negative() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![6, 5, 4]),
+            series: cell_value_number(vec![6, 5, 4]),
             spaces: 4,
             negative: true,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![10, 9, 8, 7]));
+        assert_eq!(results, cell_value_number(vec![10, 9, 8, 7]));
     }
 
     #[test]
     fn find_a_number_series_positive_addition_by_minus_two_negative() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![6, 4, 2]),
+            series: cell_value_number(vec![6, 4, 2]),
             spaces: 4,
             negative: true,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![14, 12, 10, 8]));
+        assert_eq!(results, cell_value_number(vec![14, 12, 10, 8]));
     }
 
     #[test]
     fn find_a_number_series_positive_positive_multiplication() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![2, 4, 8]),
+            series: cell_value_number(vec![2, 4, 8]),
             spaces: 4,
             negative: false,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![16, 32, 64, 128]));
+        assert_eq!(results, cell_value_number(vec![16, 32, 64, 128]));
     }
 
     #[test]
     fn find_a_number_series_positive_descending_multiplication() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![128, 64, 32]),
+            series: cell_value_number(vec![128, 64, 32]),
             spaces: 4,
             negative: false,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![16, 8, 4, 2]));
+        assert_eq!(results, cell_value_number(vec![16, 8, 4, 2]));
     }
 
     #[test]
     fn find_a_number_series_positive_positive_multiplication_negative() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![16, 32, 64, 128]),
+            series: cell_value_number(vec![16, 32, 64, 128]),
             spaces: 4,
             negative: true,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![1, 2, 4, 8]));
+        assert_eq!(results, cell_value_number(vec![1, 2, 4, 8]));
     }
 
     #[test]
     fn find_a_number_series_positive_descending_multiplication_negative() {
         let options = SeriesOptions {
-            series: to_cell_values(vec![128, 64, 32]),
+            series: cell_value_number(vec![128, 64, 32]),
             spaces: 4,
             negative: true,
         };
         let results = find_auto_complete(options);
-        assert_eq!(results, to_cell_values(vec![2048, 1024, 512, 256]));
+        assert_eq!(results, cell_value_number(vec![2048, 1024, 512, 256]));
+    }
+
+    #[test]
+    fn find_a_text_series_lowercase_letters() {
+        let options = SeriesOptions {
+            series: cell_value_text(vec!["a", "b", "c"]),
+            spaces: 4,
+            negative: false,
+        };
+        let results = find_auto_complete(options);
+        assert_eq!(results, cell_value_text(vec!["d", "e", "f", "g"]));
+    }
+
+    #[test]
+    fn find_a_text_series_uppercase_letters() {
+        let options = SeriesOptions {
+            series: cell_value_text(vec!["A", "B", "C"]),
+            spaces: 4,
+            negative: false,
+        };
+        let results = find_auto_complete(options);
+        assert_eq!(results, cell_value_text(vec!["D", "E", "F", "G"]));
+    }
+
+    #[test]
+    fn find_a_text_series_uppercase_letters_with_wrap() {
+        let options = SeriesOptions {
+            series: cell_value_text(vec!["X", "Y", "Z"]),
+            spaces: 4,
+            negative: false,
+        };
+        let results = find_auto_complete(options);
+        assert_eq!(results, cell_value_text(vec!["A", "B", "C", "D"]));
+    }
+
+    #[test]
+    fn find_a_text_series_uppercase_letters_with_wrap_negative() {
+        let options = SeriesOptions {
+            series: cell_value_text(vec!["A", "B", "C"]),
+            spaces: 4,
+            negative: true,
+        };
+        let results = find_auto_complete(options);
+        println!("{:#?}", results);
+        assert_eq!(results, cell_value_text(vec!["W", "X", "Y", "Z"]));
     }
 }
