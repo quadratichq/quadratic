b14ef2db7875c9ea9f6ed6918ebf4dccfdf35115
David DiMaria <info@collectivesessions.com>
2023-09-26 05:46:05 -0600

    cleanup

--- quadratic-core/src/controller/import.rs
+++ quadratic-core/src/controller/import.rs
@@ -76,14 +76,7 @@ impl GridController {
 
 #[cfg(test)]
 mod tests {
-    use crate::{grid::Bold, CellValue, Rect};
-
     use super::*;
-    use tabled::{
-        builder::Builder,
-        settings::Color,
-        settings::{Modify, Style},
-    };
 
     const SIMPLE_CSV: &str = r#"city,region,country,population
 Southborough,MA,United States,9686
@@ -98,70 +91,14 @@ Springfield,OR,United States,56032
 Concord,NH,United States,42605
 "#;
 
-    fn table(grid_controller: GridController, sheet_id: SheetId, range: &Rect) {
-        let sheet = grid_controller.grid().sheet_from_id(sheet_id);
-        let mut vals = vec![];
-        let mut builder = Builder::default();
-        let columns = (range.x_range())
-            .map(|i| i.to_string())
-            .collect::<Vec<String>>();
-        let mut blank = vec!["".to_string()];
-        blank.extend(columns.clone());
-        builder.set_header(blank.into_iter());
-        let mut bolds = vec![];
-        let mut count_x = 0;
-        let mut count_y = 0;
-
-        range.y_range().for_each(|y| {
-            vals.push(y.to_string());
-            range.x_range().for_each(|x| {
-                let pos: Pos = Pos { x, y };
-
-                if sheet.get_formatting_value::<Bold>(pos).is_some() {
-                    bolds.push((count_y + 1, count_x + 1));
-                }
-
-                vals.push(
-                    sheet
-                        .get_cell_value(pos)
-                        .unwrap_or(CellValue::Blank)
-                        .to_string(),
-                );
-                count_x += 1;
-            });
-            builder.push_record(vals.clone());
-            vals.clear();
-            count_x = 0;
-            count_y += 1;
-        });
-
-        let mut table = builder.build();
-        table.with(Style::modern());
-
-        bolds.iter().for_each(|coords| {
-            table.with(
-                Modify::new((coords.0, coords.1))
-                    .with(Color::BOLD)
-                    .with(Color::FG_BRIGHT_RED),
-            );
-        });
-        println!("\nsheet: {}\n{}", sheet.id, table);
-    }
-
     #[test]
-    fn import_simple_csv() {
+    fn imports_a_simple_csv() {
         let mut grid_controller = GridController::new();
         let sheet_id = grid_controller.grid.sheets()[0].id;
         let pos = Pos { x: 0, y: 0 };
 
-        let result = grid_controller
+        grid_controller
             .import_csv(sheet_id, SIMPLE_CSV, "smapppop.csc", pos, None)
             .unwrap();
-
-        table(
-            grid_controller,
-            sheet_id,
-            &result.cell_regions_modified[0].1,
-        );
     }
 }
