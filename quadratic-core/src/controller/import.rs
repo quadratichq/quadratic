27dda0a498cd538f3f5d48fc6c62aa9e0d9dd05a
David D <info@collectivesessions.com>
2023-09-28 08:18:24 -0600

    Next import csv (#708)
    * Stub out csv import process
    
    * Import CSV from the frontend and display on the grid via rust
    
    * Include headers
    
    * Remove header/record separation
    
    * TS files for imports
    
    * Fix width issue + refactor
    
    * fix hoverover
    
    * increase server file size
    
    * Refactors
    
    * Handles csv file parse errors in rust
    
    * Capture CSV errors in Sentry
    
    * cleanup
    
    * Add in tests and test utils
    
    * Small refactors
    
    * Remove insertCsv.ts and move code directly into FileUploadWrapper
    
    * Remove test csv file
    
    * Add in error handling of failed csv imports in the UI
    
    * Send csv file uint8 bytes before sending to rust
    
    * Small perf enhancement
    
    * Fix typo and handle zero byte file to avoid division errors
    
    * Document test functions plus other PR nits
    
    ---------
    
    Co-authored-by: David Kircos <davidkircos@gmail.com>

---
+++ quadratic-core/src/controller/import.rs
@@ -0,0 +1,141 @@
+use anyhow::{anyhow, Result};
+use bigdecimal::BigDecimal;
+use smallvec::SmallVec;
+use std::str::FromStr;
+
+use crate::{
+    controller::operations::Operation, grid::SheetId, Array, ArraySize, CellValue, Pos, Rect,
+};
+
+use super::{transactions::TransactionSummary, GridController};
+
+impl GridController {
+    /// Imports a CSV file into the grid.
+    ///
+    /// Returns a [`TransactionSummary`].
+    pub fn import_csv(
+        &mut self,
+        sheet_id: SheetId,
+        file: &[u8],
+        file_name: &str,
+        insert_at: Pos,
+        cursor: Option<String>,
+    ) -> Result<TransactionSummary> {
+        let error = |message: String| anyhow!("Error parsing CSV file {}: {}", file_name, message);
+        let width = csv::ReaderBuilder::new().from_reader(file).headers()?.len() as u32;
+
+        if width == 0 {
+            return Err(error("empty files cannot be processed".into()));
+        }
+
+        let mut reader = csv::ReaderBuilder::new()
+            .has_headers(false)
+            .from_reader(file);
+
+        let values = reader
+            .records()
+            .into_iter()
+            .enumerate()
+            .map(|(index, record)| {
+                let record = record.map_err(|e| error(format!("line {}: {}", index + 1, e)))?;
+
+                // convert the record into a vector of CellValues
+                record
+                    .iter()
+                    .map(|value| {
+                        // TODO(ddimaria): Replace with a standard converter once it's in place
+                        Ok(if let Ok(number) = BigDecimal::from_str(value) {
+                            CellValue::Number(number)
+                        } else {
+                            CellValue::Text(value.into())
+                        })
+                    })
+                    .collect::<Result<SmallVec<[CellValue; 1]>>>()
+            })
+            .collect::<Result<Vec<SmallVec<[CellValue; 1]>>>>()?
+            .into_iter()
+            .flatten()
+            .collect::<SmallVec<[CellValue; 1]>>();
+
+        let height = values.len() as u32 / width;
+        let size = ArraySize::new_or_err(width, height).map_err(|e| error(e.to_string()))?;
+        let values = Array::new_row_major(size, values).map_err(|e| error(e.to_string()))?;
+        let rect = Rect::new_span(
+            insert_at,
+            Pos {
+                x: insert_at.x + (width as i64) - 1,
+                y: insert_at.y + (height as i64) - 1,
+            },
+        );
+        let region = self.region(sheet_id, rect);
+        let ops = vec![Operation::SetCellValues { region, values }];
+
+        Ok(self.transact_forward(ops, cursor))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::test_util::{assert_cell_value_row, print_table};
+
+    use super::*;
+
+    const SIMPLE_CSV: &str = r#"city,region,country,population
+Southborough,MA,United States,9686
+Northbridge,MA,United States,14061
+Westborough,MA,United States,29313
+Marlborough,MA,United States,38334
+Springfield,MA,United States,152227
+Springfield,MO,United States,150443
+Springfield,NJ,United States,14976
+Springfield,OH,United States,64325
+Springfield,OR,United States,56032
+Concord,NH,United States,42605
+"#;
+
+    #[test]
+    fn imports_a_simple_csv() {
+        let mut grid_controller = GridController::new();
+        let sheet_id = grid_controller.grid.sheets()[0].id;
+        let pos = Pos { x: -100, y: 100 };
+
+        grid_controller
+            .import_csv(sheet_id, SIMPLE_CSV.as_bytes(), "smallpop.csv", pos, None)
+            .unwrap();
+
+        print_table(
+            grid_controller.clone(),
+            sheet_id,
+            &Rect::new_span(pos, Pos { x: 3, y: 10 }),
+        );
+
+        assert_cell_value_row(
+            &grid_controller,
+            sheet_id,
+            -100,
+            -97,
+            100,
+            vec!["city", "region", "country", "population"],
+        );
+
+        assert_cell_value_row(
+            &grid_controller,
+            sheet_id,
+            -100,
+            -97,
+            110,
+            vec!["Concord", "NH", "United States", "42605"],
+        );
+    }
+
+    #[test]
+    fn errors_on_an_empty_csv() {
+        let mut grid_controller = GridController::new();
+        let sheet_id = grid_controller.grid.sheets()[0].id;
+        let pos = Pos { x: 0, y: 0 };
+
+        let result = grid_controller.import_csv(sheet_id, "".as_bytes(), "smallpop.csv", pos, None);
+        assert!(result.is_err());
+    }
+}
--- src/grid/controller/Grid.ts
+++ src/grid/controller/Grid.ts
@@ -1,5 +1,9 @@
+import * as Sentry from '@sentry/browser';
 import { Point, Rectangle } from 'pixi.js';
 import { debugMockLargeData } from '../../debugFlags';
+import { debugTimeCheck, debugTimeReset } from '../../gridGL/helpers/debugPerformance';
+import { Coordinate } from '../../gridGL/types/size';
+import { readFileAsArrayBuffer } from '../../helpers/files';
 import { GridController, MinMax, Placement, Pos, Rect as RectInternal } from '../../quadratic-core/quadratic_core';
 import {
   CellAlign,
@@ -480,6 +484,27 @@ export class Grid {
 
   //#endregion
 
+  //#region Imports
+
+  async importCsv(sheetId: string, file: File, insertAtCellLocation: Coordinate, reportError: (error: string) => void) {
+    debugTimeReset();
+    const pos = new Pos(insertAtCellLocation.x, insertAtCellLocation.y);
+    const file_bytes = await readFileAsArrayBuffer(file);
+
+    try {
+      const summary = this.gridController.importCsv(sheetId, file_bytes, file.name, pos, sheets.getCursorPosition());
+      transactionResponse(summary);
+    } catch (error) {
+      // TODO(ddimaria): standardize on how WASM formats errors for a consistent error
+      // type in the UI.
+      reportError(error as unknown as string);
+      Sentry.captureException(error);
+    }
+    debugTimeCheck(`uploading and processing csv file ${file.name}`);
+  }
+
+  //#endregion
+
   //#region column/row sizes
 
   getColumnWidth(sheetId: string, x: number): number {
@@ -556,5 +581,7 @@ export class Grid {
 
   //#endregion
 }
+
+//#end
 
 export const grid = new Grid();
