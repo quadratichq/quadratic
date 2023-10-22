36c3d13f43ef9f6b8f1ab21dd7d752e06ef6a25a
David Figatner <david@yopeyopey.com>
2023-10-22 08:18:37 -0700

    LabelMesh can handle multiple meshes

--- src/gridGL/cells/cellsLabel/LabelMesh.ts
+++ src/gridGL/cells/cellsLabel/LabelMesh.ts
@@ -1,67 +1,57 @@
 import { v4 as uuid } from 'uuid';
 
-import { BLEND_MODES, BitmapFont, Mesh, MeshGeometry, MeshMaterial, Program, Renderer, Texture } from 'pixi.js';
+import {
+  BLEND_MODES,
+  BitmapFont,
+  Container,
+  Mesh,
+  MeshGeometry,
+  MeshMaterial,
+  Program,
+  Renderer,
+  Texture,
+} from 'pixi.js';
 import { debugShowCellHashesInfo } from '../../../debugFlags';
 import * as shaderNoTint from './cellLabelShader';
 import * as shaderTint from './cellLabelShaderTint';
 
-export class LabelMesh extends Mesh {
-  id: string;
+const MAX_VERTICES = 65535;
 
-  fontName: string;
-  fontSize: number;
+class LabelMeshEntry extends Mesh {
+  private labelMesh: LabelMesh;
 
   index = 0;
   indexCount = 0;
   vertexCount = 0;
   uvsCount = 0;
-  total = 1;
-
-  hasColor: boolean;
-
-  static textureCache = new Map<number, Texture>();
 
   indices!: Uint16Array;
   vertices!: Float32Array;
   uvs!: Float32Array;
   colors!: Float32Array;
 
-  constructor(charTexture: Texture, fontName: string, fontSize: number, color: boolean) {
+  constructor(labelMesh: LabelMesh, total: number) {
     const geometry = new MeshGeometry();
-    let texture = LabelMesh.textureCache.get(charTexture.baseTexture.uid);
-    if (!texture) {
-      texture = new Texture(charTexture.baseTexture);
-      LabelMesh.textureCache.set(charTexture.baseTexture.uid, texture);
-    }
-    const shader = color ? shaderTint : shaderNoTint;
-    const material = new MeshMaterial(texture, {
+    const shader = labelMesh.hasColor ? shaderTint : shaderNoTint;
+    const material = new MeshMaterial(labelMesh.texture, {
       program: Program.from(shader.msdfVert, shader.msdfFrag),
       uniforms: { uFWidth: 0 },
     });
     super(geometry, material);
-    this.hasColor = color;
+    this.labelMesh = labelMesh;
     this.blendMode = BLEND_MODES.NORMAL_NPM;
-
-    this.id = uuid();
-    this.fontName = fontName;
-    this.fontSize = fontSize;
-  }
-
-  prepare(): void {
-    this.index = 0;
-    const total = this.total;
     this.vertices = new Float32Array(4 * 2 * total);
     this.uvs = new Float32Array(4 * 2 * total);
     this.indices = new Uint16Array(6 * total);
     this.size = 6 * total;
 
-    if (this.hasColor) {
+    if (labelMesh.hasColor) {
       this.colors = new Float32Array(4 * 4 * total);
       this.geometry.addAttribute('aColors', this.colors, 4);
     }
   }
 
-  finalize(): void {
+  finalize() {
     const vertexBuffer = this.geometry.getBuffer('aVertexPosition');
     const textureBuffer = this.geometry.getBuffer('aTextureCoord');
     const indexBuffer = this.geometry.getIndex();
@@ -74,7 +64,7 @@ export class LabelMesh extends Mesh {
     textureBuffer.update();
     indexBuffer.update();
 
-    if (this.hasColor) {
+    if (this.labelMesh.hasColor) {
       const colorBuffer = this.geometry.getBuffer('aColors');
       colorBuffer.data = this.colors;
       colorBuffer.update();
@@ -85,10 +75,77 @@ export class LabelMesh extends Mesh {
     }
   }
 
+  reduceSize(delta: number) {
+    this.size -= delta;
+  }
+
+  specialRender(renderer: Renderer, ufWidth: number) {
+    this.shader.uniforms.uFWidth = ufWidth;
+    this.render(renderer);
+  }
+}
+
+export class LabelMesh extends Container<LabelMeshEntry> {
+  static textureCache = new Map<number, Texture>();
+
+  id: string;
+  texture: Texture;
+
+  fontName: string;
+  fontSize: number;
+  hasColor: boolean;
+
+  total = 1;
+
+  private currentEntry = 0;
+
+  constructor(charTexture: Texture, fontName: string, fontSize: number, color: boolean) {
+    super();
+    let texture = LabelMesh.textureCache.get(charTexture.baseTexture.uid);
+    if (!texture) {
+      texture = new Texture(charTexture.baseTexture);
+      LabelMesh.textureCache.set(charTexture.baseTexture.uid, texture);
+    }
+    this.texture = texture;
+    this.hasColor = color;
+
+    this.id = uuid();
+    this.fontName = fontName;
+    this.fontSize = fontSize;
+  }
+
+  prepare(): void {
+    this.removeChildren();
+    this.currentEntry = 0;
+    while (this.total > 0) {
+      const size = this.total > MAX_VERTICES ? MAX_VERTICES : this.total;
+      const entry = new LabelMeshEntry(this, size);
+      this.addChild(entry);
+      this.total -= size;
+    }
+  }
+
+  getBuffer(): LabelMeshEntry {
+    const entry = this.children[this.currentEntry];
+    if (entry.index + 1 > MAX_VERTICES) {
+      console.log('too large...');
+      this.currentEntry++;
+      // this should never happen
+      if (this.currentEntry >= this.children.length) {
+        throw new Error('LabelMeshEntries out of bounds');
+      }
+    }
+    return entry;
+  }
+
+  finalize(): void {
+    this.children.forEach((entry) => entry.finalize());
+  }
+
   specialRender(renderer: Renderer, scale: number): void {
     const { distanceFieldRange, size } = BitmapFont.available[this.fontName];
     const fontScale = this.fontSize / size;
-    this.shader.uniforms.uFWidth = distanceFieldRange * fontScale * scale;
-    this.render(renderer);
+    const ufWidth = distanceFieldRange * fontScale * scale;
+    this.children.forEach((entry) => entry.specialRender(renderer, ufWidth));
   }
 }
