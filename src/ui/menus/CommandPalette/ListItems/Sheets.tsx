863f15b1907942d32a5c0fe175d328e60650d911
David Figatner <david@yopeyopey.com>
2023-10-24 12:48:23 -0700

    Next sheet palette permission (#741)
    * sheet commands are only available for editors
    
    * allow switching of sheets w/o editor permission

--- src/ui/menus/CommandPalette/ListItems/Sheets.tsx
+++ src/ui/menus/CommandPalette/ListItems/Sheets.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
+import { isEditorOrAbove } from '../../../../actions';
 import { grid } from '../../../../grid/controller/Grid';
 import { sheets } from '../../../../grid/controller/Sheets';
 import { focusGrid } from '../../../../helpers/focusGrid';
@@ -13,12 +14,14 @@ const ListItems = () => {
     const items: Commands[] = [
       {
         label: 'Sheet: Create',
+        isAvailable: isEditorOrAbove,
         Component: (props: any) => {
           return <CommandPaletteListItem {...props} action={() => sheets.createNew()} />;
         },
       },
       {
         label: 'Sheet: Delete',
+        isAvailable: isEditorOrAbove,
         Component: (props: any) => {
           return (
             <CommandPaletteListItem
@@ -35,6 +38,7 @@ const ListItems = () => {
       },
       {
         label: 'Sheet: Duplicate',
+        isAvailable: isEditorOrAbove,
         Component: (props: any) => {
           return <CommandPaletteListItem {...props} action={() => grid.duplicateSheet(sheets.sheet.id)} />;
         },
