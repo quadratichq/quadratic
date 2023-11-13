db2ab768cbf3400ccd20734ba8c354cf23c94f6a
David DiMaria <info@collectivesessions.com>
2023-11-13 09:08:42 -0700

    Add tests to cover Operation::ResizeColumn and Operation::ResizeRow

--- quadratic-core/src/controller/operations.rs
+++ quadratic-core/src/controller/operations.rs
@@ -340,29 +340,57 @@ mod tests {
         assert_eq!(gc.grid.sheets()[0].color, color);
     }
 
-    // #[test]
-    // fn test_execute_operation_resize_column() {
-    //     let mut gc = GridController::new();
-    //     let (_, column) = gc.grid_mut().sheets()[0].get_or_create_column(0);
-    //     let sheet_id = gc.sheet_ids()[0];
-    //     let color = Some("red".to_string());
-    //     let operation = Operation::ResizeColumn {
-    //         sheet_id,
-    //         column: column.id,
-    //         new_size: 100.0,
-    //     };
+    #[test]
+    fn test_execute_operation_resize_column() {
+        let mut gc = GridController::new();
+        let sheet = &mut gc.grid_mut().sheets_mut()[0];
+        let (_, column) = sheet.get_or_create_column(0);
+        let column_id = column.id.clone();
+        let sheet_id = sheet.id.clone();
+        let new_size = 100.0;
+        let operation = Operation::ResizeColumn {
+            sheet_id,
+            column: column_id,
+            new_size,
+        };
 
-    //     assert_eq!(
-    //         format!("{:?}", operation),
-    //         format!(
-    //             "SetSheetColor {{ sheet_id: SheetId {{ id: {} }}, color: Some(\"red\") }}",
-    //             sheet_id
-    //         )
-    //     );
+        assert_eq!(
+            format!("{:?}", operation),
+            format!(
+                "ResizeColumn {{ sheet_id: SheetId {{ id: {} }}, column: ColumnId {{ id: {} }}, new_size: {:.1} }}",
+                sheet_id, column_id, new_size
+            )
+        );
 
-    //     execute(&mut gc, operation);
-    //     assert_eq!(gc.grid.sheets()[0].color, color);
+        execute(&mut gc, operation);
+        let column_width = gc.grid.sheets()[0].offsets.column_width(0);
+        assert_eq!(column_width, new_size);
+    }
 
-    //     // println!("{:?}", gc.grid.sheets()[0].color);
-    // }
+    #[test]
+    fn test_execute_operation_resize_row() {
+        let mut gc = GridController::new();
+        let sheet = &mut gc.grid_mut().sheets_mut()[0];
+        let row = sheet.get_or_create_row(0);
+        let row_id = row.id.clone();
+        let sheet_id = sheet.id.clone();
+        let new_size = 100.0;
+        let operation = Operation::ResizeRow {
+            sheet_id,
+            row: row_id,
+            new_size,
+        };
+
+        assert_eq!(
+            format!("{:?}", operation),
+            format!(
+                "ResizeRow {{ sheet_id: SheetId {{ id: {} }}, row: RowId {{ id: {} }}, new_size: {:.1} }}",
+                sheet_id, row_id, new_size
+            )
+        );
+
+        execute(&mut gc, operation);
+        let row_height = gc.grid.sheets()[0].offsets.row_height(0);
+        assert_eq!(row_height, new_size);
+    }
 }
