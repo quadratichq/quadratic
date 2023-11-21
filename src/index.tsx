c1b394bb4dacd0f3f11c42a19b6e76d7f32ee27b
Jim Nielsen <jimniels@gmail.com>
2023-11-21 13:23:18 -0700

    remove CRA-specific reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ShowAfter } from './components/ShowAfter';
-// @ts-expect-error
-import reportWebVitals from './reportWebVitals';
 import { router } from './router';
 import './styles.css';
 import { QuadraticLoading } from './ui/loading/QuadraticLoading';
@@ -38,8 +36,3 @@ root.render(
     />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
