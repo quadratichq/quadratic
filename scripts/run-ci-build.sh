21619766b3a39f9d5994b1b1893864693f842345
David Kircos <davidkircos@gmail.com>
2023-11-22 12:01:20 -0700

    fix vercel

--- scripts/run-ci-build.sh
+++ scripts/run-ci-build.sh
@@ -3,6 +3,8 @@ if [ "$VERCEL_ENV" == "preview" ]; then
   echo "On preview branch. Setting VITE_QUADRATIC_API_URL to quadratic-api-dev-pr-$VERCEL_GIT_PULL_REQUEST_ID.herokuapp.com"
 fi
 
+cd ..
+
 echo 'Installing rustup...'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
@@ -13,12 +15,12 @@ curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 cd quadratic-core
 
 echo 'Building wasm...'
-wasm-pack build --target web --out-dir ../src/quadratic-core
+wasm-pack build --target web --out-dir ../quadratic-client/src/quadratic-core
 
 echo 'Exporting TS/Rust types...'
 cargo run --bin export_types
 
-cd ..
+cd ../quadratic-client
 
 echo 'Building front-end...'
 npm run build
