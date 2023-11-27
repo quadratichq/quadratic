3dfda402393ea7327d6b0c4af0a203c4036b965c
David Figatner <david@yopeyopey.com>
2023-11-27 05:46:45 -0800

    fixed the zoom issue with iframes

--- quadratic-client/src/gridGL/htmlCells/IFrameHtmlCell.tsx
+++ quadratic-client/src/gridGL/htmlCells/IFrameHtmlCell.tsx
@@ -12,57 +12,62 @@ interface Props {
 export const IFrameHtmlCell = (props: Props) => {
   const { htmlCell } = props;
 
-  const iframeRef = useCallback(
-    (node: HTMLIFrameElement | null) => {
-      if (node) {
-        node.addEventListener('load', () => {
-          if (node.contentWindow) {
-            // turn off zooming within the iframe
-            node.contentWindow.document.body.style.touchAction = 'none pan-x pan-y';
+  const divRef = useCallback(
+    (div: HTMLDivElement | null) => {
+      if (!div) return;
+      const iframe = div.childNodes[0] as HTMLIFrameElement;
+      if (!iframe) return;
 
-            // forward the wheel event to the pixi viewport and adjust its position
-            node.contentWindow.document.body.addEventListener(
-              'wheel',
-              (event) => {
-                const viewport = pixiApp.viewport;
-                const wheel = viewport.plugins.get('wheel') as Wheel | null;
-                if (!wheel) {
-                  throw new Error('Expected wheel plugin to be defined on viewport');
-                }
-                const bounding = node.getBoundingClientRect();
-                wheel.wheel(event, {
-                  x: bounding.left + event.clientX * viewport.scale.x - event.clientX,
-                  y: bounding.top + event.clientY * viewport.scale.y - event.clientY,
-                });
-                event.stopPropagation();
-                event.preventDefault();
-              },
-              { passive: false }
-            );
-            const style = window.getComputedStyle(node.contentWindow.document.body);
-            if (!htmlCell.w) {
-              node.width = (
-                node.contentWindow.document.body.scrollWidth +
-                parseInt(style.marginLeft, 10) +
-                parseInt(style.marginRight, 10)
-              ).toString();
-            } else {
-              node.width = htmlCell.w.toString();
-            }
-            if (!htmlCell.h) {
-              node.height = (
-                node.contentWindow.document.body.scrollHeight +
-                parseInt(style.marginTop, 10) +
-                parseInt(style.marginBottom, 10)
-              ).toString();
-            } else {
-              node.height = htmlCell.h.toString();
-            }
+      iframe.addEventListener('load', () => {
+        if (iframe.contentWindow) {
+          // turn off zooming within the iframe
+          iframe.contentWindow.document.body.style.touchAction = 'none pan-x pan-y';
+
+          // forward the wheel event to the pixi viewport and adjust its position
+          iframe.contentWindow.document.body.addEventListener(
+            'wheel',
+            (event) => {
+              const viewport = pixiApp.viewport;
+              const wheel = viewport.plugins.get('wheel') as Wheel | null;
+              if (!wheel) {
+                throw new Error('Expected wheel plugin to be defined on viewport');
+              }
+              const bounding = iframe.getBoundingClientRect();
+              wheel.wheel(event, {
+                x: bounding.left + event.clientX * viewport.scale.x - event.clientX,
+                y: bounding.top + event.clientY * viewport.scale.y - event.clientY,
+              });
+              event.stopPropagation();
+              event.preventDefault();
+            },
+            { passive: false }
+          );
+          const style = window.getComputedStyle(iframe.contentWindow.document.body);
+
+          // move margin to the div holding the iframe to avoid pinch-to-zoom issues
+
+          if (!htmlCell.w) {
+            iframe.width = (
+              iframe.contentWindow.document.body.scrollWidth +
+              parseInt(style.marginLeft, 10) +
+              parseInt(style.marginRight, 10)
+            ).toString();
           } else {
-            throw new Error('Expected content window to be defined on iframe');
+            iframe.width = htmlCell.w.toString();
           }
-        });
-      }
+          if (!htmlCell.h) {
+            iframe.height = (
+              iframe.contentWindow.document.body.scrollHeight +
+              parseInt(style.marginTop, 10) +
+              parseInt(style.marginBottom, 10)
+            ).toString();
+          } else {
+            iframe.height = htmlCell.h.toString();
+          }
+        } else {
+          throw new Error('Expected content window to be defined on iframe');
+        }
+      });
     },
     [htmlCell.h, htmlCell.w]
   );
@@ -70,28 +75,32 @@ export const IFrameHtmlCell = (props: Props) => {
   const offset = sheets.sheet.getCellOffsets(Number(htmlCell.x), Number(htmlCell.y));
 
   return (
-    <iframe
-      ref={iframeRef}
-      seamless
-      srcDoc={htmlCell.html}
+    <div
+      ref={divRef}
       // this is needed by HtmlCells.tsx
       data-sheet={htmlCell.sheet_id}
       // this is needed by PointerHtmlCells.ts
       data-pos={`${htmlCell.x},${htmlCell.y}`}
-      title={`HTML from ${htmlCell.x}, ${htmlCell.y}}`}
-      width={htmlCell.w ? Number(htmlCell.w) : ''}
-      height={htmlCell.h ? Number(htmlCell.h) : ''}
       className={`border border-border bg-white shadow-md`}
       style={{
         position: 'absolute',
-        pointerEvents: 'auto',
         left: offset.x,
         top: offset.y + offset.height,
-        minWidth: `${CELL_WIDTH}px`,
-        minHeight: `${CELL_HEIGHT}px`,
         boxSizing: 'border-box',
         touchAction: 'none pan-x pan-y',
       }}
-    />
+    >
+      <iframe
+        seamless
+        srcDoc={htmlCell.html}
+        title={`HTML from ${htmlCell.x}, ${htmlCell.y}}`}
+        width={htmlCell.w ? Number(htmlCell.w) : ''}
+        height={htmlCell.h ? Number(htmlCell.h) : ''}
+        style={{
+          minWidth: `${CELL_WIDTH}px`,
+          minHeight: `${CELL_HEIGHT}px`,
+        }}
+      />
+    </div>
   );
 };
