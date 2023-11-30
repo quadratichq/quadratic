4db2aee99fad4cf6df6f0ff5cf108e7b9c085625
Jim Nielsen <jimniels@gmail.com>
2023-11-30 12:19:38 -0700

    Update vite.config.js

--- quadratic-client/vite.config.js
+++ quadratic-client/vite.config.js
@@ -6,9 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
-    esbuild: {
-      exclude: ["public/pyodide/**/*"]
-    },
     build: {
       outDir: '../build',
     },
