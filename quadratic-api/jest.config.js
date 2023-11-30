76f2c49e8d2ae8e0432330b4163d7d62b581a77b
Jim Nielsen <jimniels@gmail.com>
2023-11-30 13:57:17 -0700

    fix jest api tests with docker

--- quadratic-api/jest.config.js
+++ quadratic-api/jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@quadratic-shared/(.*)$': '<rootDir>/../quadratic-shared/$1',
+  },
 };
