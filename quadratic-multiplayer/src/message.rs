1e939acdd3c73bb1515b8c62c33172e4b0057f90
David DiMaria <info@collectivesessions.com>
2023-11-30 08:36:16 -0700

    Improve NewRoom message, add MouseMove message

--- quadratic-multiplayer/src/message.rs
+++ quadratic-multiplayer/src/message.rs
@@ -1,24 +1,27 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum MessageRequest {
-    NewRoom { name: String },
+    NewRoom { user_id: Uuid, file_id: Uuid },
+    MouseMove { user_id: Uuid, x: f64, y: f64 },
 }
 
 #[derive(Serialize)]
 #[serde(tag = "type")]
 pub enum MessageResponse {
     Room { name: String },
+    MouseMove { user_id: Uuid, x: f64, y: f64 },
 }
 
 pub fn handle_message(request: MessageRequest) -> MessageResponse {
+    tracing::trace!("Handling message {:?}", request);
+
     match request {
-        MessageRequest::NewRoom { name } => {
-            tracing::info!("Handling message {name}");
-            MessageResponse::Room {
-                name: "test".into(),
-            }
-        }
+        MessageRequest::NewRoom { user_id, file_id } => MessageResponse::Room {
+            name: format!("room-{}", user_id),
+        },
+        MessageRequest::MouseMove { user_id, x, y } => MessageResponse::MouseMove { user_id, x, y },
     }
 }
