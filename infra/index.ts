c9aa54155b63e48f0cd1941516707ebc48ebc2f5
David Kircos <davidkircos@gmail.com>
2023-12-08 15:51:36 -0700

    pulumi setup full stack

--- infra/index.ts
+++ infra/index.ts
@@ -4,8 +4,9 @@ import * as fs from "fs";
 
 const size = "t2.micro";
 
-const targetGroupArn =
-  "arn:aws:elasticloadbalancing:us-west-2:896845383385:targetgroup/test-multiplayer-3/6f03de054c2c866f";
+// Define the ARN of your ACM certificate
+const certificateArn =
+  "arn:aws:acm:us-west-2:896845383385:certificate/e34f26e1-cb0e-49d1-b6da-5a4644cb3403";
 
 const ami = pulumi.output(
   aws.ec2.getAmi({
@@ -22,41 +23,95 @@ const ami = pulumi.output(
   })
 );
 
-const group = new aws.ec2.SecurityGroup(
-  "security-group-ec2-quadratic-multiplayer",
-  {
-    ingress: [
-      { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
-      { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
-    ],
-    egress: [
-      { protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] },
-    ],
-  }
-);
+const group = new aws.ec2.SecurityGroup("multiplayer-sc", {
+  ingress: [
+    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+    { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+  ],
+  egress: [
+    { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+    { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
+  ],
+});
 
 // Read the content of the Bash script
 const userData = fs.readFileSync(
   "scripts/setup-multiplayer-service.sh",
   "utf-8"
 );
-const instance = new aws.ec2.Instance("quadratic-multiplayer", {
+const instance = new aws.ec2.Instance("multiplayer-instance", {
   instanceType: size,
-  securityGroups: [group.name],
+  vpcSecurityGroupIds: [group.id],
   ami: ami.id,
   keyName: "test2",
-  // other configuration
+  // Run Setup script on instance boot
   userData: userData,
 });
 
-// Attach the instance to the target group
+// Create a new Network Load Balancer
+const nlb = new aws.lb.LoadBalancer("multiplayer-nlb", {
+  internal: false,
+  loadBalancerType: "network",
+  subnets: ["subnet-0ae50871c8ec4e68f", "subnet-0c6f318928373a253"],
+  enableCrossZoneLoadBalancing: true,
+});
+
+// Create a new Target Group
+const targetGroup = new aws.lb.TargetGroup("multiplayer-nlb-tg", {
+  port: 80,
+  protocol: "TCP",
+  targetType: "instance",
+  vpcId: "vpc-035fff213c528dbe5",
+});
+
+// Attach the instance to the new Target Group
 const targetGroupAttachment = new aws.lb.TargetGroupAttachment(
-  "attachInstance",
+  "attachInstanceToNewGroup",
   {
     targetId: instance.id,
-    targetGroupArn: targetGroupArn,
+    targetGroupArn: targetGroup.arn,
   }
 );
 
+// Create NLB Listener for TLS on port 443
+const nlbListener = new aws.lb.Listener("multiplayer-nlb-listener", {
+  loadBalancerArn: nlb.arn,
+  port: 443,
+  protocol: "TLS",
+  certificateArn: certificateArn, // Attach the SSL certificate
+  sslPolicy: "ELBSecurityPolicy-2016-08", // Choose an appropriate SSL policy
+  defaultActions: [
+    {
+      type: "forward",
+      targetGroupArn: targetGroup.arn,
+    },
+  ],
+});
+
+// Get the hosted zone ID for quadratic-preview.com
+const hostedZone = pulumi.output(
+  aws.route53.getZone(
+    {
+      name: "quadratic-preview.com",
+    },
+    { async: true }
+  )
+);
+
+// Create a Route 53 record pointing to the NLB
+const dnsRecord = new aws.route53.Record("multiplayer-r53-record", {
+  zoneId: hostedZone.id,
+  name: "wss-2.quadratic-preview.com", // subdomain you want to use
+  type: "A", // or "CNAME" if preferred
+  aliases: [
+    {
+      name: nlb.dnsName,
+      zoneId: nlb.zoneId,
+      evaluateTargetHealth: true,
+    },
+  ],
+});
+
 export const publicIp = instance.publicIp;
 export const publicDns = instance.publicDns;
+export const loadBalancerDns = nlb.dnsName;
