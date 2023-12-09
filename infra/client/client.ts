c80027a22ea53c418718626a142ba48c65c1b75a
David Kircos <davidkircos@gmail.com>
2023-12-09 00:05:05 -0700

    preview branches for client via pulumi

---
+++ infra/client/client.ts
@@ -0,0 +1,105 @@
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+
+// Assume you have a domain name, e.g., "example.com"
+const domainName = "quadratic-preview.com";
+const subdomainName = "app-preview-1";
+const cdnPriceClass = "PriceClass_100";
+
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.Bucket("client-bucket", {
+  website: {
+    indexDocument: "index.html",
+  },
+});
+
+// Create an Origin Access Identity for the CloudFront distribution
+const originAccessIdentity = new aws.cloudfront.OriginAccessIdentity(
+  "client-oai"
+);
+
+const bucketPolicy = new aws.s3.BucketPolicy("client-bucket-policy", {
+  bucket: bucket.id,
+  policy: pulumi
+    .all([bucket.id, originAccessIdentity.iamArn])
+    .apply(([bucketName, oaiArn]) =>
+      JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Effect: "Allow",
+            Principal: { AWS: oaiArn },
+            Action: "s3:GetObject",
+            Resource: `arn:aws:s3:::${bucketName}/*`,
+          },
+        ],
+      })
+    ),
+});
+
+// Create a CloudFront distribution for the S3 bucket
+const cdn = new aws.cloudfront.Distribution("client-cdn", {
+  origins: [
+    {
+      domainName: bucket.bucketRegionalDomainName,
+      originId: bucket.arn,
+      s3OriginConfig: {
+        originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath,
+      },
+    },
+  ],
+  enabled: true,
+  defaultCacheBehavior: {
+    targetOriginId: bucket.id,
+    viewerProtocolPolicy: "redirect-to-https",
+    allowedMethods: ["GET", "HEAD"],
+    cachedMethods: ["GET", "HEAD"],
+    forwardedValues: {
+      queryString: false,
+      cookies: {
+        forward: "none",
+      },
+    },
+    minTtl: 0,
+    defaultTtl: 3600,
+    maxTtl: 86400,
+  },
+  priceClass: cdnPriceClass,
+  defaultRootObject: "index.html",
+  customErrorResponses: [
+    {
+      errorCode: 404,
+      responseCode: 404,
+      responsePagePath: "/404.html",
+    },
+  ],
+  restrictions: {
+    geoRestriction: {
+      restrictionType: "none",
+    },
+  },
+  viewerCertificate: {
+    cloudfrontDefaultCertificate: true,
+  },
+});
+
+// Create or use an existing hosted zone (uncomment the appropriate line)
+// const hostedZone = new aws.route53.Zone("my-zone", { name: domainName });
+const hostedZone = aws.route53.getZone({ name: domainName });
+
+// Create a DNS record for the CloudFront distribution
+const dnsRecord = new aws.route53.Record("client-cdn-record", {
+  zoneId: hostedZone.then((hz) => hz.zoneId),
+  name: `${subdomainName}.${domainName}`,
+  type: "A",
+  aliases: [
+    {
+      name: cdn.domainName,
+      zoneId: cdn.hostedZoneId,
+      evaluateTargetHealth: true,
+    },
+  ],
+});
+
+// Export the domain name of the CloudFront distribution
+export const clientPublicDns = dnsRecord.name;
--- infra/index.ts
+++ infra/index.ts
@@ -1,124 +1,13 @@
-import * as aws from "@pulumi/aws";
-import * as pulumi from "@pulumi/pulumi";
-import * as fs from "fs";
-const config = new pulumi.Config();
+// This file is the entry point for the infra package.
 
-// Configuration
-const domain = config.get("domain") || "quadratic-preview.com"; // ex quadratic-preview.com
-const multiplayerSubdomain = config.get("subdomain") || "wss-2"; // ex ws
-const certificateArn =
-  config.get("certificate-arn") ||
-  "arn:aws:acm:us-west-2:896845383385:certificate/e34f26e1-cb0e-49d1-b6da-5a4644cb3403";
-const instanceSize = config.get("instance-size") || "t2.micro";
-const instanceKeyName = config.get("instance-key-name") || "test2";
-const subNet1 = config.get("subnet1") || "subnet-0ae50871c8ec4e68f";
-const subNet2 = config.get("subnet2") || "subnet-0c6f318928373a253";
-const vpcId = config.get("vpc-id") || "vpc-035fff213c528dbe5";
+// Infra stack for the multiplayer service.
+import {
+  multiplayerInstanceDns,
+  multiplayerPublicDns,
+} from "./multiplayer/multiplayer";
 
-const ami = pulumi.output(
-  aws.ec2.getAmi({
-    owners: ["099720109477"], // Canonical ID
-    mostRecent: true,
-    filters: [
-      {
-        name: "name",
-        values: ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"],
-      },
-      { name: "architecture", values: ["x86_64"] },
-      { name: "root-device-type", values: ["ebs"] },
-    ],
-  })
-);
+// Infra stack for the client CDN.
+import { clientPublicDns } from "./client/client";
 
-const group = new aws.ec2.SecurityGroup("multiplayer-sc", {
-  ingress: [
-    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
-    { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
-  ],
-  egress: [
-    { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
-    { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
-  ],
-});
-
-// Read the content of the Bash script
-const userData = fs.readFileSync(
-  "scripts/setup-multiplayer-service.sh",
-  "utf-8"
-);
-const instance = new aws.ec2.Instance("multiplayer-instance", {
-  instanceType: instanceSize,
-  vpcSecurityGroupIds: [group.id],
-  ami: ami.id,
-  keyName: instanceKeyName,
-  // Run Setup script on instance boot
-  userData: userData,
-});
-
-// Create a new Network Load Balancer
-const nlb = new aws.lb.LoadBalancer("multiplayer-nlb", {
-  internal: false,
-  loadBalancerType: "network",
-  subnets: [subNet1, subNet2],
-  enableCrossZoneLoadBalancing: true,
-});
-
-// Create a new Target Group
-const targetGroup = new aws.lb.TargetGroup("multiplayer-nlb-tg", {
-  port: 80,
-  protocol: "TCP",
-  targetType: "instance",
-  vpcId: vpcId,
-});
-
-// Attach the instance to the new Target Group
-const targetGroupAttachment = new aws.lb.TargetGroupAttachment(
-  "multiplayer-attach-instance-tg",
-  {
-    targetId: instance.id,
-    targetGroupArn: targetGroup.arn,
-  }
-);
-
-// Create NLB Listener for TLS on port 443
-const nlbListener = new aws.lb.Listener("multiplayer-nlb-listener", {
-  loadBalancerArn: nlb.arn,
-  port: 443,
-  protocol: "TLS",
-  certificateArn: certificateArn, // Attach the SSL certificate
-  sslPolicy: "ELBSecurityPolicy-2016-08", // Choose an appropriate SSL policy
-  defaultActions: [
-    {
-      type: "forward",
-      targetGroupArn: targetGroup.arn,
-    },
-  ],
-});
-
-// Get the hosted zone ID for quadratic-preview.com
-const hostedZone = pulumi.output(
-  aws.route53.getZone(
-    {
-      name: domain,
-    },
-    { async: true }
-  )
-);
-
-// Create a Route 53 record pointing to the NLB
-const dnsRecord = new aws.route53.Record("multiplayer-r53-record", {
-  zoneId: hostedZone.id,
-  name: `${multiplayerSubdomain}.${domain}`, // subdomain you want to use
-  type: "A",
-  aliases: [
-    {
-      name: nlb.dnsName,
-      zoneId: nlb.zoneId,
-      evaluateTargetHealth: true,
-    },
-  ],
-});
-
-export const publicIp = instance.publicIp;
-export const publicDns = instance.publicDns;
-export const loadBalancerDns = nlb.dnsName;
+// Global exports
+export { clientPublicDns, multiplayerInstanceDns, multiplayerPublicDns };
