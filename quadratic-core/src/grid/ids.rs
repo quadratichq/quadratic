848384f3dcdbf43d37fd7385653c0d21731b3d50
David Figatner <david@yopeyopey.com>
2023-12-19 08:04:02 -0500

    remove macros from ids

--- quadratic-core/src/grid/ids.rs
+++ quadratic-core/src/grid/ids.rs
@@ -10,35 +10,29 @@ use uuid::Uuid;
 #[cfg(feature = "js")]
 use wasm_bindgen::prelude::*;
 
-macro_rules! uuid_wrapper_struct {
-    ($name:ident) => {
-        #[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq, Hash)]
-        #[cfg_attr(feature = "js", wasm_bindgen, derive(ts_rs::TS))]
-        pub struct $name {
-            id: Uuid,
-        }
-
-        impl $name {
-            pub(crate) fn new() -> Self {
-                $name { id: Uuid::new_v4() }
-            }
-        }
-
-        impl FromStr for $name {
-            type Err = anyhow::Error;
-
-            fn from_str(s: &str) -> Result<Self> {
-                let id = Uuid::parse_str(s);
-                Ok($name { id: id? })
-            }
-        }
-
-        impl Display for $name {
-            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                write!(f, "{}", self.id)
-            }
-        }
-    };
+#[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "js", wasm_bindgen, derive(ts_rs::TS))]
+pub struct SheetId {
+    id: Uuid,
 }
 
-uuid_wrapper_struct!(SheetId);
+impl SheetId {
+    pub fn new() -> Self {
+        Self { id: Uuid::new_v4() }
+    }
+}
+
+impl FromStr for SheetId {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let id = Uuid::parse_str(s);
+        Ok(SheetId { id: id? })
+    }
+}
+
+impl Display for SheetId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.id)
+    }
+}
