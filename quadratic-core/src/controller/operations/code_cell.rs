c739cc9a563e4a59384a5defbe712b36dad681a5
David Figatner <david@yopeyopey.com>
2023-12-26 04:57:05 -0800

    operation/code_cell tests

--- quadratic-core/src/controller/operations/code_cell.rs
+++ quadratic-core/src/controller/operations/code_cell.rs
@@ -122,3 +122,72 @@ impl GridController {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_delete_code_cell_operations() {
+        let mut gc = GridController::default();
+        let sheet_id = gc.sheet_ids()[0];
+        let sheet = gc.grid_mut().try_sheet_mut_from_id(sheet_id).unwrap();
+        let pos = Pos { x: 0, y: 0 };
+        sheet.set_code_cell(
+            pos,
+            Some(CodeCellValue {
+                language: CodeCellLanguage::Python,
+                code_string: "print('hello world')".to_string(),
+                formatted_code_string: None,
+                output: None,
+                last_modified: date_string(),
+            }),
+        );
+
+        let operations = gc.delete_code_cell_operations(&SheetRect::single_pos(pos, sheet_id));
+        assert_eq!(operations.len(), 1);
+        assert_eq!(
+            operations[0],
+            Operation::SetCodeCell {
+                sheet_pos: pos.to_sheet_pos(sheet_id),
+                code_cell_value: None,
+            }
+        );
+    }
+
+    #[test]
+    fn test_set_code_cell_operations() {
+        let mut gc = GridController::default();
+        let sheet_id = gc.sheet_ids()[0];
+        let sheet = gc.grid_mut().try_sheet_mut_from_id(sheet_id).unwrap();
+        let pos = Pos { x: 0, y: 0 };
+        sheet.set_cell_value(pos, CellValue::Text("delete me".to_string()));
+
+        let operations = gc.set_code_cell_operations(
+            pos.to_sheet_pos(sheet_id),
+            CodeCellLanguage::Python,
+            "print('hello world')".to_string(),
+        );
+        assert_eq!(operations.len(), 2);
+        assert_eq!(
+            operations[0],
+            Operation::SetCellValues {
+                sheet_rect: SheetRect::from(pos.to_sheet_pos(sheet_id)),
+                values: Array::from(CellValue::Blank),
+            }
+        );
+        assert_eq!(
+            operations[1],
+            Operation::SetCodeCell {
+                sheet_pos: pos.to_sheet_pos(sheet_id),
+                code_cell_value: Some(CodeCellValue {
+                    language: CodeCellLanguage::Python,
+                    code_string: "print('hello world')".to_string(),
+                    formatted_code_string: None,
+                    output: None,
+                    last_modified: date_string(),
+                }),
+            }
+        );
+    }
+}
