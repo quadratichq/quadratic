63d3df470fcbec25bf5a43cc1e68d3c0ca1faf0f
David Kircos <davidkircos@gmail.com>
2024-01-02 14:48:07 -0700

    fix bash

--- infra/multiplayer/setup-multiplayer-service.sh
+++ infra/multiplayer/setup-multiplayer-service.sh
@@ -15,14 +15,14 @@ Environment="HOST=0.0.0.0"
 Environment="PORT=80"
 Environment="HEARTBEAT_CHECK_S=15"
 Environment="HEARTBEAT_TIMEOUT_S=60"
-Environment="AUTHENTICATE_JWT=false"
+Environment="AUTHENTICATE_JWT=true"
 Environment="QUADRATIC_API_URI={{QUADRATIC_API_URI}}"
 Environment="QUADRATIC_API_JWT=ADD_TOKEN_HERE"
 Environment="AUTH0_JWKS_URI=https://dev-nje7dw8s.us.auth0.com/.well-known/jwks.json"
 Environment="AWS_S3_BUCKET_NAME=quadratic-api-development"
-Environment="AWS_S3_BUCKET_REGION=us-west-2"
-Environment="AWS_S3_BUCKET_ACCESS_KEY_ID={{MULTIPLAYER_AWS_S3_ACCESS_KEY_ID}}"
-Environment="AWS_S3_BUCKET_SECRET_ACCESS_KEY={{MULTIPLAYER_AWS_S3_SECRET_ACCESS_KEY}}"
+Environment="AWS_S3_REGION=us-west-2"
+Environment="AWS_S3_ACCESS_KEY_ID={{MULTIPLAYER_AWS_S3_ACCESS_KEY_ID}}"
+Environment="AWS_S3_SECRET_ACCESS_KEY={{MULTIPLAYER_AWS_S3_SECRET_ACCESS_KEY}}"
 
 [Install]
 WantedBy=multi-user.target
