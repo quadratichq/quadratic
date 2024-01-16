9b5ae3d7bb5918a64b6a8f59af35a037d338556e
David Kircos <davidkircos@gmail.com>
2024-01-16 13:36:59 -0700

    use latest ami

--- infra/helpers/latestAmazonAmi.ts
+++ infra/helpers/latestAmazonAmi.ts
@@ -5,7 +5,7 @@ export const latestAmazonLinuxAmi = aws.ec2.getAmiOutput({
   filters: [
     {
       name: "name",
-      values: ["amzn2-ami-hvm-*-x86_64-gp2"],
+      values: ["al2023-ami-*-x86_64"],
     },
   ],
   owners: ["amazon"], // This is the AWS account ID for Amazon
