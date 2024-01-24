9331bc1ece43a378c231d1d583bd261291514a1b
David DiMaria <info@collectivesessions.com>
2024-01-24 12:36:27 -0700

    Test permissions

--- quadratic-multiplayer/src/permissions.rs
+++ quadratic-multiplayer/src/permissions.rs
@@ -47,3 +47,47 @@ pub(crate) async fn validate_user_can_edit_file(
 
     validate_can_edit_file(&user.permissions)
 }
+
+#[cfg(test)]
+pub(crate) mod tests {
+
+    use crate::test_util::setup;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn validates_can_edit_or_view_file() {
+        let roles = vec![FilePermRole::FileView, FilePermRole::FileEdit];
+        let result = validate_can_edit_or_view_file(&roles);
+        assert!(result.is_ok());
+
+        let roles = vec![FilePermRole::FileDelete];
+        let result = validate_can_edit_or_view_file(&roles);
+        assert!(matches!(result, Err(MpError::FilePermissions(_))));
+    }
+
+    #[tokio::test]
+    async fn validates_can_edit_file() {
+        let roles = vec![FilePermRole::FileView, FilePermRole::FileEdit];
+        let result = validate_can_edit_file(&roles);
+        assert!(result.is_ok());
+
+        let roles = vec![FilePermRole::FileView];
+        let result = validate_can_edit_file(&roles);
+        assert!(matches!(result, Err(MpError::FilePermissions(_))));
+    }
+
+    #[tokio::test]
+    async fn validates_user_can_edit_or_view_file() {
+        let (_, state, _, file_id, user, _) = setup().await;
+        let session_id = user.session_id;
+
+        let roles = vec![FilePermRole::FileView, FilePermRole::FileEdit];
+        let result = validate_user_can_edit_or_view_file(state.clone(), file_id, session_id).await;
+        // assert!(result.is_ok());
+
+        // let roles = vec![FilePermRole::FileDelete];
+        // let result = validate_user_can_edit_or_view_file(&roles);
+        // assert!(matches!(result, Err(MpError::FilePermissions(_))));
+    }
+}
