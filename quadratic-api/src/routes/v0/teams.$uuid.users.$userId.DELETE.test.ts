2e997dc6ed018057f876daecd617d5864b451697
Jim Nielsen <jimniels@gmail.com>
2024-02-08 13:41:16 -0700

    delete user from team

--- quadratic-api/src/routes/v0/teams.$uuid.users.$userId.DELETE.test.ts
+++ quadratic-api/src/routes/v0/teams.$uuid.users.$userId.DELETE.test.ts
@@ -1,67 +1,38 @@
 import request from 'supertest';
 import { app } from '../../app';
 import dbClient from '../../dbClient';
+import { expectError, getUserIdByAuth0Id } from '../../tests/helpers';
 
 beforeEach(async () => {
   // Create some users
-  const user_1 = await dbClient.user.upsert({
-    create: {
-      auth0Id: 'test_user_1',
-      id: 1,
-    },
-    update: {},
-    where: {
-      id: 1,
+  const user1 = await dbClient.user.create({
+    data: {
+      auth0Id: 'user1',
     },
   });
-  const user_2 = await dbClient.user.upsert({
-    create: {
-      auth0Id: 'test_user_2',
-      id: 2,
-    },
-    update: {},
-    where: {
-      id: 2,
+  const user2 = await dbClient.user.create({
+    data: {
+      auth0Id: 'user2',
     },
   });
-  const user_3 = await dbClient.user.upsert({
-    create: {
-      auth0Id: 'test_user_3',
-      id: 3,
-    },
-    update: {},
-    where: {
-      id: 3,
+  const user3 = await dbClient.user.create({
+    data: {
+      auth0Id: 'user3',
     },
   });
-  const user_4 = await dbClient.user.upsert({
-    create: {
-      auth0Id: 'test_user_4',
-      id: 4,
-    },
-    update: {},
-    where: {
-      id: 4,
+  const user4 = await dbClient.user.create({
+    data: {
+      auth0Id: 'user4',
     },
   });
-  const user_5 = await dbClient.user.upsert({
-    create: {
-      auth0Id: 'test_user_5',
-      id: 5,
-    },
-    update: {},
-    where: {
-      id: 5,
+  const user5 = await dbClient.user.create({
+    data: {
+      auth0Id: 'user5',
     },
   });
-  const user_6 = await dbClient.user.upsert({
-    create: {
-      auth0Id: 'test_user_6',
-      id: 6,
-    },
-    update: {},
-    where: {
-      id: 6,
+  const user6 = await dbClient.user.create({
+    data: {
+      auth0Id: 'user6',
     },
   });
 
@@ -73,11 +44,11 @@ beforeEach(async () => {
       UserTeamRole: {
         create: [
           {
-            userId: user_1.id,
+            userId: user1.id,
             role: 'OWNER',
           },
-          { userId: user_2.id, role: 'EDITOR' },
-          { userId: user_3.id, role: 'VIEWER' },
+          { userId: user2.id, role: 'EDITOR' },
+          { userId: user3.id, role: 'VIEWER' },
         ],
       },
     },
@@ -90,171 +61,96 @@ beforeEach(async () => {
       UserTeamRole: {
         create: [
           {
-            userId: user_1.id,
+            userId: user1.id,
             role: 'OWNER',
           },
-          { userId: user_2.id, role: 'OWNER' },
-          { userId: user_3.id, role: 'EDITOR' },
-          { userId: user_4.id, role: 'EDITOR' },
-          { userId: user_5.id, role: 'VIEWER' },
-          { userId: user_6.id, role: 'VIEWER' },
+          { userId: user2.id, role: 'OWNER' },
+          { userId: user3.id, role: 'EDITOR' },
+          { userId: user4.id, role: 'EDITOR' },
+          { userId: user5.id, role: 'VIEWER' },
+          { userId: user6.id, role: 'VIEWER' },
         ],
       },
     },
   });
-  // Create a team nobody has access to except 1
 });
 
 afterEach(async () => {
-  const deleteTeamUsers = dbClient.userTeamRole.deleteMany();
-  const deleteUsers = dbClient.user.deleteMany();
-  const deleteTeams = dbClient.team.deleteMany();
-
-  await dbClient.$transaction([deleteTeamUsers, deleteUsers, deleteTeams]);
+  await dbClient.$transaction([
+    dbClient.userTeamRole.deleteMany(),
+    dbClient.user.deleteMany(),
+    dbClient.team.deleteMany(),
+  ]);
 });
 
-describe('DELETE /v0/teams/:uuid/users/:userId - unauthenticated requests', () => {
-  it('responds with a 401', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(401);
+describe('DELETE /v0/teams/:uuid/users/:userId', () => {
+  describe('invalid request', () => {
+    it('responds with a 404 for a valid user that doesn’t exist', async () => {
+      await request(app)
+        .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/245')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ValidToken user1`)
+        .expect(404)
+        .expect(expectError);
+    });
+    it('responds with a 400 for an invalid user', async () => {
+      await request(app)
+        .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/foo')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ValidToken user1`)
+        .expect(400)
+        .expect(expectError);
+    });
   });
-});
 
-// TODO move to a teamSharing middleware?
-describe('DELETE /v0/teams/:uuid/users/:userId - invalid user', () => {
-  it('responds with a 404 for a valid user that doesn’t exist', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/245')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_1`)
-      .expect('Content-Type', /json/)
-      .expect(404);
+  describe('deleting yourself', () => {
+    it('allows a team viewer to remove themselves from a team', async () => {
+      const userId = await getUserIdByAuth0Id('user3');
+      await request(app)
+        .delete(`/v0/teams/00000000-0000-4000-8000-000000000001/users/${userId}`)
+        .set('Authorization', `Bearer ValidToken user3`)
+        .expect(200);
+    });
+    it('allows owners to remove themselves from a team IF there’s at least one other owner', async () => {
+      const userId = await getUserIdByAuth0Id('user1');
+      await request(app)
+        .delete(`/v0/teams/00000000-0000-4000-8000-000000000002/users/${userId}`)
+        .set('Authorization', `Bearer ValidToken user1`)
+        .expect(200);
+    });
+    it('does not allow owners to remove themselves from a team IF they’re the only owner', async () => {
+      const userId = await getUserIdByAuth0Id('user1');
+      await request(app)
+        .delete(`/v0/teams/00000000-0000-4000-8000-000000000001/users/${userId}`)
+        .set('Authorization', `Bearer ValidToken user1`)
+        .expect(403)
+        .expect(expectError);
+    });
   });
-  it('responds with a 400 for an invalid user', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/foo')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_1`)
-      .expect('Content-Type', /json/)
-      .expect(400);
-  });
-});
 
-describe('DELETE /v0/teams/:uuid/users/:userId - deleting yourself', () => {
-  it('allows viewers to remove themselves from a team', async () => {
-    await request(app)
-      .delete(`/v0/teams/00000000-0000-4000-8000-000000000001/users/3`)
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_3`)
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-  it('allows editors to remove themselves from a team', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/2')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_2`)
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-  it('allows owners to remove themselves from a team IF there’s at least one other owner', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000002/users/1')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_1`)
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-  it('does not allow owners to remove themselves from a team IF they’re the only one', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/1')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_1`)
-      .expect('Content-Type', /json/)
-      .expect(403);
-  });
-});
-
-describe('DELETE /v0/teams/:uuid/users/:userId - deleteing others as an owner', () => {
-  it('allows owners to remove other owners', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000002/users/2')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_1`)
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-  it('allows owners to remove editors', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/2')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_1`)
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-  it('allows owners to remove viewers', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000001/users/3')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_1`)
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-});
-
-describe('DELETE /v0/teams/:uuid/users/:userId - deleteing others as an editor', () => {
-  it('doesn’t allow editors to remove owners', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000002/users/1')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_3`)
-      .expect('Content-Type', /json/)
-      .expect(403);
-  });
-  it('allows editors to remove other editors', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000002/users/4')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_3`)
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-  it('allows editors to remove viewers', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000002/users/5')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_3`)
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-});
-
-describe('DELETE /v0/teams/:uuid/users/:userId - deleteing others as a viewer', () => {
-  it('doesn’t allow viewers to remove owners', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000002/users/1')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_5`)
-      .expect('Content-Type', /json/)
-      .expect(403);
-  });
-  it('doesn’t allow viewers to remove editors', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000002/users/3')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_5`)
-      .expect('Content-Type', /json/)
-      .expect(403);
-  });
-  it('doesn’t allow viewers to remove other viewers', async () => {
-    await request(app)
-      .delete('/v0/teams/00000000-0000-4000-8000-000000000002/users/6')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ValidToken test_user_5`)
-      .expect('Content-Type', /json/)
-      .expect(403);
+  describe('deleteing others', () => {
+    it('doesn’t allow users without sufficient permission to edit other users', async () => {
+      const userId = await getUserIdByAuth0Id('user1');
+      await request(app)
+        .delete(`/v0/teams/00000000-0000-4000-8000-000000000002/users/${userId}`)
+        .set('Authorization', `Bearer ValidToken user3`)
+        .expect(403);
+    });
+    it('rejects requests to delete a user that isn’t part of the team', async () => {
+      const userId = await getUserIdByAuth0Id('user4');
+      await request(app)
+        .delete(`/v0/teams/00000000-0000-4000-8000-000000000001/users/${userId}`)
+        .set('Authorization', `Bearer ValidToken user1`)
+        .expect(404)
+        .expect(expectError);
+    });
+    it('rejects requests for an EDITOR to delete an OWNER', async () => {
+      const userId = await getUserIdByAuth0Id('user1');
+      await request(app)
+        .delete(`/v0/teams/00000000-0000-4000-8000-000000000001/users/${userId}`)
+        .set('Authorization', `Bearer ValidToken user2`)
+        .expect(403)
+        .expect(expectError);
+    });
   });
 });
--- quadratic-api/src/routes/v0/teams.$uuid.users.$userId.DELETE.ts
+++ quadratic-api/src/routes/v0/teams.$uuid.users.$userId.DELETE.ts
@@ -5,34 +5,31 @@ import dbClient from '../../dbClient';
 import { getTeam } from '../../middleware/getTeam';
 import { userMiddleware } from '../../middleware/user';
 import { validateAccessToken } from '../../middleware/validateAccessToken';
-import { validateRequestSchema } from '../../middleware/validateRequestSchema';
+import { parseRequest } from '../../middleware/validateRequestSchema';
 import { RequestWithUser } from '../../types/Request';
+import { ApiError } from '../../utils/ApiError';
 
-export default [
-  validateRequestSchema(
-    z.object({
-      params: z.object({
-        uuid: z.string().uuid(),
-        userId: z.coerce.number(),
-      }),
-    })
-  ),
-  validateAccessToken,
-  userMiddleware,
-  handler,
-];
+export default [validateAccessToken, userMiddleware, handler];
 
-async function handler(req: Request, res: Response) {
-  const resSuccess: ApiTypes['/v0/teams/:uuid/users/:userId.DELETE.response'] = { message: 'User deleted' };
+const schema = z.object({
+  params: z.object({
+    uuid: z.string().uuid(),
+    userId: z.coerce.number(),
+  }),
+});
+
+async function handler(req: Request, res: Response<ApiTypes['/v0/teams/:uuid/users/:userId.DELETE.response']>) {
+  const {
+    params: { userId: userToDeleteId },
+  } = parseRequest(req, schema);
   const {
     user: { id: userMakingRequestId },
-    params: { userId: userIdString },
   } = req as RequestWithUser;
-  const userToDeleteId = Number(userIdString);
   const {
     team: { id: teamId },
     userMakingRequest,
   } = await getTeam({ uuid: req.params.uuid, userId: userMakingRequestId });
+  const resSuccess = { message: 'User deleted' };
 
   // Allow the user to delete themselves from a team
   if (userMakingRequestId === userToDeleteId) {
@@ -45,9 +42,7 @@ async function handler(req: Request, res: Response) {
         },
       });
       if (teamOwners.length <= 1) {
-        return res.status(403).json({
-          error: { message: 'There must be at least one owner on a team.' },
-        });
+        throw new ApiError(403, 'There must be at least one owner on a team.');
       }
     }
 
@@ -68,9 +63,7 @@ async function handler(req: Request, res: Response) {
 
   // User making the request can edit the team
   if (!userMakingRequest.permissions.includes('TEAM_EDIT')) {
-    return res.status(403).json({
-      error: { message: 'User does not have permission to edit this team' },
-    });
+    throw new ApiError(403, 'User does not have permission to edit this team');
   }
 
   // Get the user that's being deleted
@@ -84,15 +77,11 @@ async function handler(req: Request, res: Response) {
   });
   // Ensure they exist
   if (!userToDelete) {
-    return res.status(404).json({ error: { message: 'User not found' } });
+    throw new ApiError(404, 'User not found');
   }
   // And make sure they have a role equal to or lower than the deleter
   if (userMakingRequest.role === 'EDITOR' && userToDelete.role === 'OWNER') {
-    return res.status(403).json({
-      error: {
-        message: 'User does not have the ability to delete an owner',
-      },
-    });
+    throw new ApiError(403, 'User does not have the ability to delete an owner');
   }
 
   // Ok, now we're good to delete the user
