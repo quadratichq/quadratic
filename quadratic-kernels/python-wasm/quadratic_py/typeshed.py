0b8ee93da098f2727155b72182ec9eec2afd58b7
David DiMaria <info@collectivesessions.com>
2024-02-21 11:25:33 -0700

    Add more typesheds

--- quadratic-kernels/python-wasm/quadratic_py/typeshed.py
+++ quadratic-kernels/python-wasm/quadratic_py/typeshed.py
@@ -2,40 +2,33 @@
 
 import os
 import json
-import sys
 
-
-def create_typeshed_json(source_path):
+def generate_json(path):
     results = dict(files={})
     output_path = f"../dist/typeshed.json"
-    
-    print(f"{source_path} -> {output_path}")
 
-    for (source_dir, prefix) in ((source_path, "/typeshed/"), ("config", "/src/")):
-        strip_parts = len(source_dir.split(os.path.sep))
+    for (directory, prefix) in ((path, "/typeshed/"), ("config", "/src/")):
+        strip_parts = len(directory.split(os.path.sep))
        
-        for (dirpath, dirnames, filenames) in os.walk(source_dir):
-            for f in sorted(filenames):
-                if os.path.splitext(f)[1] != ".pyi":
+        for (dirpath, _, filenames) in os.walk(directory):
+            for filename in sorted(filenames):
+                if os.path.splitext(filename)[1] != ".pyi":
                     continue
 
-                source_path = os.path.join(dirpath, f)
+                path = os.path.join(dirpath, filename)
                 destination = os.path.join(
                     prefix,
-                    os.path.join(*source_path.split(os.path.sep)[strip_parts:]),
+                    os.path.join(*path.split(os.path.sep)[strip_parts:]),
                 )
        
-                print(f"  {source_path} -> {destination}")
-       
-                text = open(source_path, "r", encoding="utf-8").read()
+                text = open(path, "r", encoding="utf-8").read()
                 results["files"][destination] = text
 
-    with open(output_path, "w", encoding="utf-8") as f:
-        f.write(json.dumps(results, indent=2))
+    with open(output_path, "w", encoding="utf-8") as file:
+        file.write(json.dumps(results, indent=2))
+    
+    return os.path.realpath(output_path)
 
 if __name__ == "__main__":
-    print ('argument list', sys.argv)
-    create_typeshed_json("typeshed")
-    # for lang in os.listdir("lang"):
-    #     source_path = os.path.join("lang", lang, "typeshed")
-    #     create_typeshed_json(source_path, lang)
+    output_path = generate_json("typeshed")
+    print(f"Generated typeshed at {output_path}")
