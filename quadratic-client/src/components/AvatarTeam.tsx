9acc7130ae5f08e5d5c7955b4edf3effa3578e18
Jim Nielsen <jimniels@gmail.com>
2024-02-23 22:16:02 -0700

    Update AvatarTeam.tsx

--- quadratic-client/src/components/AvatarTeam.tsx
+++ quadratic-client/src/components/AvatarTeam.tsx
@@ -1,11 +1,12 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/shadcn/ui/avatar';
+import { cn } from '@/shadcn/utils';
 import { PersonIcon } from '@radix-ui/react-icons';
 
 export function AvatarTeam({ className, src }: { className?: string; src?: string }) {
   return (
-    <Avatar className={className}>
+    <Avatar className={cn(className, 'rounded bg-transparent')}>
       <AvatarImage src={src} />
-      <AvatarFallback className={'bg-muted text-muted-foreground'}>
+      <AvatarFallback className={'rounded bg-transparent text-muted-foreground'}>
         <PersonIcon />
         <PersonIcon className="-ml-1" />
       </AvatarFallback>
