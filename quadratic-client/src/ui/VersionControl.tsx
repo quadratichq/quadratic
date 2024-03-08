220a19281b4caf24b5fc4d8f82ff546909041b12
Jim Nielsen <jimniels@gmail.com>
2024-03-08 16:50:11 -0700

    style tweaks

--- quadratic-client/src/ui/VersionControl.tsx
+++ quadratic-client/src/ui/VersionControl.tsx
@@ -1,36 +1,37 @@
-import { Alert, Button } from '@mui/material';
+import { Type } from '@/components/Type';
+import { Button } from '@/shadcn/ui/button';
+import { RocketIcon } from '@radix-ui/react-icons';
 import { useEffect, useState } from 'react';
-import { JSX } from 'react/jsx-runtime';
-import { QuadraticSnackBar } from './components/QuadraticSnackBar';
+import { FixedBottomAlert } from './components/PermissionOverlay';
 
-export const VersionControl = (): JSX.Element | null => {
+export const VersionControl = () => {
   const [showDialog, setShowDialog] = useState<false | 'recommended' | 'required'>(false);
   useEffect(() => {
-    const needRefresh = (message: any /* { details: 'required' | 'recommended' } */) => setShowDialog(message.detail);
+    const needRefresh = (message: any /* { detail: 'required' | 'recommended' } */) => setShowDialog(message.detail);
     window.addEventListener('need-refresh', needRefresh);
     return () => {
       window.removeEventListener('need-refresh', needRefresh);
     };
   });
 
-  if (!showDialog) return null;
+  if (showDialog === false) return null;
 
   return (
-    <QuadraticSnackBar open={!!showDialog} autoHideDuration={null}>
-      <Alert severity={showDialog === 'recommended' ? 'warning' : 'error'}>
-        Quadratic has been updated. We {showDialog === 'recommended' ? 'recommend' : 'require'} you refresh your browser
-        to get the latest version.
-        <div className="mt-4 flex justify-end gap-4">
-          <Button variant="contained" onClick={() => window.location.reload()}>
-            Refresh
+    <FixedBottomAlert>
+      <div>
+        <RocketIcon className="text-muted-foreground" />
+      </div>
+      <Type className="flex-grow">
+        <strong>App update:</strong> there’s a new version available.
+      </Type>
+      <div className="flex justify-end gap-2">
+        {showDialog === 'recommended' && (
+          <Button variant="outline" onClick={() => setShowDialog(false)}>
+            Dismiss
           </Button>
-          {showDialog === 'recommended' && (
-            <Button variant="outlined" onClick={() => setShowDialog(false)}>
-              Dismiss
-            </Button>
-          )}
-        </div>
-      </Alert>
-    </QuadraticSnackBar>
+        )}
+        <Button onClick={() => window.location.reload()}>Refresh</Button>
+      </div>
+    </FixedBottomAlert>
   );
 };
