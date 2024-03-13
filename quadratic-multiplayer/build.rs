a9f6e27b3accec751407a86b4dd5849280b51073
David DiMaria <info@collectivesessions.com>
2024-03-13 12:00:03 -0600

    Load min version at startup, copy file at build time

---
+++ quadratic-multiplayer/build.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+static MIN_VERSION: &str = "updateAlertVersion.json";
+
+fn main() {
+    let out_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let source_path = Path::new(&out_dir).parent().unwrap().join(MIN_VERSION);
+    let dest_path = Path::new(&out_dir).join(MIN_VERSION);
+    println!(
+        "Copying {} to {}",
+        source_path.display(),
+        dest_path.display()
+    );
+    fs::copy(&source_path, &dest_path).unwrap();
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
