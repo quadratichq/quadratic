6a95163af6864ee68ffcf0716c372ecc938de4ea
David Figatner <david@yopeyopey.com>
2024-03-30 06:49:12 -0700

    removing console.logs

--- quadratic-client/src/web-workers/multiplayerWebWorker/multiplayer.ts
+++ quadratic-client/src/web-workers/multiplayerWebWorker/multiplayer.ts
@@ -52,7 +52,6 @@ export class Multiplayer {
     // this is only a partial solution mostly for desktop
     // see https://www.igvita.com/2015/11/20/dont-lose-user-and-app-state-use-page-visibility/ for a further discussion
     const alertUser = (e: BeforeUnloadEvent) => {
-      console.log(this.state);
       if (
         this.state === 'syncing' &&
         !this.brokenConnection &&
@@ -91,7 +90,6 @@ export class Multiplayer {
     switch (e.data.type) {
       case 'multiplayerClientState':
         this.state = e.data.state;
-        console.log(this.state);
         if (this.state === 'no internet' || this.state === 'waiting to reconnect') {
           this.clearAllUsers();
         }
