93854619085a7467c98668e9dd41d2d9cdd14d45
AyushAgrawal-A2 <ayushagl06@gmail.com>
2024-05-12 03:24:57 +0530

    fix: bug

--- quadratic-client/src/shared/shadcn/ui/textarea.tsx
+++ quadratic-client/src/shared/shadcn/ui/textarea.tsx
@@ -8,40 +8,43 @@ export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextArea
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, autoHeight, maxHeight, ...props }, ref) => {
+  ({ className, autoHeight, maxHeight, onChange, onKeyDown, ...props }, ref) => {
     const textareaRef = React.useRef<HTMLTextAreaElement>(null);
 
     React.useImperativeHandle(ref, () => textareaRef.current!);
 
-    React.useEffect(() => {
-      const textarea = textareaRef.current;
-      if (!textarea) return;
-
-      textarea.style.maxHeight = maxHeight || 'none';
-
-      const adjustHeight = () => {
-        textarea.style.height = '';
-        textarea.style.height = `${textarea.scrollHeight + 2}px`;
-      };
-
-      if (!autoHeight) return;
-
-      adjustHeight();
-
-      textarea.addEventListener('input', adjustHeight);
-
-      return () => {
-        textarea.removeEventListener('input', adjustHeight);
-      };
-    }, [autoHeight, maxHeight, textareaRef]);
+    const adjustHeight = React.useCallback(() => {
+      window.requestAnimationFrame(() => {
+        const textarea = textareaRef.current;
+        if (textarea) {
+          textarea.style.height = '';
+          textarea.style.height = `${textarea.scrollHeight + 2}px`;
+        }
+      });
+    }, [textareaRef]);
 
     return (
       <textarea
+        ref={textareaRef}
         className={cn(
-          'flex h-8 min-h-8 w-full resize-none rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-8 max-h-[120px] min-h-8 w-full resize-none rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+          `max-h-[${maxHeight || 'none'}]`,
           className
         )}
-        ref={textareaRef}
+        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+          onChange?.(event);
+          if (autoHeight) {
+            adjustHeight();
+          }
+        }}
+        onKeyDown={(event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+          if (event.key === 'Enter' && !event.shiftKey) {
+            onKeyDown?.(event);
+            if (autoHeight) {
+              adjustHeight();
+            }
+          }
+        }}
         {...props}
       />
     );
