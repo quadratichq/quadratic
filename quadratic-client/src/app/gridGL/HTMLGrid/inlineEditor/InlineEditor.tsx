56f4cc40362008d551655e15d0db287a6eb9434c
David Figatner <david@yopeyopey.com>
2024-05-16 06:00:12 -0700

    fix tooltip message

--- quadratic-client/src/app/gridGL/HTMLGrid/inlineEditor/InlineEditor.tsx
+++ quadratic-client/src/app/gridGL/HTMLGrid/inlineEditor/InlineEditor.tsx
@@ -5,9 +5,8 @@
 import { inlineEditorHandler } from '@/app/gridGL/HTMLGrid/inlineEditor/inlineEditorHandler';
 import { colors } from '@/app/theme/colors';
 import { Button } from '@/shared/shadcn/ui/button';
-import { Tooltip, TooltipContent, TooltipProvider } from '@/shared/shadcn/ui/tooltip';
 import { SubtitlesOutlined } from '@mui/icons-material';
-import { TooltipTrigger } from '@radix-ui/react-tooltip';
+import { Tooltip } from '@mui/material';
 import { useCallback } from 'react';
 
 export const InlineEditor = () => {
@@ -15,33 +14,35 @@ export const InlineEditor = () => {
     inlineEditorHandler.attach(div);
   }, []);
 
+  // Note: I switched to using material's Tooltip because radix-ui's Tooltip did
+  // not keep position during viewport changes. Even forcing a remount did not
+  // fix its positioning problem. There's probably a workaround, but it was too
+  // much work.
+
   return (
     <div ref={ref} style={{ position: 'absolute', display: 'flex', pointerEvents: 'auto', alignItems: 'center' }}>
       <div id="cell-edit"></div>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              style={{
-                position: 'absolute',
-                display: 'none',
-                alignItems: 'center',
-                borderRadius: '0',
-                padding: '0',
-                marginTop: '-0.23px',
-                width: '23px',
-                height: '23.23px',
-                right: '-23px',
-                backgroundColor: colors.languageFormula,
-              }}
-            >
-              <SubtitlesOutlined sx={{ width: '18px', height: '18px', color: 'white' }} />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent>Open Formula in multi-line code editor</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+
+      <Tooltip title="Open Formula in multi-line code editor">
+        <Button
+          className="button***"
+          variant="ghost"
+          style={{
+            position: 'absolute',
+            display: 'none',
+            alignItems: 'center',
+            borderRadius: '0',
+            padding: '0',
+            marginTop: '-0.23px',
+            width: '23px',
+            height: '23.23px',
+            right: '-23px',
+            backgroundColor: colors.languageFormula,
+          }}
+        >
+          <SubtitlesOutlined sx={{ width: '18px', height: '18px', color: 'white' }} />
+        </Button>
+      </Tooltip>
     </div>
   );
 };
