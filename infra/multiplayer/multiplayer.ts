42801a8b4b58c54b19f69aa5fa04ba4da2eb6d11
David Kircos <davidkircos@gmail.com>
2024-05-21 13:04:35 -0600

    make sure instance is launched with the correct subnet

--- infra/multiplayer/multiplayer.ts
+++ infra/multiplayer/multiplayer.ts
@@ -34,6 +34,7 @@ const instance = new aws.ec2.Instance("multiplayer-instance", {
   instanceType: instanceSize,
   iamInstanceProfile: instanceProfileIAMContainerRegistry,
   vpcSecurityGroupIds: [multiplayerEc2SecurityGroup.id],
+  subnetId: [subNet1, subNet2][Math.floor(Math.random() * 2)],
   ami: latestAmazonLinuxAmi.id,
   // Run Setup script on instance boot to create multiplayer systemd service
   userDataReplaceOnChange: true,
