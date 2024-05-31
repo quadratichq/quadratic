37026926a3cecb8a8bade9ab9d32158a1290700f
AyushAgrawal-A2 <ayushagl06@gmail.com>
2024-05-31 11:51:19 +0530

    fix: comment

--- quadratic-kernels/python-wasm/quadratic_py/utils.py
+++ quadratic-kernels/python-wasm/quadratic_py/utils.py
@@ -12,8 +12,10 @@ import pytz
 
 
 def attempt_fix_await(code: str) -> str:
-    # Wrap known async functions with "(await <function-call>)" to improve the UX
+    # Convert c((x,y), ...) cell((x,y), ...) to cells((x,y), ...)
     code = re.sub(r"([^a-zA-Z0-9_]|^)c(?:ell)?(\([^\(\)]*\([^\)]*\)(?:[^\(\)]*|\([^\)]*\))*\))", r"\1cells\2", code) # captures c((x,y), ...) cell((x,y), ...)
+    
+    # Wrap known async functions with "(await <function-call>)" to improve the UX
     code = re.sub(r"([^a-zA-Z0-9_]|^)(c(?:ells?)?\((?:[^\(\)]*|\([^\)]*\))*\))", r"\1(await \2)", code) # captures c( cell( cells(
     code = re.sub(r"([^a-zA-Z0-9_]|^)(cells\[[^\]]*\])", r"\1(await \2)", code)                         # captures cells[
     code = re.sub(r"([^a-zA-Z0-9_]|^)(getCells?\((?:[^\(\)]*|\([^\)]*\))*\))", r"\1(await \2)", code)   # captures get_cell( get_cells(
