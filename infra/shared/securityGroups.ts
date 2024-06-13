b540634489ce32952fbc4c7ea026217ae9136133
David Kircos <davidkircos@gmail.com>
2024-06-13 14:20:37 -0600

    allow ssh on connection

--- infra/shared/securityGroups.ts
+++ infra/shared/securityGroups.ts
@@ -121,4 +121,12 @@ if (isPreviewEnvironment) {
     cidrBlocks: ["0.0.0.0/0"],
     securityGroupId: multiplayerEc2SecurityGroup.id,
   });
+  new aws.ec2.SecurityGroupRule(`multiplayer-ssh-ingress-rule`, {
+    type: "ingress",
+    fromPort: 22,
+    toPort: 22,
+    protocol: "tcp",
+    cidrBlocks: ["0.0.0.0/0"],
+    securityGroupId: connectionEc2SecurityGroup.id,
+  });
 }
