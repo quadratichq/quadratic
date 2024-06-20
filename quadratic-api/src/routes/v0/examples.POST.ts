dc933eb6564190ae2b43c047c066a6445be4af51
Jim Nielsen <jimniels@gmail.com>
2024-06-20 14:59:50 -0600

    check permissions when creating example file

--- quadratic-api/src/routes/v0/examples.POST.ts
+++ quadratic-api/src/routes/v0/examples.POST.ts
@@ -22,11 +22,19 @@ async function handler(req: RequestWithUser, res: Response<ApiTypes['/v0/example
   const {
     body: { publicFileUrlInProduction, teamUuid },
   } = parseRequest(req, schema);
-  const { team } = await getTeam({ uuid: teamUuid, userId });
+  const {
+    team,
+    userMakingRequest: { permissions: teamPermissions },
+  } = await getTeam({ uuid: teamUuid, userId });
 
   // We validate that we get a UUID in the zod schema, so if we reach here
   // we know we can do this simple operation.
   const fileUuid = publicFileUrlInProduction.split('/').pop() as string;
+
+  // Make sure the user has the ability to create a file in the given team
+  if (!teamPermissions.includes('TEAM_EDIT')) {
+    throw new ApiError(403, 'You do not have permission to create a file in this team.');
+  }
 
   try {
     const apiUrl = `https://api.quadratichq.com/v0/files/${fileUuid}`;
