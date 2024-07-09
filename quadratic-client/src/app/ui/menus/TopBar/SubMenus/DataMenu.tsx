aaba24aa753ce3046bdcb2666522f0920f87cffa
Jim Nielsen <jimniels@gmail.com>
2024-07-09 15:20:52 -0600

    menu permissions

--- quadratic-client/src/app/ui/menus/TopBar/SubMenus/DataMenu.tsx
+++ quadratic-client/src/app/ui/menus/TopBar/SubMenus/DataMenu.tsx
@@ -1,8 +1,10 @@
-import { downloadSelectionAsCsvAction } from '@/app/actions';
+import { downloadSelectionAsCsvAction, isAvailableBecauseFileLocationIsAccessibleAndWriteable } from '@/app/actions';
 import { editorInteractionStateAtom } from '@/app/atoms/editorInteractionStateAtom';
 import { KeyboardSymbols } from '@/app/helpers/keyboardSymbols';
 import { useFileContext } from '@/app/ui/components/FileProvider';
 import { DataIcon } from '@/app/ui/icons';
+import { useRootRouteLoaderData } from '@/routes/_root';
+import { useFileRouteLoaderData } from '@/routes/file.$uuid';
 import { useGlobalSnackbar } from '@/shared/components/GlobalSnackbarProvider';
 import { CSV_IMPORT_MESSAGE, PARQUET_IMPORT_MESSAGE } from '@/shared/constants/appConstants';
 import { Menu, MenuDivider, MenuItem } from '@szhsin/react-menu';
@@ -15,6 +17,16 @@ export const DataMenu = () => {
   const setEditorInteractionState = useSetRecoilState(editorInteractionStateAtom);
   const { addGlobalSnackbar } = useGlobalSnackbar();
   const { name: fileName } = useFileContext();
+  const { isAuthenticated } = useRootRouteLoaderData();
+  const {
+    userMakingRequest: { filePermissions, fileTeamPrivacy, teamPermissions },
+  } = useFileRouteLoaderData();
+  const showManageConnections = isAvailableBecauseFileLocationIsAccessibleAndWriteable({
+    isAuthenticated,
+    filePermissions,
+    fileTeamPrivacy,
+    teamPermissions,
+  });
 
   return (
     <>
@@ -50,10 +62,14 @@ export const DataMenu = () => {
             secondary={KeyboardSymbols.Command + KeyboardSymbols.Shift + 'E'}
           />
         </MenuItem>
-        <MenuDivider />
-        <MenuItem onClick={() => setEditorInteractionState((prev) => ({ ...prev, showConnectionsMenu: true }))}>
-          <MenuLineItem primary="Manage connections" />
-        </MenuItem>
+        {showManageConnections && (
+          <>
+            <MenuDivider />
+            <MenuItem onClick={() => setEditorInteractionState((prev) => ({ ...prev, showConnectionsMenu: true }))}>
+              <MenuLineItem primary="Manage connections" />
+            </MenuItem>
+          </>
+        )}
       </Menu>
     </>
   );
