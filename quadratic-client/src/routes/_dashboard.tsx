13bbc8566801d599782f376bc55fc9163d518ed5
David Kircos <davidkircos@gmail.com>
2024-07-15 10:58:52 -0600

    capitalize default team team

--- quadratic-client/src/routes/_dashboard.tsx
+++ quadratic-client/src/routes/_dashboard.tsx
@@ -95,7 +95,7 @@ export const loader = async ({ params, request }: LoaderFunctionArgs): Promise<L
 
     // 4) there's no teams in the API, so create one and send the user to it
   } else if (teams.length === 0) {
-    const newTeam = await apiClient.teams.create({ name: 'My team' });
+    const newTeam = await apiClient.teams.create({ name: 'My Team' });
     return redirectDocument(ROUTES.TEAM(newTeam.uuid));
   }
 
