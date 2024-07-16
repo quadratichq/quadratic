02acf6dffe7481272dae4f5466b70a7512e52488
David Figatner <david@yopeyopey.com>
2024-07-17 05:16:47 +0800

    fix transaction loop for connections

--- quadratic-core/src/controller/execution/control_transaction.rs
+++ quadratic-core/src/controller/execution/control_transaction.rs
@@ -127,6 +127,7 @@ impl GridController {
         Ok(())
     }
 
+    // todo: this should be moved to after_calculation_async() so it's similar to Python and JS
     /// Externally called when an async connection completes
     pub fn connection_complete(
         &mut self,
@@ -167,10 +168,10 @@ impl GridController {
                 cells_accessed: transaction.cells_accessed.clone(),
             };
 
-            self.start_transaction(&mut transaction);
             self.finalize_code_run(&mut transaction, current_sheet_pos, Some(code_run), None);
             transaction.waiting_for_async = None;
-            self.finalize_transaction(&mut transaction);
+
+            self.start_transaction(&mut transaction);
         }
 
         Ok(())
