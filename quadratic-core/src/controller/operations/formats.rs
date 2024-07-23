f8609898bffa3a447b1312e4f429d0cfe3b6ccd9
David Figatner <david@yopeyopey.com>
2024-07-23 06:26:47 -0700

    fixes clear format for borders (only in rects)

--- quadratic-core/src/controller/operations/formats.rs
+++ quadratic-core/src/controller/operations/formats.rs
@@ -1,19 +1,49 @@
 use super::operation::Operation;
 use crate::{
     controller::GridController,
-    grid::formats::{format_update::FormatUpdate, Formats},
+    grid::{
+        formats::{format_update::FormatUpdate, Formats},
+        generate_borders, get_rect_borders, BorderSelection, Sheet,
+    },
     selection::Selection,
+    Rect,
 };
 
 impl GridController {
+    fn clear_border_op(sheet: &Sheet, rect: &Rect) -> Operation {
+        let selections = vec![BorderSelection::Clear];
+        let cur_borders = get_rect_borders(sheet, rect);
+        let new_borders = generate_borders(sheet, rect, selections.clone(), None);
+        let borders = if cur_borders.render_lookup == new_borders.render_lookup {
+            generate_borders(sheet, rect, selections.clone(), None)
+        } else {
+            new_borders
+        };
+        Operation::SetBorders {
+            sheet_rect: rect.to_sheet_rect(sheet.id),
+            borders,
+        }
+    }
+
     pub(crate) fn clear_format_selection_operations(
         &self,
         selection: &Selection,
     ) -> Vec<Operation> {
-        vec![Operation::SetCellFormatsSelection {
+        let mut ops = vec![Operation::SetCellFormatsSelection {
             selection: selection.clone(),
             formats: Formats::repeat(FormatUpdate::cleared(), selection.count()),
-        }]
+        }];
+        let sheet_id = selection.sheet_id;
+        if let Some(sheet) = self.try_sheet(sheet_id) {
+            // todo: this is very hacky. We need to refactor borders to make
+            // this work the same as all other formats.
+            if let Some(rects) = selection.rects.as_ref() {
+                rects.iter().for_each(|rect| {
+                    ops.push(GridController::clear_border_op(sheet, rect));
+                });
+            }
+        }
+        ops
     }
 }
 
