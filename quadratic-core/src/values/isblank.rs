0e73c9464e3ccabf65695586b76eea578eee04e8
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-07-29 18:05:48 -0400

    Test `IsBlank` implementations

--- quadratic-core/src/values/isblank.rs
+++ quadratic-core/src/values/isblank.rs
@@ -50,3 +50,39 @@ impl<T: IsBlank> IsBlank for [T] {
         self.iter().all(|v| v.is_blank())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_blank() {
+        assert!(Value::Single(CellValue::Blank).is_blank());
+        assert!(!Value::from("").is_blank());
+        assert!(!Value::from(0).is_blank());
+        assert!(!Value::from(1).is_blank());
+
+        assert!((&Value::Single(CellValue::Blank)).is_blank());
+        assert!(!(&Value::from("")).is_blank());
+        assert!(!(&Value::from(0)).is_blank());
+        assert!(!(&Value::from(1)).is_blank());
+
+        let a = Array::from_random_floats(crate::ArraySize::new(1, 2).unwrap());
+        assert!(!Value::Array(a.clone()).is_blank());
+        let mut b = Array::new_empty(crate::ArraySize::new(4, 3).unwrap());
+        assert!(Value::Array(b.clone()).is_blank());
+        b.set(1, 2, CellValue::from(0)).unwrap();
+        assert!(!Value::Array(b.clone()).is_blank());
+
+        let tuple = |elems: &[_]| Value::Tuple(elems.iter().copied().cloned().collect());
+        assert!(!tuple(&[&a]).is_blank());
+        assert!(!tuple(&[&b]).is_blank());
+        assert!(!tuple(&[&a, &b]).is_blank());
+        let c = Array::new_empty(crate::ArraySize::new(5, 6).unwrap());
+        assert!(!tuple(&[&a, &c]).is_blank());
+        assert!(!tuple(&[&c, &a]).is_blank());
+        assert!(tuple(&[]).is_blank());
+        assert!(tuple(&[&c]).is_blank());
+        assert!(tuple(&[&c, &c]).is_blank());
+    }
+}
