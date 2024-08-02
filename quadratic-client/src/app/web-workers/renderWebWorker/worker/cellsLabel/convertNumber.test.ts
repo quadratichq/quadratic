12d0284c1ac3c61ee8ef8edffc1812e3f8ec2dcf
David Figatner <david@yopeyopey.com>
2024-08-02 13:50:43 -0700

    put back tests

--- quadratic-client/src/app/web-workers/renderWebWorker/worker/cellsLabel/convertNumber.test.ts
+++ quadratic-client/src/app/web-workers/renderWebWorker/worker/cellsLabel/convertNumber.test.ts
@@ -3,29 +3,29 @@ import { convertNumber, reduceDecimals } from './convertNumber';
 
 describe('convertNumber', () => {
   it('using various number formatting', () => {
-    // expect(convertNumber('1234', { commas: null, decimals: null, format: null })).toBe('1234');
-    // expect(convertNumber('1234', { commas: true, decimals: null, format: null })).toBe('1,234');
-    // expect(convertNumber('1234', { commas: true, decimals: 2, format: null })).toBe('1,234.00');
-    // expect(convertNumber('1234', { commas: true, decimals: 2, format: { type: 'CURRENCY', symbol: '$' } })).toBe(
-    //   '$1,234.00'
-    // );
-    // expect(convertNumber('1234.5678', { commas: true, decimals: 2, format: null })).toBe('1,234.57');
-    // expect(convertNumber('1234.5678', { commas: null, decimals: 5, format: null })).toBe('1234.56780');
-    // expect(
-    //   convertNumber('0.01234', { commas: null, decimals: null, format: { type: 'PERCENTAGE', symbol: null } })
-    // ).toBe('1.234%');
-    // expect(
-    //   convertNumber('123123222', { format: { type: 'EXPONENTIAL', symbol: null }, commas: null, decimals: null })
-    // ).toBe('1.23123222e+8');
-    // expect(
-    //   convertNumber('123123222', { format: { type: 'EXPONENTIAL', symbol: null }, commas: null, decimals: 2 })
-    // ).toBe('1.23e+8');
-    // expect(
-    //   convertNumber('0.0000001', { commas: null, decimals: null, format: { type: 'EXPONENTIAL', symbol: null } })
-    // ).toBe('1e-7');
-    // expect(
-    //   convertNumber('123456789', { commas: null, decimals: null, format: { type: 'EXPONENTIAL', symbol: null } })
-    // ).toBe('1.23456789e+8');
+    expect(convertNumber('1234', { commas: null, decimals: null, format: null })).toBe('1234');
+    expect(convertNumber('1234', { commas: true, decimals: null, format: null })).toBe('1,234');
+    expect(convertNumber('1234', { commas: true, decimals: 2, format: null })).toBe('1,234.00');
+    expect(convertNumber('1234', { commas: true, decimals: 2, format: { type: 'CURRENCY', symbol: '$' } })).toBe(
+      '$1,234.00'
+    );
+    expect(convertNumber('1234.5678', { commas: true, decimals: 2, format: null })).toBe('1,234.57');
+    expect(convertNumber('1234.5678', { commas: null, decimals: 5, format: null })).toBe('1234.56780');
+    expect(
+      convertNumber('0.01234', { commas: null, decimals: null, format: { type: 'PERCENTAGE', symbol: null } })
+    ).toBe('1.234%');
+    expect(
+      convertNumber('123123222', { format: { type: 'EXPONENTIAL', symbol: null }, commas: null, decimals: null })
+    ).toBe('1.23123222e+8');
+    expect(
+      convertNumber('123123222', { format: { type: 'EXPONENTIAL', symbol: null }, commas: null, decimals: 2 })
+    ).toBe('1.23e+8');
+    expect(
+      convertNumber('0.0000001', { commas: null, decimals: null, format: { type: 'EXPONENTIAL', symbol: null } })
+    ).toBe('1e-7');
+    expect(
+      convertNumber('123456789', { commas: null, decimals: null, format: { type: 'EXPONENTIAL', symbol: null } })
+    ).toBe('1.23456789e+8');
     expect(
       convertNumber('100200100.1234', { commas: null, decimals: null, format: { type: 'CURRENCY', symbol: '$' } })
     ).toBe('$100,200,100.12');
