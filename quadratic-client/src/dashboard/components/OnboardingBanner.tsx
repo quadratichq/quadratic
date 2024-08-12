be67fcb9dc02a9e276fd8f1f0b7fbbcc16a5bd79
Jim Nielsen <jimniels@gmail.com>
2024-08-12 12:06:04 -0600

    Update OnboardingBanner.tsx

--- quadratic-client/src/dashboard/components/OnboardingBanner.tsx
+++ quadratic-client/src/dashboard/components/OnboardingBanner.tsx
@@ -4,6 +4,16 @@ import { useDashboardRouteLoaderData } from '@/routes/_dashboard';
 import { apiClient } from '@/shared/api/apiClient';
 import { ROUTES } from '@/shared/constants/routes';
 import { CONTACT_URL, DOCUMENTATION_URL } from '@/shared/constants/urls';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/shared/shadcn/ui/alert-dialog';
 import { Button } from '@/shared/shadcn/ui/button';
 import { cn } from '@/shared/shadcn/utils';
 import { CheckCircledIcon, CircleIcon, Cross1Icon } from '@radix-ui/react-icons';
@@ -20,8 +30,14 @@ export function OnboardingBanner() {
       files,
       users,
       invites,
+      userMakingRequest: { teamPermissions },
     },
   } = useDashboardRouteLoaderData();
+  const [isOpenConfirmDismiss, setIsOpenConfirmDismiss] = useState(false);
+  // Only show the banner to people who can 1) write to the team, and 2) haven't dismissed it yet
+  const [showBanner, setShowBanner] = useState(
+    teamPermissions.includes('TEAM_EDIT') && !clientDataKv.onboardingBannerDismissed
+  );
   const tabContentClassName = 'flex flex-col gap-2';
   const contentBtnClassName = 'min-w-44';
 
@@ -79,103 +95,126 @@ export function OnboardingBanner() {
       ),
     },
   ];
+
   // Set the tab based on which item is incomplete
   const firstCompletedIndex = tabs.findIndex(({ completed }) => !completed);
   const [activeTabIndex, setActiveTabIndex] = useState(String(firstCompletedIndex === -1 ? 0 : firstCompletedIndex));
-  const noCompletions = tabs.length;
-  const noCompletionsCompleted = tabs.filter(({ completed }) => completed).length;
+  const tasksCount = tabs.length;
+  const tasksCompletedCount = tabs.filter(({ completed }) => completed).length;
 
+  // Immediately hide the UI then persist the dismissal to the server
   const handleDismiss = () => {
+    setShowBanner(false);
     apiClient.teams.update(teamUuid, { clientDataKv: { onboardingBannerDismissed: true } });
   };
 
-  if (clientDataKv.onboardingBannerDismissed) {
-    return null;
-  }
+  return showBanner ? (
+    <>
+      <div className="relative mt-3 flex flex-col rounded-md border border-input px-4 py-3 shadow-sm">
+        <Button
+          variant="link"
+          size="icon"
+          className="absolute right-0.5 top-0.5 text-muted-foreground hover:text-foreground"
+          aria-label="Dismiss"
+          onClick={() => {
+            if (tasksCount === tasksCompletedCount) {
+              handleDismiss();
+            } else {
+              setIsOpenConfirmDismiss(true);
+            }
+          }}
+        >
+          <Cross1Icon />
+        </Button>
 
-  // TODO: don't show this banner if the user is a TEAM_VIEWER
-
-  return (
-    <div className="relative mt-3 flex flex-col rounded-md border border-input px-4 py-3 shadow-sm">
-      <Button
-        variant="link"
-        size="icon"
-        className="absolute right-0.5 top-0.5 text-muted-foreground hover:text-foreground"
-        aria-label="Dismiss"
-        onClick={handleDismiss}
-      >
-        <Cross1Icon />
-      </Button>
-      <div className="flex items-center gap-4 border-b border-border pb-3">
-        <img src={Logo} width="24" height="35" alt="Quadratic logo" />
-        <div className="flex flex-col">
-          <h3 className="text-md font-medium leading-6 tracking-tight">Getting started with your team</h3>
-          <p className="text-sm text-muted-foreground">
-            <strong className="font-semibold">
-              {noCompletionsCompleted} of {noCompletions} complete
-            </strong>{' '}
-            ·{' '}
-            {noCompletionsCompleted === noCompletions ? (
-              <>
-                All tasks complete!{' '}
-                <Button
-                  onClick={handleDismiss}
-                  variant="link"
-                  className="h-auto p-0 text-inherit underline hover:text-primary"
-                >
-                  Dismiss
-                </Button>
-                .
-              </>
-            ) : (
-              <>
-                Need help?{' '}
-                <a href={DOCUMENTATION_URL} target="_blank" rel="noreferrer" className="underline hover:text-primary">
-                  Read the docs
-                </a>{' '}
-                or{' '}
-                <a href={CONTACT_URL} target="_blank" rel="noreferrer" className="underline hover:text-primary">
-                  contact us
-                </a>
-                , we love hearing from folks.
-              </>
-            )}
-          </p>
-        </div>
-      </div>
-      <Tabs.Root
-        value={activeTabIndex}
-        onValueChange={(newActiveTab) => setActiveTabIndex(newActiveTab)}
-        className="grid grid-cols-[14rem_1fr] text-sm"
-      >
-        <Tabs.List className="flex flex-col gap-[1px] border-r border-border pr-4 pt-2">
-          {tabs.map(({ label, completed }, index) => (
-            <Tabs.Trigger
-              key={index}
-              value={String(index)}
-              className={cn(
-                'inline-flex h-9 items-center gap-2 rounded px-2 ring-offset-background transition-all hover:bg-accent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-accent',
-                completed && 'text-success'
-              )}
-            >
-              {completed ? (
-                <CheckCircledIcon className="text-success" />
+        <div className="flex items-center gap-4 border-b border-border pb-3">
+          <img src={Logo} width="24" height="35" alt="Quadratic logo" />
+          <div className="flex flex-col">
+            <h3 className="text-md font-medium leading-6 tracking-tight">Getting started with your team</h3>
+            <p className="text-sm text-muted-foreground">
+              <strong className="font-semibold">
+                {tasksCompletedCount} of {tasksCount} complete
+              </strong>{' '}
+              ·{' '}
+              {tasksCompletedCount === tasksCount ? (
+                <span className="text-success">
+                  All tasks complete!{' '}
+                  <Button onClick={handleDismiss} variant="link" className="h-auto p-0 text-inherit underline">
+                    Dismiss
+                  </Button>
+                  .
+                </span>
               ) : (
-                <CircleIcon className="text-muted-foreground opacity-30" />
+                <>
+                  Need help?{' '}
+                  <a href={DOCUMENTATION_URL} target="_blank" rel="noreferrer" className="underline hover:text-primary">
+                    Read the docs
+                  </a>{' '}
+                  or{' '}
+                  <a href={CONTACT_URL} target="_blank" rel="noreferrer" className="underline hover:text-primary">
+                    contact us
+                  </a>
+                  , we love hearing from folks.
+                </>
               )}
-              {label}
-            </Tabs.Trigger>
-          ))}
-        </Tabs.List>
-
-        <div className="py-3 pl-4">
-          {tabs.map(({ content }, index) => (
-            <Tabs.Content key={index} value={String(index)} className={tabContentClassName}>
-              {content}
-            </Tabs.Content>
-          ))}
+            </p>
+          </div>
         </div>
-      </Tabs.Root>
-    </div>
-  );
+        <Tabs.Root
+          value={activeTabIndex}
+          onValueChange={(newActiveTab) => setActiveTabIndex(newActiveTab)}
+          className="grid grid-cols-[14rem_1fr] text-sm"
+        >
+          <Tabs.List className="flex flex-col gap-[1px] border-r border-border pr-4 pt-2">
+            {tabs.map(({ label, completed }, index) => (
+              <Tabs.Trigger
+                key={index}
+                value={String(index)}
+                className={cn(
+                  'inline-flex h-9 items-center gap-2 rounded px-2 ring-offset-background transition-all hover:bg-accent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-accent',
+                  completed && 'text-success'
+                )}
+              >
+                {completed ? (
+                  <CheckCircledIcon className="text-success" />
+                ) : (
+                  <CircleIcon className="text-muted-foreground opacity-30" />
+                )}
+                {label}
+              </Tabs.Trigger>
+            ))}
+          </Tabs.List>
+
+          <div className="py-3 pl-4">
+            {tabs.map(({ content }, index) => (
+              <Tabs.Content key={index} value={String(index)} className={tabContentClassName}>
+                {content}
+              </Tabs.Content>
+            ))}
+          </div>
+        </Tabs.Root>
+      </div>
+      <AlertDialog open={isOpenConfirmDismiss} onOpenChange={setIsOpenConfirmDismiss}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Please confirm</AlertDialogTitle>
+            <AlertDialogDescription>
+              You haven't completed all of Quadratic's basic tasks. Dismissing this banner will hide it permanently.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setIsOpenConfirmDismiss(false)}>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                setIsOpenConfirmDismiss(false);
+                handleDismiss();
+              }}
+            >
+              Dismiss
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  ) : null;
 }
