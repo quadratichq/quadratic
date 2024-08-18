c597244966954d2669b4fb32532f52f3eb57f96f
AyushAgrawal-A2 <ayushagl06@gmail.com>
2024-08-19 05:00:52 +0530

    fix: number decimals clip

--- quadratic-client/src/app/web-workers/renderWebWorker/worker/cellsLabel/convertNumber.ts
+++ quadratic-client/src/app/web-workers/renderWebWorker/worker/cellsLabel/convertNumber.ts
@@ -58,7 +58,7 @@ export const reduceDecimals = (
   currentFractionDigits?: number
 ): { number: string; currentFractionDigits: number } | undefined => {
   if (currentFractionDigits === undefined) {
-    currentFractionDigits = getFractionDigits(number, format);
+    currentFractionDigits = getFractionDigits(current, format);
     currentFractionDigits = Math.max(0, currentFractionDigits - 1);
   }
   const updated = convertNumber(number, format, currentFractionDigits);
