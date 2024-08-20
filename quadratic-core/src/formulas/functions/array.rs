0b6c34a9dc9dcdc66ed7ed646c8f948cc165444e
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-08-20 18:08:43 -0400

    Add stubs for `SORT` and `UNIQUE`

--- quadratic-core/src/formulas/functions/array.rs
+++ quadratic-core/src/formulas/functions/array.rs
@@ -12,82 +12,104 @@ pub const CATEGORY: FormulaFunctionCategory = FormulaFunctionCategory {
 };
 
 fn get_functions() -> Vec<FormulaFunction> {
-    vec![formula_fn!(
-        /// Filters an array of values by a list of booleans.
-        ///
-        /// `include` must contain either a single row or a single column. If
-        /// `include` is a single column, then each value in it corresponds to a
-        /// row from `array`; if `include` is a single row, then each value in
-        /// it corresponds to a column from `array`. If the value in `include`
-        /// is [truthy](#logic-functions), then the corresponding row/column
-        /// from `array` is included in the output. If `include` contains a
-        /// single value, then it corresponds to all of `array`.
-        ///
-        /// If no rows/columns are included in the output, then `if_empty` is
-        /// outputted instead. If no rows/columns are included in the output
-        /// _and_ `if_empty` is omitted, then an error is returned.
-        #[examples("FILTER(A1:C5, D1:D5, )")]
-        fn FILTER(
-            span: Span,
-            array: (Spanned<Array>),
-            include: (Spanned<Array>),
-            if_empty: (Option<Value>),
-        ) {
-            // Construct this `Result`, but don't return an error yet.
-            let empty_result = if_empty.ok_or_else(|| RunErrorMsg::EmptyArray.with_span(span));
+    vec![
+        formula_fn!(
+            /// Filters an array of values by a list of booleans.
+            ///
+            /// `include` must contain either a single row or a single column. If
+            /// `include` is a single column, then each value in it corresponds to a
+            /// row from `array`; if `include` is a single row, then each value in
+            /// it corresponds to a column from `array`. If the value in `include`
+            /// is [truthy](#logic-functions), then the corresponding row/column
+            /// from `array` is included in the output. If `include` contains a
+            /// single value, then it corresponds to all of `array`.
+            ///
+            /// If no rows/columns are included in the output, then `if_empty` is
+            /// outputted instead. If no rows/columns are included in the output
+            /// _and_ `if_empty` is omitted, then an error is returned.
+            #[examples("FILTER(A1:C5, D1:D5, )")]
+            fn FILTER(
+                span: Span,
+                array: (Spanned<Array>),
+                include: (Spanned<Array>),
+                if_empty: (Option<Value>),
+            ) {
+                // Construct this `Result`, but don't return an error yet.
+                let empty_result = if_empty.ok_or_else(|| RunErrorMsg::EmptyArray.with_span(span));
 
-            let include_span = include.span;
+                let include_span = include.span;
 
-            let axis = include.array_linear_axis()?;
-            let include: Vec<bool> = include
-                .try_as_linear_array()?
-                .iter()
-                .map(|v| bool::try_from(v.clone()).map_err(|e| e.with_span(include_span)))
-                .try_collect()?;
+                let axis = include.array_linear_axis()?;
+                let include: Vec<bool> = include
+                    .try_as_linear_array()?
+                    .iter()
+                    .map(|v| bool::try_from(v.clone()).map_err(|e| e.with_span(include_span)))
+                    .try_collect()?;
 
-            match axis {
-                None => {
-                    if *include.get(0).unwrap_or(&false) {
-                        Value::from(array.inner)
-                    } else {
-                        empty_result?
-                    }
-                }
-
-                Some(axis) => {
-                    array.check_array_size_on(axis, include.len() as u32)?;
-                    let mut matches = 0;
-                    let mut return_values = smallvec![];
-                    let perpendicular_len = array.inner.size()[axis.other_axis()].get();
-                    for i in include.iter().positions(|&x| x) {
-                        matches += 1;
-                        let i = i as u32;
-                        for j in 0..perpendicular_len {
-                            let v = match axis {
-                                Axis::X => array.inner.get(i, j),
-                                Axis::Y => array.inner.get(j, i),
-                            };
-                            return_values.push(v.map_err(|e| e.with_span(span))?.clone());
+                match axis {
+                    None => {
+                        if *include.get(0).unwrap_or(&false) {
+                            Value::from(array.inner)
+                        } else {
+                            empty_result?
                         }
                     }
-                    let return_array_size = ArraySize::new(perpendicular_len, matches);
-                    match return_array_size {
-                        None => empty_result?,
-                        Some(size) => {
-                            let a = Array::new_row_major(size, return_values)?;
-                            Value::from(match axis {
-                                Axis::X => a.transpose(),
-                                Axis::Y => a,
-                            })
+
+                    Some(axis) => {
+                        array.check_array_size_on(axis, include.len() as u32)?;
+                        let mut matches = 0;
+                        let mut return_values = smallvec![];
+                        let perpendicular_len = array.inner.size()[axis.other_axis()].get();
+                        for i in include.iter().positions(|&x| x) {
+                            matches += 1;
+                            let i = i as u32;
+                            for j in 0..perpendicular_len {
+                                let v = match axis {
+                                    Axis::X => array.inner.get(i, j),
+                                    Axis::Y => array.inner.get(j, i),
+                                };
+                                return_values.push(v.map_err(|e| e.with_span(span))?.clone());
+                            }
+                        }
+                        let return_array_size = ArraySize::new(perpendicular_len, matches);
+                        match return_array_size {
+                            None => empty_result?,
+                            Some(size) => {
+                                let a = Array::new_row_major(size, return_values)?;
+                                Value::from(match axis {
+                                    Axis::X => a.transpose(),
+                                    Axis::Y => a,
+                                })
+                            }
                         }
                     }
                 }
             }
-        }
-    )]
+        ),
+        formula_fn!(
+            /// TODO: documentation
+            #[examples("UNIQUE()")]
+            fn UNIQUE(array: Array, by_column: (Option<bool>), exactly_once: (Option<bool>)) {
+                array // TODO
+            }
+        ),
+        formula_fn!(
+            /// TODO: documentation
+            #[examples("SORT()")]
+            fn SORT(
+                array: Array,
+                sort_index: (Option<f64>),
+                sort_order: (Option<f64>),
+                by_column: (Option<bool>),
+            ) {
+                array // TODO
+            }
+        ),
+    ]
 }
 
 #[cfg(test)]
+#[serial_test::parallel]
 mod tests {
     use crate::formulas::tests::*;
 
@@ -160,4 +182,10 @@ mod tests {
             eval_to_err(&g, "FILTER(A1:E3, {0,1,-6,'a',TRUE}, 'oh no')").msg,
         )
     }
+
+    #[test]
+    fn test_formula_sort() {}
+
+    #[test]
+    fn test_formula_unique() {}
 }
