2b5308771a311b98918d13866a3cb4c5fe651736
David DiMaria <info@collectivesessions.com>
2024-08-26 16:03:45 -0600

    Separate apart init.sh and start.sh

--- self-hosting/start.sh
+++ self-hosting/start.sh
@@ -1,44 +1,9 @@
 #!/bin/sh
 
-REPO="https://github.com/quadratichq/quadratic.git"
-BRANCH="self-hosting-setup"
-DIR="self-hosting"
-INVALID_LICENSE_KEY="Invalid license key."
-
-get_license_key() {
-    read -p "Please enter your license key: " user_input
-    
-    if [[ $user_input =~ ^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$ ]]; then
-      echo $user_input
-    else
-      echo $INVALID_LICENSE_KEY
-      return 1
-    fi
-}
-
-checkout() {
-  git clone -b $BRANCH --filter=blob:none --no-checkout --depth 1 --sparse $REPO
-  cd quadratic
-  git sparse-checkout set ${DIR}/
-  git checkout
-  cd $DIR
-
-}
-
 start() {
   docker compose --profile "*" down
   yes | docker compose rm quadratic-client
   docker compose --profile "*" up
 }
 
-LICENSE_KEY=$(get_license_key)
-
-if [ "$LICENSE_KEY" = "$INVALID_LICENSE_KEY" ]; then
-  echo $INVALID_LICENSE_KEY
-else
-  checkout
-
-  sed -i '' "s/\"LICENSE_KEY\"/\"$LICENSE_KEY\"/g" "docker-compose.yml"
-  
-  start
-fi
+start
