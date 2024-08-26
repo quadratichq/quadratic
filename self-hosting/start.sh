459995626566437a96a750c877b1a9d40522aaa0
David DiMaria <info@collectivesessions.com>
2024-08-26 15:29:43 -0600

    Ask user for license key in self-hosting and add value to docker-compose

--- self-hosting/start.sh
+++ self-hosting/start.sh
@@ -3,6 +3,18 @@
 REPO="https://github.com/quadratichq/quadratic.git"
 BRANCH="self-hosting-setup"
 DIR="self-hosting"
+INVALID_LICENSE_KEY="Invalid license key."
+
+get_license_key() {
+    read -p "Please enter your license key: " user_input
+    
+    if [[ $user_input =~ ^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$ ]]; then
+      echo $user_input
+    else
+      echo $INVALID_LICENSE_KEY
+      return 1
+    fi
+}
 
 checkout() {
   git clone -b $BRANCH --filter=blob:none --no-checkout --depth 1 --sparse $REPO
@@ -19,5 +31,12 @@ start() {
   docker compose --profile "*" up
 }
 
-checkout
-start
+LICENSE_KEY=$(get_license_key)
+
+if [ "$LICENSE_KEY" = "$INVALID_LICENSE_KEY" ]; then
+  echo $INVALID_LICENSE_KEY
+else
+  checkout
+  start
+  sed -i '' "s/\"LICENSE_KEY\"/\"$LICENSE_KEY\"/g" "docker-compose.yml"
+fi
