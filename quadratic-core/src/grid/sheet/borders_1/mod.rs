75cc92610052188688a6a66542f08b02c85f87ee
David Figatner <david@yopeyopey.com>
2024-08-29 05:56:35 -0700

    cleaning up

--- quadratic-core/src/grid/sheet/borders_1/mod.rs
+++ quadratic-core/src/grid/sheet/borders_1/mod.rs
@@ -2,7 +2,7 @@ use borders_style::{BorderStyle, BorderStyleCell};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use crate::grid::{block::Block, ColumnData};
+use crate::grid::{block::SameValue, ColumnData};
 
 pub mod borders_set_get;
 pub mod borders_style;
@@ -15,10 +15,10 @@ pub struct Borders {
     pub(crate) rows: HashMap<i64, BorderStyleCell>,
 
     // cell-specific formatting
-    pub(crate) top: HashMap<i64, ColumnData<Block<BorderStyle>>>,
-    pub(crate) bottom: HashMap<i64, ColumnData<Block<BorderStyle>>>,
+    pub(crate) top: HashMap<i64, ColumnData<SameValue<BorderStyle>>>,
+    pub(crate) bottom: HashMap<i64, ColumnData<SameValue<BorderStyle>>>,
 
     // note, this ColumnData is actually horizontal
-    pub(crate) left: HashMap<i64, ColumnData<Block<BorderStyle>>>,
-    pub(crate) right: HashMap<i64, ColumnData<Block<BorderStyle>>>,
+    pub(crate) left: HashMap<i64, ColumnData<SameValue<BorderStyle>>>,
+    pub(crate) right: HashMap<i64, ColumnData<SameValue<BorderStyle>>>,
 }
