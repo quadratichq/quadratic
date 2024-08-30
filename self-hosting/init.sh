e20a821e40ec867b54fe61def4ca7aeebb7a8187
David DiMaria <info@collectivesessions.com>
2024-08-30 12:06:23 -0600

    Improve init experience for self-hosting

--- self-hosting/init.sh
+++ self-hosting/init.sh
@@ -22,18 +22,37 @@ checkout() {
   cd quadratic
   git sparse-checkout set ${DIR}/
   git checkout
-  cd $DIR
 
 }
 
-LICENSE_KEY=$(get_license_key)
+LICENSE_KEY=""
 
-if [ "$LICENSE_KEY" = "$INVALID_LICENSE_KEY" ]; then
-  echo $INVALID_LICENSE_KEY
+# check if LICENSE file exists
+if ! [ -f "quadratic/LICENSE" ]; then
+  LICENSE_KEY=$(get_license_key)
+
+  if [ "$LICENSE_KEY" = "$INVALID_LICENSE_KEY" ]; then
+    echo $INVALID_LICENSE_KEY
+  else
+    # retrieve the code from github
+    checkout
+    
+    # write license key to LICENSE file
+    touch LICENSE
+    echo $LICENSE_KEY > LICENSE
+
+    cp -a self-hosting/. .
+    rm -rf self-hosting
+    rm ../init.sh
+    rm init.sh
+
+    sed -i '' "s/\"LICENSE_KEY\"/\"$LICENSE_KEY\"/g" "docker-compose.yml"
+
+    cd quadratic
+  fi
 else
-  checkout
-
-  sed -i '' "s/\"LICENSE_KEY\"/\"$LICENSE_KEY\"/g" "docker-compose.yml"
-  
-  sh start.sh
+  cd quadratic
+  LICENSE_KEY=$(<LICENSE)
 fi
+
+sh start.sh
