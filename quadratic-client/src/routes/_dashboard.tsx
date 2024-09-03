07881f5f89afe57ae1bca399d90aed545244a48c
AyushAgrawal-A2 <ayushagl06@gmail.com>
2024-09-04 02:27:23 +0530

    fix: redirect after login

--- quadratic-client/src/routes/_dashboard.tsx
+++ quadratic-client/src/routes/_dashboard.tsx
@@ -23,7 +23,6 @@ import {
   ShouldRevalidateFunctionArgs,
   isRouteErrorResponse,
   redirect,
-  replace,
   useLocation,
   useNavigation,
   useRevalidator,
@@ -42,7 +41,7 @@ export const ACTIVE_TEAM_UUID_KEY = 'activeTeamUuid';
 export const shouldRevalidate = ({ currentUrl, nextUrl }: ShouldRevalidateFunctionArgs) => {
   return (
     currentUrl.pathname === '/' ||
-    currentUrl.pathname.startsWith('/files') ||
+    currentUrl.pathname.startsWith('/files/') ||
     nextUrl.pathname === '/' ||
     nextUrl.pathname.startsWith('/teams/')
   );
@@ -96,7 +95,7 @@ export const loader = async ({ params, request }: LoaderFunctionArgs): Promise<L
     // 4) there's no teams in the API, so create one and send the user to it
   } else if (teams.length === 0) {
     const newTeam = await apiClient.teams.create({ name: 'My Team' });
-    return replace(ROUTES.TEAM(newTeam.uuid));
+    return redirect(ROUTES.TEAM(newTeam.uuid));
   }
 
   // This should never happen, but if it does, we'll log it to sentry
