21057a2d3b9e7e43e08ca239f3a6c14da2b9f8f9
David DiMaria <info@collectivesessions.com>
2024-09-04 16:44:44 -0600

    Allow command line arguments to be used in init.sh

--- self-hosting/init.sh
+++ self-hosting/init.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Self-Hosting Initialization
+# 
+# Usage:
+# 
+#   ./init.sh 83f0ebdf-eafb-4c8d-bd7b-04ea07d61b7f localhost
+# 
+# 
+# Flow:
+# 
+# First, check to see if there is a VERSION file, if so, use that version.
+# If not, then check for the first command line argument, if so, use that version.
+# Else, prompt the user.
+# 
+# First, check to see if there is a HOST file, if so, use that host.
+# If not, then check for the first command line argument, if so, use that host.
+# Else, prompt the user.
+
 REPO="https://github.com/quadratichq/quadratic.git"
 BRANCH="self-hosting-setup"
 DIR="self-hosting"
@@ -35,48 +52,44 @@ checkout() {
 LICENSE_KEY=""
 HOST=""
 
-# check if LICENSE file exists
-if ! [ -f "quadratic/LICENSE" ]; then
-  LICENSE_KEY=$(get_license_key)
-
-  if [ "$LICENSE_KEY" = "$INVALID_LICENSE_KEY" ]; then
-    echo $INVALID_LICENSE_KEY
-  else
-
-    if ! [ -f "quadratic/HOST" ]; then
-      HOST=$(get_host)
-    fi
-
-    # retrieve the code from github
-    checkout
-    
-    # write license key to LICENSE file
-    touch LICENSE
-    echo $LICENSE_KEY > LICENSE
-    
-    # write host to HOST file
-    touch HOST
-    echo $HOST > HOST
-
-    cp -a self-hosting/. .
-    rm -rf self-hosting
-    rm ../init.sh
-    rm init.sh
-
-    # adding .bak for compatibility with both GNU (Linux) and BSD (MacOS) sed
-    sed -i.bak "s/#LICENSE_KEY#/$LICENSE_KEY/g" "docker-compose.yml"
-    sed -i.bak "s/#HOST#/$HOST/g" "docker-compose.yml"
-    sed -i.bak "s/#HOST#/$HOST/g" "docker/ory-auth/config/kratos.yml"
-
-    rm docker-compose.yml.bak
-    rm docker/ory-auth/config/kratos.yml.bak
-
-    cd quadratic
-  fi
+if [ -f "quadratic/LICENSE" ]; then
+  LICENSE_KEY=$(<quadratic/LICENSE)
+elif [ $1 ]; then
+  LICENSE_KEY=$1
 else
-  cd quadratic
-  LICENSE_KEY=$(<LICENSE)
-  HOST=$(<HOST)
+  LICENSE_KEY=$(get_license_key)
 fi
+
+if [ -f "quadratic/HOST" ]; then
+  HOST=$(<quadratic/HOST)
+elif [ $2 ]; then
+  HOST=$2
+else
+  HOST=$(get_host)
+fi
+
+# retrieve the code from github
+checkout
+
+# write license key to LICENSE file
+touch LICENSE
+echo $LICENSE_KEY > LICENSE
+
+# write host to HOST file
+touch HOST
+echo $HOST > HOST
+
+cp -a self-hosting/. .
+rm -rf self-hosting
+rm ../init.sh
+rm init.sh
+
+# adding .bak for compatibility with both GNU (Linux) and BSD (MacOS) sed
+sed -i.bak "s/#LICENSE_KEY#/$LICENSE_KEY/g" "docker-compose.yml"
+sed -i.bak "s/#HOST#/$HOST/g" "docker-compose.yml"
+sed -i.bak "s/#HOST#/$HOST/g" "docker/ory-auth/config/kratos.yml"
+
+rm docker-compose.yml.bak
+rm docker/ory-auth/config/kratos.yml.bak
 
 sh start.sh
