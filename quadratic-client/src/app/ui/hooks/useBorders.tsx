2213b4a690d9a68f412a2de568fc588481d762a8
David Figatner <david@yopeyopey.com>
2024-09-22 06:54:25 -0700

    remove limitation on multi-cursor borders

--- quadratic-client/src/app/ui/hooks/useBorders.tsx
+++ quadratic-client/src/app/ui/hooks/useBorders.tsx
@@ -26,30 +26,25 @@ export const useBorders = (): UseBordersResults => {
         const selection = options.selection ?? prev.selection;
         const color = options.color ?? prev.color;
         const line = options.line ?? prev.line;
-        const cursor = sheets.sheet.cursor;
-        if (cursor.multiCursor && cursor.multiCursor.length > 1) {
-          console.log('TODO: implement multiCursor border support');
-        } else {
-          const colorTint = convertColorStringToTint(color);
-          const colorArray = convertTintToArray(colorTint);
-          const style: BorderStyle = {
-            color: {
-              red: Math.floor(colorArray[0] * 255),
-              green: Math.floor(colorArray[1] * 255),
-              blue: Math.floor(colorArray[2] * 255),
-              alpha: 1,
-            },
-            line,
-          };
-          const rustSelection = sheets.getRustSelection();
-          if (options.selection) {
-            quadraticCore.setBorders(rustSelection, options.selection, style);
-          } else if (
-            prev.selection &&
-            ((!!options.color && options.color !== prev.color) || (!!options.line && options.line !== prev.line))
-          ) {
-            quadraticCore.setBorders(rustSelection, prev.selection, style);
-          }
+        const colorTint = convertColorStringToTint(color);
+        const colorArray = convertTintToArray(colorTint);
+        const style: BorderStyle = {
+          color: {
+            red: Math.floor(colorArray[0] * 255),
+            green: Math.floor(colorArray[1] * 255),
+            blue: Math.floor(colorArray[2] * 255),
+            alpha: 1,
+          },
+          line,
+        };
+        const rustSelection = sheets.getRustSelection();
+        if (options.selection) {
+          quadraticCore.setBorders(rustSelection, options.selection, style);
+        } else if (
+          prev.selection &&
+          ((!!options.color && options.color !== prev.color) || (!!options.line && options.line !== prev.line))
+        ) {
+          quadraticCore.setBorders(rustSelection, prev.selection, style);
         }
         return { selection, color, line };
       });
@@ -58,11 +53,6 @@ export const useBorders = (): UseBordersResults => {
   );
 
   const clearBorders = useCallback((): void => {
-    const cursor = sheets.sheet.cursor;
-    if (cursor.multiCursor && cursor.multiCursor.length > 1) {
-      console.log('TODO: implement multiCursor border support');
-      return;
-    }
     quadraticCore.setBorders(sheets.getRustSelection(), 'clear');
   }, []);
 
