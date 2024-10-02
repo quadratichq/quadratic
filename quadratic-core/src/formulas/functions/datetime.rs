2254294c91ebefe6fba0db1be5bcefcc5521a99a
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-10-02 12:37:24 -0400

    Implement and test `TIME()`

--- quadratic-core/src/formulas/functions/datetime.rs
+++ quadratic-core/src/formulas/functions/datetime.rs
@@ -36,16 +36,20 @@ fn get_functions() -> Vec<FormulaFunction> {
             /// `year`, `month`, and `day` must be integers.
             ///
             /// If `day` is outside the range of days in the given month, then
-            /// days are counted before or after the given month. For example,
-            /// `DATE(2024, 1, 99)` returns `2024-04-08`.
+            /// it overflows and offsets the `month`. For example, `DATE(2024,
+            /// 1, 99)` returns `2024-04-08`.
             ///
             /// If `month` is outside the range from `1` to `12` (inclusive),
-            /// then months are counted before or after the given year. For
-            /// example, `DATE(2024, 13, 1)` returns `2025-01-01` and
-            /// `DATE(2024, 0, 1)` returns `2023-12-01`.
+            /// then it overflows and offsets the `year`. For example,
+            /// `DATE(2024, 13, 1)` returns `2025-01-01` and `DATE(2024, 0, 1)`
+            /// returns `2023-12-01`.
             ///
             /// _Note that February 29, 1900 does not exist._
-            #[examples("DATE(2024, 04, 08)", "DATE(1995, 12, 25)")]
+            ///
+            /// To construct a date time, simply add a date and time. For
+            /// example, `DATE(1965, 3, 18) + TIME(8, 34, 51)` was the second
+            /// (in UTC) that Alexei Leonov began the first-ever spacewalk.
+            #[examples("DATE(2024, 04, 08)", "DATE(1995, 12, 25)", "DATE(1965, 3, 18)")]
             #[zip_map]
             fn DATE(span: Span, [year]: i64, [month]: i64, [day]: i64) {
                 // IIFE to mimic try_block
@@ -81,22 +85,46 @@ fn get_functions() -> Vec<FormulaFunction> {
             /// `hour` and `minute` must be integers. `second` must be a number
             /// (but does not need to be an integer).
             ///
-            /// If the number of seconds is outside the range from `0`
-            /// (inclusive) to `60` (exclusive), then it carries over to some
-            /// number of minutes.
+            /// If `second` is outside the range from `0` to `59` inclusive,
+            /// then it is divided by 60. The remainder is used for `second` and
+            /// the quotient is added to `minute`. For example, `TIME(9, 0,
+            /// 844)` returns `9:14:04 AM`.
             ///
-            /// If the number of minutes is outside the range from `0` to `59`
-            /// (inclusive), then it carries over to some number of hours.
+            /// Similarly, if `minute` is outside the range from `0` to `59`
+            /// inclusive, then it is divided by 60. The remainder is used for
+            /// `minute` and the quotient is added to `hour`. For example,
+            /// `TIME(16, 70, 45)` returns `5:10:45 PM` and `TIME(12, -1, -1)`
+            /// returns `11:58:59 AM`.
             ///
-            /// Any number for `hour` Negative numbers or numbers greater than
-            /// 23 for `hour` wrap around so that the hour is always between 0
-            /// and 23.
-            #[examples("TIME()")]
-            fn TIME() {
-                CellValue::Date(chrono::Local::now().date_naive())
+            /// If `hour` is outside the range from `0` to `23`, then it is
+            /// divided by 24. The quotient is discarded and the remainder is
+            /// used for `hour`. For example, `TIME(-8, 30, 0)` returns `4:30:00
+            /// PM`.
+            ///
+            /// To construct a date time, simply add a date and time. For
+            /// example, `DATE(1969, 7, 21) + TIME(2, 56, 0)` was the minute (in
+            /// UTC) that Neil Armstrong became the first person to walk on the
+            /// surface of the moon.
+            #[examples("TIME(8, 34, 51)", "TIME(2, 56, 0)", "TIME(2,30,59.99)")]
+            #[zip_map]
+            fn TIME(span: Span, [hour]: i64, [minute]: i64, [second]: f64) {
+                const NANO_SECONDS_PER_SECOND: f64 = 1_000_000_000.0;
+
+                (|| {
+                    let minute = minute.checked_add(second.div_euclid(60.0) as i64)?;
+                    let nanoseconds = ((second - second.floor()) * NANO_SECONDS_PER_SECOND) as u32;
+                    let second = second.rem_euclid(60.0).floor() as u32;
+
+                    let hour = hour.checked_add(minute.div_euclid(60) as i64)?;
+                    let minute = minute.rem_euclid(60) as u32;
+
+                    let hour = hour.rem_euclid(24) as u32;
+
+                    chrono::NaiveTime::from_hms_nano_opt(hour, minute, second, nanoseconds)
+                })()
+                .ok_or(RunErrorMsg::Overflow.with_span(span))?
             }
         ),
-        // formula_fn!(),
     ]
 }
 
@@ -134,5 +162,16 @@ mod tests {
         assert_eq!(eval_to_string(&g, "DATE(2024, 0, 0)"), "2023-11-30"); // negative wrap month->year and day->month->year
         assert_eq!(eval_to_string(&g, "DATE(2024, -22, -16)"), "2022-01-15"); // lotsa negatives
         assert_eq!(eval_to_string(&g, "DATE(1900, 2, 29)"), "1900-03-01"); // no 1900-02-29!
+    }
+
+    #[test]
+    fn test_formula_time() {
+        let g = Grid::new();
+        assert_eq!(eval_to_string(&g, "TIME(2, 30, 45)"), "02:30:45");
+        assert_eq!(eval_to_string(&g, "TIME(2, 30, -45)"), "02:29:15");
+        assert_eq!(eval_to_string(&g, "TIME(2, -30, 45)"), "01:30:45");
+        assert_eq!(eval_to_string(&g, "TIME(2, -30, -45)"), "01:29:15");
+        assert_eq!(eval_to_string(&g, "TIME(-1, 0, 0)"), "23:00:00");
+        assert_eq!(eval_to_string(&g, "TIME(999, 72, 235)"), "16:15:55");
     }
 }
