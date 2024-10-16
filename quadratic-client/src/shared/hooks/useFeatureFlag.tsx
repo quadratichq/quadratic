543a5655fbdd034e2ec65334b576d86b92c792e9
Jim Nielsen <jimniels@gmail.com>
2024-10-16 14:08:30 -0600

    Update useFeatureFlag.tsx

--- quadratic-client/src/shared/hooks/useFeatureFlag.tsx
+++ quadratic-client/src/shared/hooks/useFeatureFlag.tsx
@@ -3,9 +3,6 @@ import useLocalStorage from '@/shared/hooks/useLocalStorage';
 const initialState = { themeAppearanceMode: false, themeAccentColor: false };
 export type FeatureFlagKey = keyof typeof initialState;
 
-/**
- * Hook - this is how you should get the state of this provider
- */
 export const useFeatureFlag = (key: FeatureFlagKey) => {
   const [state, setState] = useLocalStorage('featureFlags', initialState);
 
