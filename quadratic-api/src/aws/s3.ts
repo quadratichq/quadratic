edd5cf819eface9f9ac2268162777cf8d4caba37
AyushAgrawal-A2 <ayushagl06@gmail.com>
2024-10-23 02:37:24 +0530

    fix localstack

--- quadratic-api/src/aws/s3.ts
+++ quadratic-api/src/aws/s3.ts
@@ -1,6 +1,13 @@
 import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { AWS_S3_ACCESS_KEY_ID, AWS_S3_BUCKET_NAME, AWS_S3_REGION, AWS_S3_SECRET_ACCESS_KEY } from '../env-vars';
+import {
+  AWS_S3_ACCESS_KEY_ID,
+  AWS_S3_BUCKET_NAME,
+  AWS_S3_ENDPOINT,
+  AWS_S3_REGION,
+  AWS_S3_SECRET_ACCESS_KEY,
+} from '../env-vars';
+const endpoint = AWS_S3_ENDPOINT;
 
 // Initialize S3 client
 export const s3Client = new S3Client({
@@ -9,7 +16,14 @@ export const s3Client = new S3Client({
     accessKeyId: AWS_S3_ACCESS_KEY_ID,
     secretAccessKey: AWS_S3_SECRET_ACCESS_KEY,
   },
-  useAccelerateEndpoint: true,
+  ...(endpoint === undefined
+    ? {
+        useAccelerateEndpoint: true,
+      }
+    : {
+        endpoint,
+        forcePathStyle: true,
+      }),
 });
 
 export const uploadStringAsFileS3 = async (fileKey: string, contents: string) => {
