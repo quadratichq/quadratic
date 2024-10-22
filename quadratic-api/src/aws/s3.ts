d0bcfddc09fa5aa91ed40142f653676efa828fb3
AyushAgrawal-A2 <ayushagl06@gmail.com>
2024-10-23 01:38:16 +0530

    test: try aws s3 transfer accelerate

--- quadratic-api/src/aws/s3.ts
+++ quadratic-api/src/aws/s3.ts
@@ -1,13 +1,6 @@
 import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import {
-  AWS_S3_ACCESS_KEY_ID,
-  AWS_S3_BUCKET_NAME,
-  AWS_S3_ENDPOINT,
-  AWS_S3_REGION,
-  AWS_S3_SECRET_ACCESS_KEY,
-} from '../env-vars';
-const endpoint = AWS_S3_ENDPOINT;
+import { AWS_S3_ACCESS_KEY_ID, AWS_S3_BUCKET_NAME, AWS_S3_REGION, AWS_S3_SECRET_ACCESS_KEY } from '../env-vars';
 
 // Initialize S3 client
 export const s3Client = new S3Client({
@@ -16,8 +9,7 @@ export const s3Client = new S3Client({
     accessKeyId: AWS_S3_ACCESS_KEY_ID,
     secretAccessKey: AWS_S3_SECRET_ACCESS_KEY,
   },
-  endpoint,
-  forcePathStyle: true,
+  useAccelerateEndpoint: true,
 });
 
 export const uploadStringAsFileS3 = async (fileKey: string, contents: string) => {
