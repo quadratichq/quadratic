4436fbe2e30d5cfaf5c19f259dafe08f83eed59b
Jim Nielsen <jimniels@gmail.com>
2024-10-23 10:15:41 -0600

    add mixpanel events

--- quadratic-client/src/shared/hooks/useFeatureFlag.tsx
+++ quadratic-client/src/shared/hooks/useFeatureFlag.tsx
@@ -1,4 +1,5 @@
 import useLocalStorage from '@/shared/hooks/useLocalStorage';
+import mixpanel from 'mixpanel-browser';
 
 const initialState = { themeAppearanceMode: false, themeAccentColor: false };
 export type FeatureFlagKey = keyof typeof initialState;
@@ -7,6 +8,13 @@ export const useFeatureFlag = (key: FeatureFlagKey) => {
   const [state, setState] = useLocalStorage('featureFlags', initialState);
 
   const localState = state[key];
-  const setLocalState = (value: boolean) => setState((prevState) => ({ ...prevState, [key]: value }));
+  const setLocalState = (value: boolean) => {
+    if (value) {
+      mixpanel.track('[FeatureFlag].on', { key });
+    } else {
+      mixpanel.track('[FeatureFlag].off', { key });
+    }
+    setState((prevState) => ({ ...prevState, [key]: value }));
+  };
   return [localState, setLocalState] as const;
 };
