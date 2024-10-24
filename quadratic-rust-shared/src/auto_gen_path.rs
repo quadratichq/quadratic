f8aad5a910cbbf279ffb6f7f49a986a29fa4ee77
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-10-24 14:26:53 -0400

    Add `auto_gen_path.rs` to `.gitignore`

--- quadratic-rust-shared/src/auto_gen_path.rs
+++
@@ -1,2 +0,0 @@
-const FIXTURES_PATH: &str = "/Users/davidfig/Programming/quadratic/quadratic-rust-shared/fixtures";
-const DATA_PATH: &str = "/Users/davidfig/Programming/quadratic/quadratic-rust-shared/data";
