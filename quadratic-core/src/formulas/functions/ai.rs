1f67820823e5f1a9c2ced0939837f5d09d722b74
AyushAgrawal-A2 <ayushagl06@gmail.com>
2024-10-29 22:27:49 +0530

    fix merge bug

--- quadratic-core/src/formulas/functions/ai.rs
+++ quadratic-core/src/formulas/functions/ai.rs
@@ -4,7 +4,7 @@ pub const CATEGORY: FormulaFunctionCategory = FormulaFunctionCategory {
     include_in_docs: false,
     include_in_completions: true,
     name: "AI Researcher",
-    docs: "",
+    docs: None,
     get_functions,
 };
 
