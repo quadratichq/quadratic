78a0f179d5de6a2c2b9911214afc6d5817da2876
David Figatner <david@yopeyopey.com>
2024-12-09 05:24:12 -0800

    remove failing old borders test (since it'll be removed)

--- quadratic-core/src/grid/sheet/borders/borders_set.rs
+++ quadratic-core/src/grid/sheet/borders/borders_set.rs
@@ -261,35 +261,4 @@ mod tests {
             Some(BorderStyleCell::default())
         ));
     }
-
-    #[test]
-    #[parallel]
-    fn set_borders_all() {
-        todo!();
-
-        // let sheet_id = SheetId::test();
-        // let mut borders = Borders::default();
-        // assert!(borders.all.left.is_none());
-        // assert!(borders.all.right.is_none());
-        // assert!(borders.all.top.is_none());
-        // assert!(borders.all.bottom.is_none());
-
-        // borders.set_borders_a1(
-        //     &A1Subspaces::All(sheet_id),
-        //     &RunLengthEncoding::repeat(BorderStyleCellUpdate::all(), 1),
-        // );
-        // assert!(borders.all.left.is_some());
-        // assert!(borders.all.right.is_some());
-        // assert!(borders.all.top.is_some());
-        // assert!(borders.all.bottom.is_some());
-
-        // borders.set_borders_a1(
-        //     &A1Subspaces::All(sheet_id),
-        //     &RunLengthEncoding::repeat(BorderStyleCellUpdate::clear(false), 1),
-        // );
-        // assert!(borders.all.left.is_none());
-        // assert!(borders.all.right.is_none());
-        // assert!(borders.all.top.is_none());
-        // assert!(borders.all.bottom.is_none());
-    }
 }
