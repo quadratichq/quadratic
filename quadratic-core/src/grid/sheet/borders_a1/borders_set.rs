4ce3a49cd223a0c6e78255b162c964d53acb2210
David DiMaria <info@collectivesessions.com>
2024-12-10 09:28:33 -0700

    Refactor borders_set

--- quadratic-core/src/grid/sheet/borders_a1/borders_set.rs
+++ quadratic-core/src/grid/sheet/borders_a1/borders_set.rs
@@ -3,6 +3,15 @@ use crate::{controller::operations::operation::Operation, grid::SheetId};
 use super::*;
 
 impl BordersA1 {
+    fn set_borders_item(
+        item: &BordersA1UpdatesType,
+        border_type: &mut BordersA1Type,
+    ) -> BordersA1UpdatesType {
+        item.as_ref()
+            .map(|value| border_type.set_from(&value.map_ref(|value| value.map(Into::into))))
+            .map(|value| value.map_ref(|value| value.map(Into::into)))
+    }
+
     /// Sets the borders for a selection.
     pub fn set_borders_a1(
         &mut self,
@@ -10,38 +19,10 @@ impl BordersA1 {
         borders: &BordersA1Updates,
     ) -> Vec<Operation> {
         let reverse_borders = BordersA1Updates {
-            left: borders
-                .left
-                .as_ref()
-                .map(|value| {
-                    self.left
-                        .set_from(&value.map_ref(|value| value.map(|value| value.into())))
-                })
-                .map(|value| value.map_ref(|value| value.map(|value| value.into()))),
-            right: borders
-                .right
-                .as_ref()
-                .map(|value| {
-                    self.right
-                        .set_from(&value.map_ref(|value| value.map(|value| value.into())))
-                })
-                .map(|value| value.map_ref(|value| value.map(|value| value.into()))),
-            top: borders
-                .top
-                .as_ref()
-                .map(|value| {
-                    self.top
-                        .set_from(&value.map_ref(|value| value.map(|value| value.into())))
-                })
-                .map(|value| value.map_ref(|value| value.map(|value| value.into()))),
-            bottom: borders
-                .bottom
-                .as_ref()
-                .map(|value| {
-                    self.bottom
-                        .set_from(&value.map_ref(|value| value.map(|value| value.into())))
-                })
-                .map(|value| value.map_ref(|value| value.map(|value| value.into()))),
+            left: Self::set_borders_item(&borders.left, &mut self.left),
+            right: Self::set_borders_item(&borders.right, &mut self.right),
+            top: Self::set_borders_item(&borders.top, &mut self.top),
+            bottom: Self::set_borders_item(&borders.bottom, &mut self.bottom),
         };
 
         vec![Operation::SetBordersA1 {
@@ -53,38 +34,10 @@ impl BordersA1 {
     /// Applies the updates to the borders and returns an update to undo the changes.
     pub fn apply_updates(&mut self, updates: &BordersA1Updates) -> BordersA1Updates {
         BordersA1Updates {
-            left: updates
-                .left
-                .as_ref()
-                .map(|value| {
-                    self.left
-                        .set_from(&value.map_ref(|value| value.map(|value| value.into())))
-                })
-                .map(|value| value.map_ref(|value| value.map(|value| value.into()))),
-            right: updates
-                .right
-                .as_ref()
-                .map(|value| {
-                    self.right
-                        .set_from(&value.map_ref(|value| value.map(|value| value.into())))
-                })
-                .map(|value| value.map_ref(|value| value.map(|value| value.into()))),
-            top: updates
-                .top
-                .as_ref()
-                .map(|value| {
-                    self.top
-                        .set_from(&value.map_ref(|value| value.map(|value| value.into())))
-                })
-                .map(|value| value.map_ref(|value| value.map(|value| value.into()))),
-            bottom: updates
-                .bottom
-                .as_ref()
-                .map(|value| {
-                    self.bottom
-                        .set_from(&value.map_ref(|value| value.map(|value| value.into())))
-                })
-                .map(|value| value.map_ref(|value| value.map(|value| value.into()))),
+            left: Self::set_borders_item(&updates.left, &mut self.left),
+            right: Self::set_borders_item(&updates.right, &mut self.right),
+            top: Self::set_borders_item(&updates.top, &mut self.top),
+            bottom: Self::set_borders_item(&updates.bottom, &mut self.bottom),
         }
     }
 }
