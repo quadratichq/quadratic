acf038e5b7ef8775980e6ffa62bf2e5f0e8251ba
Jim Nielsen <jimniels@gmail.com>
2025-01-06 15:35:53 -0700

    update react-router-dom

--- quadratic-client/src/router.tsx
+++ quadratic-client/src/router.tsx
@@ -112,6 +112,10 @@ export const router = createBrowserRouter(
   {
     future: {
       v7_fetcherPersist: true,
+      v7_relativeSplatPath: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
     },
   }
 );
