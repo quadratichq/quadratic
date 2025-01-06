b382b3e74c3bd9926fbeefde189fbc3778c9c73b
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-01-06 17:53:53 +0530

    clippy

--- quadratic-core/src/grid/sheet/validations/validation_col_row.rs
+++ quadratic-core/src/grid/sheet/validations/validation_col_row.rs
@@ -358,7 +358,7 @@ mod tests {
             sheet.validations.warnings.get(&pos![B3]),
             Some(&validation_rect_columns.id)
         );
-        assert!(sheet.validations.warnings.get(&pos![C3]).is_none());
+        assert!(!sheet.validations.warnings.contains_key(&pos![C3]));
         expect_js_call(
             "jsSheetValidations",
             format!(
@@ -510,7 +510,7 @@ mod tests {
             sheet.validations.warnings.get(&pos![C2]),
             Some(&validation_rect_rows.id)
         );
-        assert!(sheet.validations.warnings.get(&pos![C3]).is_none());
+        assert!(!sheet.validations.warnings.contains_key(&pos![C3]));
         expect_js_call(
             "jsSheetValidations",
             format!(
@@ -615,7 +615,7 @@ mod tests {
             sheet.validations.warnings.get(&pos![C3]),
             Some(&validation_rect_cols.id)
         );
-        assert!(sheet.validations.warnings.get(&pos![D3]).is_none());
+        assert!(!sheet.validations.warnings.contains_key(&pos![D3]));
         expect_js_call(
             "jsSheetValidations",
             format!(
@@ -752,7 +752,7 @@ mod tests {
             sheet.validations.warnings.get(&pos![C3]),
             Some(&validation_rect_rows.id)
         );
-        assert!(sheet.validations.warnings.get(&pos![C4]).is_none());
+        assert!(!sheet.validations.warnings.contains_key(&pos![C4]));
         expect_js_call(
             "jsSheetValidations",
             format!(
