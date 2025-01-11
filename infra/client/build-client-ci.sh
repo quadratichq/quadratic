59d56e329e027430c95f9be8cab8b3701c0c0265
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-01-11 11:34:51 +0530

    type parallel

--- infra/client/build-client-ci.sh
+++ infra/client/build-client-ci.sh
@@ -18,70 +18,79 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
 echo 'Installing wasm-pack...'
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+
+echo 'Installing wasm32-unknown-unknown target...'
+rustup target add wasm32-unknown-unknown
 
 echo 'Packaging quadratic_py'
 ./quadratic-kernels/python-wasm/package.sh --no-poetry
 
-cd quadratic-core
+# cd quadratic-core
 
-echo 'Building wasm...'
-wasm-pack build --target web --out-dir ../quadratic-client/src/app/quadratic-core
+# echo 'Building wasm...'
+# wasm-pack build --target web --out-dir ../quadratic-client/src/app/quadratic-core
 
-echo 'Exporting TS/Rust types...'
-cargo run --bin export_types
+# echo 'Exporting TS/Rust types...'
+# cargo run --bin export_types
 
-cd ..
+# cd ..
 
-echo 'Building quadratic-rust-client...'
-npm run build --workspace=quadratic-rust-client
+# echo 'Building quadratic-rust-client...'
+# npm run build --workspace=quadratic-rust-client
+
+echo 'Starting rust builds...'
+(echo 'Building core...' && npm run build --workspace=quadratic-core) & \
+(echo 'Building TS/Rust types...' && npm run export_types --workspace=quadratic-core) & \
+(echo 'Building rust-client...' && npm run build --workspace=quadratic-rust-client) & \
+wait
 
 echo 'Building front-end...'
 npm ci --no-audit --no-fund
 npm run build --workspace=quadratic-client
 
 # Install compression tools
-echo 'Installing compression tools...'
-apt-get update && apt-get install -y brotli pigz
+# echo 'Installing compression tools...'
+# sudo apt-get update && sudo apt-get install -y brotli pigz
 
 # Compress files
-find "/build" -type f \( \
-  -name "*.js" -o \
-  -name "*.mjs" -o \
-  -name "*.cjs" -o \
-  -name "*.jsx" -o \
-  -name "*.ts" -o \
-  -name "*.tsx" -o \
-  -name "*.wasm" -o \
-  -name "*.whl" -o \
-  -name "*.json" -o \
-  -name "*.css" -o \
-  -name "*.html" -o \
-  -name "*.svg" -o \
-  -name "*.txt" -o \
-  -name "*.map" -o \
-  -name "*.py" -o \
-  -name "*.pyc" -o \
-  -name "*.pyi" -o \
-  -name "*.pth" -o \
-  -name "*.data" -o \
-  -name "*.mem" -o \
-  -name "*.wat" -o \
-  -name "*.md" -o \
-  -name "*.rst" -o \
-  -name "*.rtf" -o \
-  -name "*.csv" -o \
-  -name "*.tsv" -o \
-  -name "*.eot" -o \
-  -name "*.ttf" -o \
-  -name "*.otf" -o \
-  -name "*.woff" -o \
-  -name "*.woff2" -o \
-  -name "*.font" -o \
-  -name "*.font-sfnt" \
-\) -print0 | xargs -0 -n1 -P$(nproc) -I {} sh -c '\
-  echo "Compressing: {}" && \
-  brotli -q 9 -w 24 -f "{}" && \
-  pigz -6kf "{}" && \
-  echo "Created: {}.br and {}.gz" \
-'
+# find "./build" -type f \( \
+#   -name "*.js" -o \
+#   -name "*.mjs" -o \
+#   -name "*.cjs" -o \
+#   -name "*.jsx" -o \
+#   -name "*.ts" -o \
+#   -name "*.tsx" -o \
+#   -name "*.wasm" -o \
+#   -name "*.whl" -o \
+#   -name "*.json" -o \
+#   -name "*.css" -o \
+#   -name "*.html" -o \
+#   -name "*.svg" -o \
+#   -name "*.txt" -o \
+#   -name "*.map" -o \
+#   -name "*.py" -o \
+#   -name "*.pyc" -o \
+#   -name "*.pyi" -o \
+#   -name "*.pth" -o \
+#   -name "*.data" -o \
+#   -name "*.mem" -o \
+#   -name "*.wat" -o \
+#   -name "*.md" -o \
+#   -name "*.rst" -o \
+#   -name "*.rtf" -o \
+#   -name "*.csv" -o \
+#   -name "*.tsv" -o \
+#   -name "*.eot" -o \
+#   -name "*.ttf" -o \
+#   -name "*.otf" -o \
+#   -name "*.woff" -o \
+#   -name "*.woff2" -o \
+#   -name "*.font" -o \
+#   -name "*.font-sfnt" \
+# \) -print0 | xargs -0 -n1 -P$(nproc) -I {} sh -c '\
+#   echo "Compressing: {}" && \
+#   brotli -q 9 -w 24 -f "{}" && \
+#   pigz -6kf "{}" && \
+#   echo "Created: {}.br and {}.gz" \
+# '
