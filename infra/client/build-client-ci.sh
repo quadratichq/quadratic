638ff016539f72e3beb536e4b0269a8dc0a1717c
David Kircos <davidkircos@gmail.com>
2025-01-14 15:13:06 -0700

    Update build-client-ci.sh

--- infra/client/build-client-ci.sh
+++ infra/client/build-client-ci.sh
@@ -1,16 +1,5 @@
 set -euo pipefail
 
-# Setting env vars this way will only work on Vercel
-# AWS_PULL_REQUEST_ID is set on Amplify
-if [ "$VERCEL_ENV" == "preview" ]; then
-  export VITE_QUADRATIC_API_URL="https://quadratic-api-dev-pr-$VERCEL_GIT_PULL_REQUEST_ID.herokuapp.com"
-  echo "On preview branch. Setting VITE_QUADRATIC_API_URL to quadratic-api-dev-pr-$VERCEL_GIT_PULL_REQUEST_ID.herokuapp.com"
-  export VITE_QUADRATIC_MULTIPLAYER_URL="wss://multiplayer-pr-$VERCEL_GIT_PULL_REQUEST_ID.quadratic-preview.com/ws"
-  echo "On preview branch. Setting VITE_QUADRATIC_MULTIPLAYER_URL to wss://multiplayer-pr-$VERCEL_GIT_PULL_REQUEST_ID.quadratic-preview.com/ws"
-  export VITE_QUADRATIC_CONNECTION_URL="https://connection-pr-$VERCEL_GIT_PULL_REQUEST_ID.quadratic-preview.com"
-  echo "On preview branch. Setting VITE_QUADRATIC_CONNECTION_URL to https://connector-pr-$VERCEL_GIT_PULL_REQUEST_ID.quadratic-preview.com"
-fi
-
 # These commands are used in production on AWS Amplify
 
 echo 'Installing rustup...'
