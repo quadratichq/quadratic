6ae88532463cf8a77e9d275cd660a487679976a9
Luke <146771258+luke-quadratic@users.noreply.github.com>
2025-03-03 17:32:13 -0700

    update

--- quadratic-shared/ai/specs/aiToolsSpec.ts
+++ quadratic-shared/ai/specs/aiToolsSpec.ts
@@ -217,7 +217,6 @@ Use this function to add data to the currently open sheet. Don't use code cell f
 This function requires the top_left_position (in a1 notation) and the 2d array of strings representing the cell values to set. Values are string representation of text, number, logical, time instant, duration, error, html, code, image, date, time or blank.\n
 Values set using this function will replace the existing values in the cell and can be referenced in the code cells immediately. Always refer to the cell by its position on respective sheet, in a1 notation. Don't add these in code cells.\n
 To clear the values of a cell, set the value to an empty string.\n
-Never use set_cell_values function to set cells inside tables to formulas or code.\n 
 `,
   },
   [AITool.SetCodeCellValue]: {
@@ -227,7 +226,6 @@ Sets the value of a code cell and run it in the currently open sheet, requires t
 You should use the set_code_cell_value function to set this code cell value. Use this function instead of responding with code.\n
 Never use set_code_cell_value function to set the value of a cell to a value that is not code. Don't add static data to the currently open sheet using set_code_cell_value function, use set_cell_values instead. set_code_cell_value function is only meant to set the value of a cell to code.\n
 Always refer to the data from cell by its position in a1 notation from respective sheet. Don't add values manually in code cells.\n
-Never use set_code_cell_value function to set cells inside tables to formulas or code.\n 
 `,
     parameters: {
       type: 'object',
