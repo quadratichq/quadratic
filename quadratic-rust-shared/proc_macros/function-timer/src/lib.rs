669d7ddae9ede7ae00411f4db23f220b67b68472
David Figatner <david@yopeyopey.com>
2025-03-08 08:25:15 -0800

    fix code peek

--- quadratic-rust-shared/proc_macros/function-timer/src/lib.rs
+++ quadratic-rust-shared/proc_macros/function-timer/src/lib.rs
@@ -1,46 +1,46 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{ItemFn, parse_macro_input};
+// use proc_macro::TokenStream;
+// use quote::quote;
+// use syn::{parse_macro_input, ItemFn};
 
-#[proc_macro_attribute]
-pub fn function_timer(_attrs: TokenStream, item: TokenStream) -> TokenStream {
-    if cfg!(not(feature = "function-timer")) {
-        return item;
-    }
+// #[proc_macro_attribute]
+// pub fn function_timer(_attrs: TokenStream, item: TokenStream) -> TokenStream {
+//     if cfg!(not(feature = "function-timer")) {
+//         return item;
+//     }
 
-    let input_fn = parse_macro_input!(item as syn::ItemFn);
-    let fn_name = &input_fn.sig.ident.to_string();
+//     let input_fn = parse_macro_input!(item as syn::ItemFn);
+//     let fn_name = &input_fn.sig.ident.to_string();
 
-    let ItemFn {
-        attrs,
-        vis,
-        sig,
-        block,
-    } = input_fn;
+//     let ItemFn {
+//         attrs,
+//         vis,
+//         sig,
+//         block,
+//     } = input_fn;
 
-    let expanded = quote! {
-            #(#attrs)*
-            #vis #sig {
-            let start = chrono::Utc::now();
-            let out = #block;
-            let end = chrono::Utc::now();
-            let duration = end - start;
+//     let expanded = quote! {
+//             #(#attrs)*
+//             #vis #sig {
+//             let start = chrono::Utc::now();
+//             let out = #block;
+//             let end = chrono::Utc::now();
+//             let duration = end - start;
 
-            dbgjs!(format!("Function {} took {} ms", #fn_name, duration.num_milliseconds()));
+//             dbgjs!(format!("Function {} took {} ms", #fn_name, duration.num_milliseconds()));
 
-            out
-        }
-    };
+//             out
+//         }
+//     };
 
-    TokenStream::from(expanded)
-}
+//     TokenStream::from(expanded)
+// }
 
-#[cfg(test)]
-mod tests {
+// #[cfg(test)]
+// mod tests {
 
-    #[test]
-    fn test_function_timer() {
-        let t = trybuild::TestCases::new();
-        t.pass("tests/*.rs");
-    }
-}
+//     #[test]
+//     fn test_function_timer() {
+//         let t = trybuild::TestCases::new();
+//         t.pass("tests/*.rs");
+//     }
+// }
