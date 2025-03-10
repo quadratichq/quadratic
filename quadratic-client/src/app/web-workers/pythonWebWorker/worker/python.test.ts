eb33236fd9060db85d6791b9c454a7411c4d6879
David Kircos <davidkircos@gmail.com>
2025-03-10 12:37:21 -0600

    add one type

--- quadratic-client/src/app/web-workers/pythonWebWorker/worker/python.test.ts
+++ quadratic-client/src/app/web-workers/pythonWebWorker/worker/python.test.ts
@@ -25,7 +25,7 @@ vi.mock('./pythonCore.ts', () => {
 });
 
 beforeAll(async () => {
-  return new Promise((resolve) => {
+  return new Promise<void>((resolve) => {
     const interval = setInterval(() => {
       if (pythonState === 'ready') {
         clearInterval(interval);
