969ca9c3b5dbade7ff8d232963a3a3638fd8c2c2
David Kircos <davidkircos@gmail.com>
2025-03-20 17:05:19 -0600

    update to shadcn skeleton

--- quadratic-client/src/app/ai/toolCards/UserPromptSuggestions.tsx
+++ quadratic-client/src/app/ai/toolCards/UserPromptSuggestions.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from '@mui/material';
+import { Skeleton } from '@/shared/shadcn/ui/skeleton';
 
 type UserPromptSuggestionsProps = {
   args: string;
@@ -8,11 +8,17 @@ type UserPromptSuggestionsProps = {
 export const UserPromptSuggestions = ({ loading }: UserPromptSuggestionsProps) => {
   if (loading) {
     return (
-      <>
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-      </>
+      <div>
+        <div className="mb-2 flex flex-row">
+          <Skeleton className="mb-2 mr-4 h-6 w-6  rounded-md" />
+          <Skeleton className="mb-2 h-6 w-6 rounded-md" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-8 w-full rounded-md" />
+          <Skeleton className="h-8 w-full rounded-md" />
+          <Skeleton className="h-8 w-full rounded-md" />
+        </div>
+      </div>
     );
   }
 
