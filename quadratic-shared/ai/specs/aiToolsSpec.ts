dbc6d42708deb257cbc57b21407a1226a1dbbea0
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-03-20 23:37:25 +0530

    currently

--- quadratic-shared/ai/specs/aiToolsSpec.ts
+++ quadratic-shared/ai/specs/aiToolsSpec.ts
@@ -164,11 +164,11 @@ This name should be from user's perspective, not the assistant's.\n
   [AITool.AddDataTable]: {
     sources: ['AIAnalyst'],
     description: `
-Adds a data table to the currently open sheet, requires the top left cell position (in a1 notation), the name of the data table and the data to add. The data should be a 2d array of strings, where each sub array represents a row of values.\n
+Adds a data table to the current open sheet, requires the top left cell position (in a1 notation), the name of the data table and the data to add. The data should be a 2d array of strings, where each sub array represents a row of values.\n
 The first row of the data table is considered to be the header row, and the data table will be created with the first row as the header row.\n
-Always use this tool when adding a new tabular data to the currently open sheet. Don't use set_cell_values function to add tabular data.\n
+Always use this tool when adding a new tabular data to the current open sheet. Don't use set_cell_values function to add tabular data.\n
 Don't use this tool to add data to an existing data table. Use set_cell_values function to add data to an existing data table.\n
-Always prefer using this tool to add structured data to the currently open sheet. Don't use set_cell_values or set_code_cell_value function to add structured data.\n
+Always prefer using this tool to add structured data to the current open sheet. Don't use set_cell_values or set_code_cell_value function to add structured data.\n
 `,
     parameters: {
       type: 'object',
@@ -176,12 +176,12 @@ Always prefer using this tool to add structured data to the currently open sheet
         top_left_position: {
           type: 'string',
           description:
-            'The top left position of the data table on the currently open sheet, in a1 notation. This should be a single cell, not a range.',
+            'The top left position of the data table on the current open sheet, in a1 notation. This should be a single cell, not a range.',
         },
         table_name: {
           type: 'string',
           description:
-            "The name of the data table to add to the currently open sheet. This should be a concise and descriptive name of the data table. Don't use special characters or spaces in the name. Always use a unique name for the data table. Spaces, if any, in name are replaced with underscores.",
+            "The name of the data table to add to the current open sheet. This should be a concise and descriptive name of the data table. Don't use special characters or spaces in the name. Always use a unique name for the data table. Spaces, if any, in name are replaced with underscores.",
         },
         table_data: {
           type: 'array',
@@ -199,10 +199,10 @@ Always prefer using this tool to add structured data to the currently open sheet
     },
     responseSchema: AIToolsArgsSchema[AITool.AddDataTable],
     prompt: `
-Adds a data table to the currently open sheet, requires the top_left_position (in a1 notation), the name of the data table and the data to add. The data should be a 2d array of strings, where each sub array represents a row of values.\n
+Adds a data table to the current open sheet, requires the top_left_position (in a1 notation), the name of the data table and the data to add. The data should be a 2d array of strings, where each sub array represents a row of values.\n
 top_left_position is the anchor position of the data table.\n
 The first row of the data table is considered to be the header row, and the data table will be created with the first row as the header row.\n
-Always use this tool when adding a new tabular data to the currently open sheet. Don't use set_cell_values function to add tabular data.\n
+Always use this tool when adding a new tabular data to the current open sheet. Don't use set_cell_values function to add tabular data.\n
 Don't use this tool to add data to a data table that already exists. Use set_cell_values function to add data to a data table that already exists.\n
 All values can be referenced in the code cells immediately. Always refer to the cell by its position on respective sheet, in a1 notation. Don't add values manually in code cells.\n
 To delete a data table, use set_cell_values function with the top_left_position of the data table and with just one empty string value at the top_left_position. Overwriting the top_left_position (anchor position) deletes the data table.\n
@@ -212,10 +212,10 @@ Don't attempt to add formulas or code to data tables.\n
   [AITool.SetCellValues]: {
     sources: ['AIAnalyst'],
     description: `
-Sets the values of the currently open sheet cells to a 2d array of strings, requires the top_left_position (in a1 notation) and the 2d array of strings representing the cell values to set.\n
-Use set_cell_values function to add data to the currently open sheet. Don't use code cell for adding data. Always add data using this function.\n
+Sets the values of the current open sheet cells to a 2d array of strings, requires the top_left_position (in a1 notation) and the 2d array of strings representing the cell values to set.\n
+Use set_cell_values function to add data to the current open sheet. Don't use code cell for adding data. Always add data using this function.\n
 Values are string representation of text, number, logical, time instant, duration, error, html, code, image, date, time or blank.\n
-top_left_position is the position of the top left corner of the 2d array of values on the currently open sheet, in a1 notation. This should be a single cell, not a range. Each sub array represents a row of values.\n
+top_left_position is the position of the top left corner of the 2d array of values on the current open sheet, in a1 notation. This should be a single cell, not a range. Each sub array represents a row of values.\n
 All values can be referenced in the code cells immediately. Always refer to the cell by its position on respective sheet, in a1 notation. Don't add values manually in code cells.\n
 To clear the values of a cell, set the value to an empty string.\n
 `,
@@ -225,7 +225,7 @@ To clear the values of a cell, set the value to an empty string.\n
         top_left_position: {
           type: 'string',
           description:
-            'The position of the top left cell, in a1 notation, in the currently open sheet. This is the top left corner of the added 2d array of values on the currently open sheet. This should be a single cell, not a range.',
+            'The position of the top left cell, in a1 notation, in the current open sheet. This is the top left corner of the added 2d array of values on the current open sheet. This should be a single cell, not a range.',
         },
         cell_values: {
           type: 'array',
@@ -243,8 +243,8 @@ To clear the values of a cell, set the value to an empty string.\n
     },
     responseSchema: AIToolsArgsSchema[AITool.SetCellValues],
     prompt: `
-You should use the set_cell_values function to set the values of the currently open sheet cells to a 2d array of strings.\n
-Use this function to add data to the currently open sheet. Don't use code cell for adding data. Always add data using this function.\n
+You should use the set_cell_values function to set the values of the current open sheet cells to a 2d array of strings.\n
+Use this function to add data to the current open sheet. Don't use code cell for adding data. Always add data using this function.\n
 This function requires the top_left_position (in a1 notation) and the 2d array of strings representing the cell values to set. Values are string representation of text, number, logical, time instant, duration, error, html, code, image, date, time or blank.\n
 Values set using this function will replace the existing values in the cell and can be referenced in the code cells immediately. Always refer to the cell by its position on respective sheet, in a1 notation. Don't add these in code cells.\n
 To clear the values of a cell, set the value to an empty string.\n
@@ -253,10 +253,10 @@ To clear the values of a cell, set the value to an empty string.\n
   [AITool.SetCodeCellValue]: {
     sources: ['AIAnalyst'],
     description: `
-Sets the value of a code cell and runs it in the currently open sheet, requires the language, cell position (in a1 notation), code string and the width and height of the code output on running this Code in the currently open sheet.\n
+Sets the value of a code cell and runs it in the current open sheet, requires the language, cell position (in a1 notation), code string and the width and height of the code output on running this Code in the current open sheet.\n
 Default output size of a new plot/chart is 7 wide * 23 tall cells.\n
 You should use the set_code_cell_value function to set this code cell value. Use this function instead of responding with code.\n
-Never use set_code_cell_value function to set the value of a cell to a value that is not code. Don't add static data to the currently open sheet using set_code_cell_value function, use set_cell_values instead. set_code_cell_value function is only meant to set the value of a cell to code.\n
+Never use set_code_cell_value function to set the value of a cell to a value that is not code. Don't add static data to the current open sheet using set_code_cell_value function, use set_cell_values instead. set_code_cell_value function is only meant to set the value of a cell to code.\n
 Always refer to the data from cell by its position in a1 notation from respective sheet. Don't add values manually in code cells.\n
 `,
     parameters: {
@@ -270,7 +270,7 @@ Always refer to the data from cell by its position in a1 notation from respectiv
         code_cell_position: {
           type: 'string',
           description:
-            'The position of the code cell in the currently open sheet, in a1 notation. This should be a single cell, not a range.',
+            'The position of the code cell in the current open sheet, in a1 notation. This should be a single cell, not a range.',
         },
         code_string: {
           type: 'string',
@@ -279,12 +279,12 @@ Always refer to the data from cell by its position in a1 notation from respectiv
         output_width: {
           type: 'number',
           description:
-            'The width, i.e. number of columns, of the code output on running this Code in the currently open spreadsheet',
+            'The width, i.e. number of columns, of the code output on running this Code in the current open spreadsheet',
         },
         output_height: {
           type: 'number',
           description:
-            'The height, i.e. number of rows, of the code output on running this Code in the currently open spreadsheet',
+            'The height, i.e. number of rows, of the code output on running this Code in the current open spreadsheet',
         },
       },
       required: ['code_cell_language', 'code_cell_position', 'code_string', 'output_width', 'output_height'],
@@ -293,18 +293,18 @@ Always refer to the data from cell by its position in a1 notation from respectiv
     responseSchema: AIToolsArgsSchema[AITool.SetCodeCellValue],
     prompt: `
 You should use the set_code_cell_value function to set this code cell value. Use set_code_cell_value function instead of responding with code.\n
-Never use set_code_cell_value function to set the value of a cell to a value that is not code. Don't add data to the currently open sheet using set_code_cell_value function, use set_cell_values instead. set_code_cell_value function is only meant to set the value of a cell to code.\n
-set_code_cell_value function requires language, codeString, the cell position (single cell in a1 notation) and the width and height of the code output on running this Code in the currently open sheet.\n
+Never use set_code_cell_value function to set the value of a cell to a value that is not code. Don't add data to the current open sheet using set_code_cell_value function, use set_cell_values instead. set_code_cell_value function is only meant to set the value of a cell to code.\n
+set_code_cell_value function requires language, codeString, the cell position (single cell in a1 notation) and the width and height of the code output on running this Code in the current open sheet.\n
 Always refer to the cells on sheet by its position in a1 notation, using q.cells function. Don't add values manually in code cells.\n
 The required location code_cell_position for this code cell is one which satisfies the following conditions:\n
- - The code cell location should be empty and should have enough space to the right and below to accommodate the code result. If there is a value in a single cell where the code result is suppose to go, it will result in spill error. Use currently open sheet context to identify empty space.\n
+ - The code cell location should be empty and should have enough space to the right and below to accommodate the code result. If there is a value in a single cell where the code result is suppose to go, it will result in spill error. Use current open sheet context to identify empty space.\n
  - The code cell should be near the data it references, so that it is easy to understand the code in the context of the data. Identify the data being referred from code and use a cell close to it. If multiple data references are being made, choose the one which is most used or most important. This will make it easy to understand the code in the context of the table.\n
  - If the referenced data is portrait in a table format, the code cell should be next to the top right corner of the table.\n
  - If the referenced data is landscape in a table format, the code cell should be below the bottom left corner of the table.\n
  - Always leave a blank row / column between the code cell and the data it references.\n
  - In case there is not enough empty space near the referenced data, choose a distant empty cell which is in the same row as the top right corner of referenced data and to the right of this data.\n
- - If there are multiple tables or data sources being referenced, place the code cell in a location that provides a good balance between proximity to all referenced data and maintaining readability of the currently open sheet.\n
- - Consider the overall layout and organization of the currently open sheet when placing the code cell, ensuring it doesn't disrupt existing data or interfere with other code cells.\n
+ - If there are multiple tables or data sources being referenced, place the code cell in a location that provides a good balance between proximity to all referenced data and maintaining readability of the current open sheet.\n
+ - Consider the overall layout and organization of the current open sheet when placing the code cell, ensuring it doesn't disrupt existing data or interfere with other code cells.\n
  - A plot returned by the code cell occupies space on the sheet and spills if there is any data present in the sheet where the plot is suppose to take place. Default output size of a new plot is 7 wide * 23 tall cells.\n
  - Do not use conditional returns in python code cells.\n
  - Don't prefix formulas with \`=\` in code cells.\n
@@ -313,10 +313,10 @@ The required location code_cell_position for this code cell is one which satisfi
   [AITool.MoveCells]: {
     sources: ['AIAnalyst'],
     description: `
-Moves a rectangular selection of cells from one location to another on the currently open sheet, requires the source and target locations.\n
-You should use the move_cells function to move a rectangular selection of cells from one location to another on the currently open sheet.\n
+Moves a rectangular selection of cells from one location to another on the current open sheet, requires the source and target locations.\n
+You should use the move_cells function to move a rectangular selection of cells from one location to another on the current open sheet.\n
 move_cells function requires the source and target locations. Source location is the top left and bottom right corners of the selection rectangle to be moved.\n
-Target location is the top left corner of the target location on the currently open sheet.\n
+Target location is the top left corner of the target location on the current open sheet.\n
 `,
     parameters: {
       type: 'object',
@@ -324,12 +324,12 @@ Target location is the top left corner of the target location on the currently o
         source_selection_rect: {
           type: 'string',
           description:
-            'The selection of cells, in a1 notation, to be moved in the currently open sheet. This is string representation of the rectangular selection of cells to be moved',
+            'The selection of cells, in a1 notation, to be moved in the current open sheet. This is string representation of the rectangular selection of cells to be moved',
         },
         target_top_left_position: {
           type: 'string',
           description:
-            'The top left position of the target location on the currently open sheet, in a1 notation. This should be a single cell, not a range. This will be the top left corner of the source selection rectangle after moving.',
+            'The top left position of the target location on the current open sheet, in a1 notation. This should be a single cell, not a range. This will be the top left corner of the source selection rectangle after moving.',
         },
       },
       required: ['source_selection_rect', 'target_top_left_position'],
@@ -337,16 +337,16 @@ Target location is the top left corner of the target location on the currently o
     },
     responseSchema: AIToolsArgsSchema[AITool.MoveCells],
     prompt: `
-You should use the move_cells function to move a rectangular selection of cells from one location to another on the currently open sheet.\n
+You should use the move_cells function to move a rectangular selection of cells from one location to another on the current open sheet.\n
 move_cells function requires the source selection and target position. Source selection is the string representation (in a1 notation) of a selection rectangle to be moved.\n
-Target position is the top left corner of the target position on the currently open sheet, in a1 notation. This should be a single cell, not a range.\n
+Target position is the top left corner of the target position on the current open sheet, in a1 notation. This should be a single cell, not a range.\n
 `,
   },
   [AITool.DeleteCells]: {
     sources: ['AIAnalyst'],
     description: `
 Deletes the value(s) of a selection of cells, requires a string representation of a selection of cells to delete. Selection can be a single cell or a range of cells or multiple ranges in a1 notation.\n
-You should use the delete_cells function to delete the value(s) of a selection of cells on the currently open sheet.\n
+You should use the delete_cells function to delete the value(s) of a selection of cells on the current open sheet.\n
 delete_cells functions requires a string representation (in a1 notation) of a selection of cells to delete. Selection can be a single cell or a range of cells or multiple ranges in a1 notation.\n
 `,
     parameters: {
@@ -363,7 +363,7 @@ delete_cells functions requires a string representation (in a1 notation) of a se
     },
     responseSchema: AIToolsArgsSchema[AITool.DeleteCells],
     prompt: `
-You should use the delete_cells function to delete value(s) on the currently open sheet.\n
+You should use the delete_cells function to delete value(s) on the current open sheet.\n
 delete_cells functions requires a string representation (in a1 notation) of a selection of cells to delete. Selection can be a single cell or a range of cells or multiple ranges in a1 notation.\n
 `,
   },
