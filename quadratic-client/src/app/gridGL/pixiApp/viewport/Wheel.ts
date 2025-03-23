7d25da91703651bb53c03b959af541a33a0b720a
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-03-24 05:13:26 +0530

    remove @pixi/math

--- quadratic-client/src/app/gridGL/pixiApp/viewport/Wheel.ts
+++ quadratic-client/src/app/gridGL/pixiApp/viewport/Wheel.ts
@@ -3,10 +3,10 @@
 import { events } from '@/app/events/events';
 import { CELL_HEIGHT } from '@/shared/constants/gridConstants';
 import { isMac } from '@/shared/utils/isMac';
-import type { IPointData } from '@pixi/math';
-import { Point } from '@pixi/math';
 import type { Viewport } from 'pixi-viewport';
 import { Plugin } from 'pixi-viewport';
+import type { IPointData } from 'pixi.js';
+import { Point } from 'pixi.js';
 
 export const SCALE_OUT_OF_BOUNDS_SCROLL = 0.1;
 
