19f198d0f0b8961cbe52fd7ad2fadbdb318b5c8c
David DiMaria <info@collectivesessions.com>
2025-03-27 13:12:25 -0600

    Add dev dependencies to quadratic-rust-shared

--- quadratic-rust-shared/src/test/benchmark.rs
+++ quadratic-rust-shared/src/test/benchmark.rs
@@ -4,7 +4,7 @@ use criterion::{Bencher, Criterion};
 use std::sync::{LazyLock, Mutex};
 use std::time::Duration;
 
-#[cfg(feature = "test")]
+#[cfg(any(test, feature = "test"))]
 use tabled::{
     builder::Builder,
     settings::{Color, Modify, Style},
