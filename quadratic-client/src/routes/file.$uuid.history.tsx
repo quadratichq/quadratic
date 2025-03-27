b6d46aaad9788463bc8710f883fa07d0dbdfa6ae
David Kircos <davidkircos@gmail.com>
2025-03-27 12:40:25 -0600

    small copy change

--- quadratic-client/src/routes/file.$uuid.history.tsx
+++ quadratic-client/src/routes/file.$uuid.history.tsx
@@ -178,7 +178,7 @@ export const Component = () => {
           <iframe src={iframeUrl} title="App" className="h-full w-full" />
         ) : (
           <p className="flex h-full w-full flex-col items-center justify-center text-sm text-muted-foreground">
-            Select a file version on the left to preview it…
+            Select a file version on the left to preview it.
           </p>
         )}
       </div>
