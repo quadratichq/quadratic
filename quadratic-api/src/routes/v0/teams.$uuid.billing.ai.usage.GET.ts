9d73fec104a512f60ac208f0fe20b3f17d2d97bf
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-04-11 03:09:43 +0530

    chore: change ai usage calculation to team > user

--- quadratic-api/src/routes/v0/teams.$uuid.billing.ai.usage.GET.ts
+++ quadratic-api/src/routes/v0/teams.$uuid.billing.ai.usage.GET.ts
@@ -1,18 +1,19 @@
 import { SubscriptionStatus } from '@prisma/client';
-import type { Request, Response } from 'express';
+import type { Response } from 'express';
 import type { ApiTypes } from 'quadratic-shared/typesAndSchemas';
 import z from 'zod';
 import {
   BillingAIUsageForCurrentMonth,
   BillingAIUsageLimitExceeded,
-  BillingAIUsageMonthlyForUser,
+  BillingAIUsageMonthlyForUserInTeam,
 } from '../../billing/AIUsageHelpers';
+import dbClient from '../../dbClient';
 import { BILLING_AI_USAGE_LIMIT } from '../../env-vars';
-import { getTeam } from '../../middleware/getTeam';
 import { userMiddleware } from '../../middleware/user';
 import { validateAccessToken } from '../../middleware/validateAccessToken';
 import { validateRequestSchema } from '../../middleware/validateRequestSchema';
 import type { RequestWithUser } from '../../types/Request';
+import { ApiError } from '../../utils/ApiError';
 
 export default [
   validateRequestSchema(
@@ -27,38 +28,54 @@ export default [
   handler,
 ];
 
-async function handler(req: Request, res: Response) {
+async function handler(req: RequestWithUser, res: Response<ApiTypes['/v0/teams/:uuid/billing/ai/usage.GET.response']>) {
   const {
     params: { uuid },
     user: { id: userId },
-  } = req as RequestWithUser;
+  } = req;
 
   // If the billing limit is not set, we don't need to check if the user has exceeded it
   if (!BILLING_AI_USAGE_LIMIT) {
     return res.status(200).json({ exceededBillingLimit: false });
   }
 
-  // Check if the user is on a paid team
-  const team = await getTeam({ uuid, userId: userId });
-  if (team) {
-    const isOnPaidTeam = team.team.stripeSubscriptionStatus === SubscriptionStatus.ACTIVE;
+  // Lookup the team
+  const team = await dbClient.team.findUnique({
+    where: {
+      uuid,
+    },
+  });
+  if (team === null) {
+    throw new ApiError(404, 'Team not found');
+  }
+
+  // Get the user's role in this team
+  const userTeamRole = await dbClient.userTeamRole.findUnique({
+    where: {
+      userId_teamId: {
+        userId,
+        teamId: team.id,
+      },
+    },
+  });
+
+  // Check if the user is member of this team and team is on a paid plan
+  if (userTeamRole) {
+    const isOnPaidTeam = team.stripeSubscriptionStatus === SubscriptionStatus.ACTIVE;
     if (isOnPaidTeam) {
       return res.status(200).json({ exceededBillingLimit: false });
     }
   }
 
-  // Get the user's monthly AI usage
-  const usage = await BillingAIUsageMonthlyForUser(userId);
+  // Get usage
+  const usage = await BillingAIUsageMonthlyForUserInTeam(userId, team.id);
+  const exceededBillingLimit = BillingAIUsageLimitExceeded(usage);
   const currentPeriodUsage = BillingAIUsageForCurrentMonth(usage);
 
-  // Check if the user has exceeded the billing limit
-  const exceededBillingLimit = BillingAIUsageLimitExceeded(usage);
-
-  const data: ApiTypes['/v0/teams/:uuid/billing/ai/usage.GET.response'] = {
+  const data = {
     exceededBillingLimit,
     billingLimit: BILLING_AI_USAGE_LIMIT,
     currentPeriodUsage,
   };
-
   return res.status(200).json(data);
 }
