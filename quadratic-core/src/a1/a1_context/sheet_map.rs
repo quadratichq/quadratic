aa33cc90ad11df5b8c49f22b9e95d6f593f235b3
HactarCE <6060305+HactarCE@users.noreply.github.com>
2025-04-11 12:51:44 -0400

    `name_to_id` -> `folded_name_to_id`

--- quadratic-core/src/a1/a1_context/sheet_map.rs
+++ quadratic-core/src/a1/a1_context/sheet_map.rs
@@ -13,7 +13,7 @@ use crate::{
 #[derive(Default, Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct SheetMap {
     /// Map from case-folded name to sheet ID.
-    name_to_id: HashMap<String, SheetId>,
+    folded_name_to_id: HashMap<String, SheetId>,
     /// Map from sheet ID to name (not case-folded).
     id_to_name: HashMap<SheetId, String>,
 }
@@ -25,14 +25,15 @@ impl SheetMap {
     }
     /// Adds a sheet to the map given just its name and ID.
     pub fn insert_parts(&mut self, sheet_name: &str, sheet_id: SheetId) {
-        self.name_to_id.insert(case_fold(sheet_name), sheet_id);
+        self.folded_name_to_id
+            .insert(case_fold(sheet_name), sheet_id);
         self.id_to_name.insert(sheet_id, sheet_name.to_string());
     }
 
     /// Returns a sheet ID from its name. The name will be automatically
     /// case-folded.
     pub fn try_sheet_name(&self, sheet_name: &str) -> Option<SheetId> {
-        self.name_to_id.get(&case_fold(sheet_name)).copied()
+        self.folded_name_to_id.get(&case_fold(sheet_name)).copied()
     }
     /// Returns a sheet name from its ID.
     pub fn try_sheet_id(&self, sheet_id: SheetId) -> Option<&String> {
@@ -41,15 +42,17 @@ impl SheetMap {
 
     /// Removes the sheet with the given name and returns its ID.
     pub fn remove_name(&mut self, name: &str) -> Option<SheetId> {
-        self.name_to_id.remove(&case_fold(name)).map(|sheet_id| {
-            self.id_to_name.remove(&sheet_id);
-            sheet_id
-        })
+        self.folded_name_to_id
+            .remove(&case_fold(name))
+            .map(|sheet_id| {
+                self.id_to_name.remove(&sheet_id);
+                sheet_id
+            })
     }
     /// Removes the sheet with the given ID and returns its name.
     pub fn remove_sheet_id(&mut self, sheet_id: SheetId) -> Option<String> {
         self.id_to_name.remove(&sheet_id).map(|name| {
-            self.name_to_id.remove(&case_fold(&name));
+            self.folded_name_to_id.remove(&case_fold(&name));
             name
         })
     }
