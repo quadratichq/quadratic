fbdef1b19ee9d381bd2b20b1a80beb0e02a04b38
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-04-13 08:26:47 +0530

    fix move_tables_to_left

--- quadratic-core/src/grid/sheet/col_row/columns_delete_table.rs
+++ quadratic-core/src/grid/sheet/col_row/columns_delete_table.rs
@@ -1,5 +1,5 @@
 use crate::{
-    Pos, SheetRect,
+    Pos,
     controller::{
         active_transactions::pending_transaction::PendingTransaction,
         operations::operation::Operation,
@@ -98,7 +98,7 @@ impl Sheet {
         transaction: &mut PendingTransaction,
         columns: &Vec<i64>,
     ) {
-        let mut dt_to_delete = Vec::new();
+        let mut dt_to_update = Vec::new();
 
         // we need to shift the anchor if the first column is deleted
         let mut dt_to_shift_anchor = Vec::new();
@@ -109,14 +109,14 @@ impl Sheet {
             }
             let output_rect = dt.output_rect(*pos, false);
             let mut old_dt: Option<DataTable> = None;
-            let mut new_x = None;
+            let mut new_anchor_x = None;
             for col in columns {
                 if *col >= output_rect.min.x && *col <= output_rect.max.x {
                     if *col == output_rect.min.x {
                         if let Some(first_surviving_col) =
                             output_rect.x_range().find(|col| !columns.contains(col))
                         {
-                            new_x = Some(first_surviving_col);
+                            new_anchor_x = Some(first_surviving_col);
                         } else {
                             // this should never happen
                             dbgjs!(
@@ -145,15 +145,15 @@ impl Sheet {
                     }
                 }
             }
-            if let Some(new_x) = new_x {
-                dt_to_shift_anchor.push((*pos, old_dt, index, new_x));
+            if let Some(new_anchor_x) = new_anchor_x {
+                dt_to_shift_anchor.push((*pos, old_dt, index, new_anchor_x));
             } else {
-                dt_to_delete.push((*pos, old_dt, index));
+                dt_to_update.push((*pos, old_dt, index));
             }
         }
 
         // create undo and signal client of changes
-        for (pos, old_dt, index) in dt_to_delete {
+        for (pos, old_dt, index) in dt_to_update {
             if let (Some(old_dt), Some(_cell_value)) = (old_dt, self.cell_value(pos)) {
                 transaction.add_from_code_run(self.id, pos, old_dt.is_image(), old_dt.is_html());
                 transaction.add_dirty_hashes_from_sheet_rect(
@@ -170,13 +170,13 @@ impl Sheet {
         }
 
         // ensure anchor cell survives by shifting it to the right of the deleted columns
-        for (pos, old_dt, index, first_surviving_col) in dt_to_shift_anchor {
+        for (pos, old_dt, index, new_anchor_x) in dt_to_shift_anchor {
             if let (Some(old_dt), Some(cell_value)) = (old_dt, self.cell_value(pos)) {
                 transaction.add_from_code_run(self.id, pos, old_dt.is_image(), old_dt.is_html());
                 transaction.add_dirty_hashes_from_sheet_rect(
                     old_dt.output_rect(pos, false).to_sheet_rect(self.id),
                 );
-                let new_pos = Pos::new(first_surviving_col, pos.y);
+                let new_pos = Pos::new(new_anchor_x, pos.y);
                 self.move_cell_value(pos, new_pos);
                 transaction
                     .reverse_operations
@@ -203,7 +203,7 @@ impl Sheet {
     ) {
         let mut dt_to_shift_left = Vec::new();
         for (pos, table) in self.data_tables.iter() {
-            let output_rect = table.output_rect(*pos, false);
+            let mut output_rect = table.output_rect(*pos, false);
 
             // check how many deleted columns are before the table
             let mut shift_table = 0;
@@ -214,41 +214,30 @@ impl Sheet {
             }
             if shift_table > 0 {
                 transaction.add_dirty_hashes_from_sheet_rect(output_rect.to_sheet_rect(self.id));
-                let adjusted_pos = Pos {
-                    x: pos.x - shift_table as i64,
-                    y: pos.y,
-                };
-                transaction.add_dirty_hashes_from_sheet_rect(SheetRect {
-                    sheet_id: self.id,
-                    min: adjusted_pos,
-                    max: Pos {
-                        x: adjusted_pos.x + table.width() as i64,
-                        y: pos.y,
-                    },
-                });
                 transaction.add_from_code_run(self.id, *pos, table.is_image(), table.is_html());
+
+                output_rect.translate(-shift_table, 0);
+                transaction.add_dirty_hashes_from_sheet_rect(output_rect.to_sheet_rect(self.id));
                 transaction.add_from_code_run(
                     self.id,
-                    adjusted_pos,
+                    pos.translate(-shift_table, 0, 1, 1),
                     table.is_image(),
                     table.is_html(),
                 );
+
                 dt_to_shift_left.push((*pos, shift_table));
             }
         }
         for (pos, shift_table) in dt_to_shift_left {
-            let Some((_, old_dt)) = self.data_tables.shift_remove_entry(&pos) else {
+            let Some((index, _, old_dt)) = self.data_tables.shift_remove_full(&pos) else {
                 dbgjs!(format!(
                     "Error in check_delete_tables_columns: cannot shift left data table\n{:?}",
                     pos
                 ));
                 continue;
             };
-            let new_pos = Pos {
-                x: pos.x - shift_table as i64,
-                y: pos.y,
-            };
-            self.data_tables.insert(new_pos, old_dt);
+            let new_pos = pos.translate(-shift_table, 0, 1, 1);
+            self.data_tables.shift_insert(index, new_pos, old_dt);
         }
     }
 }
