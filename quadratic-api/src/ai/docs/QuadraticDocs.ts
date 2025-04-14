8fb00baac5a471e0b353a93387799f47e84a5555
Luke <146771258+luke-quadratic@users.noreply.github.com>
2025-04-14 11:33:24 -0600

    smallchange

--- quadratic-api/src/ai/docs/QuadraticDocs.ts
+++ quadratic-api/src/ai/docs/QuadraticDocs.ts
@@ -4,7 +4,7 @@ Quadratic is a modern AI-enabled spreadsheet. Quadratic is purpose built to make
 
 Quadratic combines a familiar spreadsheet and formulas with the power of AI and modern coding languages like Python, SQL, and JavaScript. 
 
-Files can be imported by drag and dropping into the sheet, those supported file types are: csv, excel, parquet. SQL can be used to connect to databases. Once in the sheet, data can be analyzed with coding languages and sped up with AI.
+Files can be imported by drag and dropping into the sheet, those supported file types are: csv, excel, parquet. SQL can be used to connect to databases. Once in the sheet, data can be analyzed with coding languages and AI.
 
 The AI chat can import PDFs and images. To add PDFs and images to the chat, use the paperclip attach button to attach your file from the chat box. You can also paste PDFs and images into the chat box or drag and drop.
 
