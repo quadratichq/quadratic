08333e1f154fa96f1c1f62537c1e53f1aedcb3d2
David DiMaria <info@collectivesessions.com>
2025-04-18 14:25:05 -0600

    Make snow_name an option for JS cells

--- quadratic-client/src/app/actions/dataTableSpec.ts
+++ quadratic-client/src/app/actions/dataTableSpec.ts
@@ -655,7 +655,7 @@ export const dataTableSpec: DataTableSpec = {
   },
   [Action.ToggleTableName]: {
     label: () => 'Show name',
-    isAvailable: () => isCodeCell('Python') || !isSingleCell(),
+    isAvailable: () => isCodeCell('Python') || isCodeCell('Javascript') || !isSingleCell(),
     labelVerbose: 'Show table name',
     checkbox: isTableNameShowing,
     run: toggleTableName,
