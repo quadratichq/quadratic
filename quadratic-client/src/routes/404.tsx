9f45225990b2d3cfb26281a47241524ce4e42f47
Jim Nielsen <jimniels@gmail.com>
2025-04-23 11:44:47 -0600

    Update 404.tsx

--- quadratic-client/src/routes/404.tsx
+++ quadratic-client/src/routes/404.tsx
@@ -1,5 +1,5 @@
 import { EmptyPage } from '@/shared/components/EmptyPage';
-import { SUPPORT_EMAIL } from '@/shared/constants/appConstants';
+import { CONTACT_URL } from '@/shared/constants/urls';
 import { Button } from '@/shared/shadcn/ui/button';
 import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
 import { Link } from 'react-router';
@@ -7,19 +7,19 @@ import { Link } from 'react-router';
 export const Component = () => (
   <EmptyPage
     title="404: not found"
-    description={
-      <>
-        Check the URL and try again. Or, contact us for help at{' '}
-        <a href={SUPPORT_EMAIL} className="underline">
-          {SUPPORT_EMAIL}
-        </a>
-      </>
-    }
+    description="Check the URL and try again."
     Icon={ExclamationTriangleIcon}
     actions={
-      <Button asChild variant="secondary">
-        <Link to="/">Go home</Link>
-      </Button>
+      <div className="flex items-center gap-2">
+        <Button asChild variant="secondary">
+          <Link to={CONTACT_URL} target="_blank">
+            Contact us
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link to="/">Go home</Link>
+        </Button>
+      </div>
     }
   />
 );
