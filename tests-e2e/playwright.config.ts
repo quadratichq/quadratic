eaa021f5bd31b8aba1fe2ba88437234d7ae914ce
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-04-27 15:32:33 +0530

    upload test report

--- tests-e2e/playwright.config.ts
+++ tests-e2e/playwright.config.ts
@@ -18,11 +18,13 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: !!process.env.CI ? "github" : [["list"], ["html"]],
+  reporter: !!process.env.CI
+    ? [["github"], ["list"], ["html"]]
+    : [["list"], ["html"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
