84d46ffd57f94102fc43ae2d227421fea792cbb0
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-04-30 17:01:15 +0530

    try matrix

--- tests-e2e/playwright.config.ts
+++ tests-e2e/playwright.config.ts
@@ -18,11 +18,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 0 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 32 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['github'], ['list'], ['html']] : [['list'], ['html']],
+  reporter: process.env.CI ? [['blob']] : [['list'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
