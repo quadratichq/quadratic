9d1188db5ba2b0c4fc6230370b559d17adcf0249
David DiMaria <info@collectivesessions.com>
2025-05-02 14:52:30 -0600

    Update migrations to us binary

--- quadratic-api/prisma/migrations/20250411190427_team_ssh_keys/migration.sql
+++ quadratic-api/prisma/migrations/20250411190427_team_ssh_keys/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
 ALTER TABLE "Team" 
-ADD COLUMN     "ssh_private_key" TEXT,
-ADD COLUMN     "ssh_public_key" TEXT;
+ADD COLUMN     "ssh_private_key" BYTEA,
+ADD COLUMN     "ssh_public_key" BYTEA;
