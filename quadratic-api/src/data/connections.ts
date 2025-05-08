88497358656535f55ce6f1e582f2a8de6c846603
Jim Nielsen <jimniels@gmail.com>
2025-05-08 11:09:36 -0600

    update demo val

--- quadratic-api/src/data/connections.ts
+++ quadratic-api/src/data/connections.ts
@@ -1,9 +1,9 @@
 import type { Connection } from 'quadratic-shared/typesAndSchemasConnections';
-import { CONNECTION_DEMO_1 } from '../env-vars';
+import { CONNECTION_DEMO } from '../env-vars';
 
 export const demoConnection = {
   // uuid, name, type, typeDetails all go in the env var
-  ...CONNECTION_DEMO_1,
+  ...CONNECTION_DEMO,
 
   // We set these because they don't really matter for the UI
   createdDate: '2022-01-01T00:00:00.000Z',
