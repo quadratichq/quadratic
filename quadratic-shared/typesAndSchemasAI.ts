52d8b0fd325c9807c75d99ca7834c4ec0567f06d
Luke <146771258+luke-quadratic@users.noreply.github.com>
2025-05-15 21:22:25 -0600

    fix

--- quadratic-shared/typesAndSchemasAI.ts
+++ quadratic-shared/typesAndSchemasAI.ts
@@ -23,7 +23,7 @@ const BedrockAnthropicModelSchema = z.enum([
 const BedrockModelSchema = z.enum(['us.deepseek.r1-v1:0']);
 const AnthropicModelSchema = z.enum(['claude-3-7-sonnet-20250219', 'claude-3-5-sonnet-20241022']);
 const OpenAIModelSchema = z.enum([
-  'ft:gpt-4.1-mini-2025-04-14:quadratic::BXVTjXMn',
+  'ft:gpt-4.1-mini-2025-04-14:quadratic::BXe8iYRT',
   'gpt-4.1-mini-2025-04-14',
   'gpt-4.1-2025-04-14',
   'o4-mini-2025-04-16',
@@ -75,7 +75,7 @@ const AnthropicModelKeySchema = z.enum([
 export type AnthropicModelKey = z.infer<typeof AnthropicModelKeySchema>;
 
 const OpenAIModelKeySchema = z.enum([
-  'openai:ft:gpt-4.1-mini-2025-04-14:quadratic::BXVTjXMn',
+  'openai:ft:gpt-4.1-mini-2025-04-14:quadratic::BXe8iYRT',
   'openai:gpt-4.1-mini-2025-04-14',
   'openai:gpt-4.1-2025-04-14',
   'openai:o4-mini-2025-04-16',
