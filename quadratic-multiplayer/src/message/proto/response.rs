e22879a0ff920ce5d7692837ebd89fd7ef18f905
David DiMaria <info@collectivesessions.com>
2025-05-20 13:17:34 -0600

    Resolve conflict in multiplayer

--- quadratic-multiplayer/src/message/proto/response.rs
+++ quadratic-multiplayer/src/message/proto/response.rs
@@ -6,24 +6,6 @@ use quadratic_rust_shared::protobuf::quadratic::transaction::{
     ReceiveTransaction, ReceiveTransactions,
 };
 
-<<<<<<< HEAD
-pub(crate) fn encode_message(message: MessageResponse) -> Result<Vec<u8>> {
-    let mut buffer = Vec::new();
-
-    match message {
-        MessageResponse::BinaryTransaction { .. } => {
-            let transaction = ReceiveTransaction::try_from(message)?;
-
-            transaction
-                .encode(&mut buffer)
-                .map_err(|e| MpError::Serialization(e.to_string()))?;
-        }
-        MessageResponse::BinaryTransactions { transactions } => {
-            let transactions = transactions
-                .into_iter()
-                .map(|transaction| transaction.try_into())
-                .collect::<Result<Vec<ReceiveTransaction>>>()?;
-=======
 /// Encode a single transaction into a protobuf message
 pub(crate) fn encode_transaction(transaction: ReceiveTransaction) -> Result<Vec<u8>> {
     let mut buffer = Vec::new();
@@ -64,70 +46,17 @@ pub(crate) fn encode_message(message: MessageResponse) -> Result<Vec<u8>> {
                 .into_iter()
                 .map(|transaction| transaction.into())
                 .collect::<Vec<ReceiveTransaction>>();
->>>>>>> more-mp-tests-and-logs
 
             let response = ReceiveTransactions {
                 r#type: "BinaryTransactions".to_string(),
                 transactions,
             };
 
-<<<<<<< HEAD
-            response
-                .encode(&mut buffer)
-                .map_err(|e| MpError::Serialization(e.to_string()))?;
-        }
-        _ => {
-            return Err(MpError::ReceivingMessage(
-                "Invalid message response".to_string(),
-            ));
-        }
-    };
-
-    Ok(buffer)
-}
-
-impl TryFrom<MessageResponse> for ReceiveTransaction {
-    type Error = MpError;
-
-    fn try_from(message_response: MessageResponse) -> Result<Self> {
-        let transaction = match message_response {
-            MessageResponse::BinaryTransaction {
-                id,
-                file_id,
-                sequence_num,
-                operations,
-            } => ReceiveTransaction {
-                r#type: "BinaryTransaction".to_string(),
-                id: id.to_string(),
-                file_id: file_id.to_string(),
-                sequence_num,
-                operations,
-            },
-            _ => {
-                return Err(MpError::ReceivingMessage(format!(
-                    "Invalid message response: {:?}",
-                    message_response
-                )));
-            }
-        };
-
-        Ok(transaction)
-    }
-}
-
-impl TryFrom<BinaryTransaction> for ReceiveTransaction {
-    type Error = MpError;
-
-    fn try_from(message_response: BinaryTransaction) -> Result<Self> {
-        message_response.try_into()
-=======
             encode_transactions(response)
         }
-        _ => {
-            Err(MpError::ReceivingMessage(
-                "Cannot encode message into protobuf".into(),
-            ))
-        }
+        _ => Err(MpError::ReceivingMessage(
+            "Cannot encode message into protobuf".into(),
+        )),
     }
 }
 
@@ -140,6 +69,5 @@ impl From<BinaryTransaction> for ReceiveTransaction {
             sequence_num: binary_transaction.sequence_num,
             operations: binary_transaction.operations,
         }
->>>>>>> more-mp-tests-and-logs
     }
 }
