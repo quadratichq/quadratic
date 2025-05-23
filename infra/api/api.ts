d71b8243befb8f885bb22964d70d88b127bc140b
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-05-23 21:34:01 +0530

    fix

--- infra/api/api.ts
+++ infra/api/api.ts
@@ -71,7 +71,7 @@ const targetGroup = new aws.lb.TargetGroup("api-alb-tg", {
     healthyThreshold: 2,
     unhealthyThreshold: 2,
     timeout: 5,
-    interval: 5,
+    interval: 10,
     matcher: "200",
   },
 
