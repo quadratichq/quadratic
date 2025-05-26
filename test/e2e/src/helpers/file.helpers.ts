e9faa22eb1d443d1c117ab9ff369a3b1487993a3
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-05-26 20:24:41 +0530

    fix flaky

--- test/e2e/src/helpers/file.helpers.ts
+++ test/e2e/src/helpers/file.helpers.ts
@@ -29,9 +29,10 @@ export const createFile = async (page: Page, { fileName }: CreateFileOptions) =>
 
   // Navigate back to files
   await page.locator(`nav a >> nth = 0`).click();
-
-  await page.waitForLoadState('domcontentloaded');
   await page.waitForTimeout(10 * 1000);
+  await page.waitForLoadState('domcontentloaded');
+  await quadraticLoading.waitFor({ state: 'hidden', timeout: 2 * 60 * 1000 });
+  await page.waitForLoadState('networkidle');
 };
 
 type CleanUpFilesOptions = {
