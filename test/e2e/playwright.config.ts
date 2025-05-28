8c85416a6d1d374ff94d119f482fd8dd0a0f3441
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-05-29 03:13:21 +0530

    linux platform and useragent

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -44,7 +44,14 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--platform=linux'],
+        },
+        userAgent:
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.7103.25 Safari/537.36',
+      },
     },
 
     // {
