58bb99225110af33ae727999db5a863de710182f
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-07-02 04:28:26 +0530

    fix: revert growable SharedArrayBuffer

--- quadratic-client/src/app/web-workers/pythonWebWorker/worker/pythonCore.ts
+++ quadratic-client/src/app/web-workers/pythonWebWorker/worker/pythonCore.ts
@@ -7,21 +7,21 @@ import { python } from '@/app/web-workers/pythonWebWorker/worker/python';
 export class PythonCore {
   private coreMessagePort?: MessagePort;
 
-  init(messagePort: MessagePort) {
+  init = (messagePort: MessagePort) => {
     this.coreMessagePort = messagePort;
     this.coreMessagePort.onmessage = this.handleMessage;
 
     if (debugFlag('debugWebWorkers')) console.log('[pythonCore] initialized');
-  }
+  };
 
-  private send(message: PythonCoreMessage, transfer?: Transferable[]) {
+  private send = (message: PythonCoreMessage, transfer?: Transferable[]) => {
     if (!this.coreMessagePort) throw new Error('coreMessagePort not initialized');
     if (transfer) {
       this.coreMessagePort.postMessage(message, transfer);
     } else {
       this.coreMessagePort.postMessage(message);
     }
-  }
+  };
 
   private handleMessage = async (e: MessageEvent<CorePythonMessage>) => {
     if (debugFlag('debugWebWorkersMessages')) console.log(`[pythonCore] message: ${e.data.type}`);
@@ -35,7 +35,7 @@ export class PythonCore {
     }
   };
 
-  sendPythonResults(jsCodeResultBuffer: ArrayBuffer) {
+  sendPythonResults = (jsCodeResultBuffer: ArrayBuffer) => {
     this.send(
       {
         type: 'pythonCoreResults',
@@ -43,34 +43,43 @@ export class PythonCore {
       },
       [jsCodeResultBuffer]
     );
-  }
+  };
 
   sendGetCellsA1 = (transactionId: string, a1: string): JsCellsA1Response => {
     try {
-      // This is a growable shared buffer that will be used to communicate with core
-      // The first 4 bytes (int32) are used as flag for atomics wait and notify
-      let sharedBuffer: SharedArrayBuffer | undefined = new SharedArrayBuffer(4, {
-        maxByteLength: 4 * 1024 * 1024 * 1024, // 4GB
-      });
-      let int32View: Int32Array | undefined = new Int32Array(sharedBuffer, 0, 1);
+      // This is a shared buffer that will be used to communicate with core
+      // The first 4 bytes are used to signal the python core that the data is ready
+      // The second 4 bytes are used to signal the length of the data
+      // The third 4 bytes are used to signal the id of the data
+      // Length of the cells string is unknown at this point
+      let sharedBuffer: SharedArrayBuffer | undefined = new SharedArrayBuffer(4 + 4 + 4);
+      let int32View: Int32Array | undefined = new Int32Array(sharedBuffer, 0, 3);
       Atomics.store(int32View, 0, 0);
 
-      this.send({ type: 'pythonCoreGetCellsA1', sharedBuffer, transactionId, a1 });
+      this.send({ type: 'pythonCoreGetCellsA1Length', sharedBuffer, transactionId, a1 });
       Atomics.wait(int32View, 0, 0);
-      int32View = undefined;
+      const byteLength = int32View[1];
+      if (byteLength === 0) return { values: null, error: { core_error: 'Error in get cells a1 length' } };
 
-      const byteLength = sharedBuffer.byteLength;
-      if (byteLength <= 4) {
-        return { values: null, error: { core_error: 'Error in get cells a1 length' } };
-      }
+      const id = int32View[2];
+
+      // New shared buffer, which is sized to hold the cells string
+      sharedBuffer = new SharedArrayBuffer(4 + byteLength);
+      int32View = new Int32Array(sharedBuffer, 0, 1);
+      Atomics.store(int32View, 0, 0);
+
+      this.send({ type: 'pythonCoreGetCellsA1Data', id, sharedBuffer });
+      Atomics.wait(int32View, 0, 0);
+
+      let uint8View: Uint8Array | undefined = new Uint8Array(sharedBuffer, 4, byteLength);
 
       // Copy the data to a non-shared buffer, for decoding
-      let uint8View: Uint8Array | undefined = new Uint8Array(sharedBuffer, 4, byteLength - 4);
       const nonSharedBuffer = new ArrayBuffer(uint8View.byteLength);
       const nonSharedView = new Uint8Array(nonSharedBuffer);
       nonSharedView.set(uint8View);
-      uint8View = undefined;
       sharedBuffer = undefined;
+      int32View = undefined;
+      uint8View = undefined;
 
       const response = fromUint8Array<JsCellsA1Response>(nonSharedView);
       return response;
