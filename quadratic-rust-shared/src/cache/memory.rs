5dba4fd9e068b8d419d7a84336ea4549fb74e98e
David DiMaria <info@collectivesessions.com>
2025-07-01 17:20:33 -0600

    Create cache trait and Implement for in-memory caching

---
+++ quadratic-rust-shared/src/cache/memory.rs
@@ -0,0 +1,151 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::time::{Duration, Instant};
+
+use async_trait::async_trait;
+
+use crate::cache::Cache;
+
+pub struct MemoryCache<K, V> {
+    cache: HashMap<K, (Option<Instant>, V)>,
+}
+
+impl<K, V> MemoryCache<K, V> {
+    pub fn new() -> Self {
+        Self {
+            cache: HashMap::new(),
+        }
+    }
+}
+
+#[async_trait]
+impl<'a, K, V> Cache<'a, K, V> for MemoryCache<K, V>
+where
+    K: Send + Sync + Hash + Eq + Clone,
+    V: Send + Sync + Clone,
+{
+    /// Create a value in the cache
+    ///
+    /// If the key already exists, the value is overwritten and the old value is returned.
+    async fn create(&mut self, key: &K, value: V, duration: Option<Duration>) -> Option<V> {
+        let expires = duration.map(|d| Self::duration_to_expires(d));
+
+        self.cache
+            .insert(key.to_owned(), (expires, value))
+            .map(|(_, v)| v)
+    }
+
+    /// Get a value from the cache
+    ///
+    /// Returns `None` if the key does not exist.
+    async fn get(&self, key: &K) -> Option<&V> {
+        self.cache.get(key).map(|(_, v)| v)
+    }
+
+    /// Get a mutable reference to a value in the cache
+    ///
+    /// Returns `None` if the key does not exist.
+    async fn get_mut(&mut self, key: &K) -> Option<&mut V> {
+        self.cache.get_mut(key).map(|(_, v)| v)
+    }
+
+    /// Get a value from the cache or create it if it doesn't exist
+    ///
+    /// If the key already exists, the value is returned.
+    /// If the key does not exist, the value is created and returned.
+    async fn get_or_create(&mut self, key: &K, value: V, expires: Option<Duration>) -> Option<&V> {
+        if !self.cache.contains_key(key) {
+            self.create(key, value, expires).await;
+        }
+
+        self.cache.get(key).map(|(_, v)| v)
+    }
+
+    /// Delete a value from the cache
+    ///
+    /// Returns `None` if the key does not exist.
+    async fn delete(&mut self, key: &K) -> Option<V> {
+        self.cache.remove(key).map(|(_, v)| v)
+    }
+
+    /// Clear the cache
+    async fn flush(&mut self) {
+        self.cache.clear();
+    }
+
+    /// Check if a value is expired
+    ///
+    /// Returns `None` if the key does not exist or if expiration is not set.
+    async fn is_expired(&self, key: &K) -> Option<bool> {
+        self.cache
+            .get(key)
+            .map(|(expires, _)| expires.map(|e| e < Instant::now()))
+            .flatten()
+    }
+
+    /// Set the expiration time of a value
+    ///
+    /// If the key does not exist, none is returned.
+    async fn set_expires(&mut self, key: &K, duration: Option<Duration>) {
+        let expires = duration.map(|d| Self::duration_to_expires(d));
+
+        self.cache.get_mut(key).map(|(e, _)| *e = expires);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_memory_cache() {
+        let mut cache = MemoryCache::<String, String>::new();
+        let duration = Duration::from_secs(10);
+        let key = "key".to_string();
+        let value = "value".to_string();
+
+        // create and get an entry
+        cache.create(&key, value.clone(), Some(duration)).await;
+        assert_eq!(cache.get(&key).await, Some(&value));
+
+        // get an entry that doesn't exist
+        assert_eq!(cache.get(&"key2".to_string()).await, None);
+
+        // get_or_create an entry that exists
+        assert_eq!(
+            cache
+                .get_or_create(&key, value.clone(), Some(duration))
+                .await,
+            Some(&value)
+        );
+
+        // get_or_create an entry that doesn't exist
+        assert_eq!(
+            cache
+                .get_or_create(&"key2".to_string(), value.clone(), Some(duration))
+                .await,
+            Some(&value)
+        );
+
+        // expiration
+        assert_eq!(cache.is_expired(&key).await, Some(false));
+        cache.set_expires(&key, Some(Duration::from_secs(0))).await;
+        assert_eq!(cache.is_expired(&key).await, Some(true));
+
+        // delete an entry
+        let key_to_delete = "key_to_delete".to_string();
+        cache
+            .create(&key_to_delete, value.clone(), Some(duration))
+            .await;
+        assert_eq!(cache.get(&key_to_delete).await, Some(&value));
+        cache.delete(&key_to_delete).await;
+        assert_eq!(cache.get(&key_to_delete).await, None);
+
+        // flush the cache
+        cache
+            .create(&key_to_delete, value.clone(), Some(duration))
+            .await;
+        cache.flush().await;
+        assert_eq!(cache.get(&key_to_delete).await, None);
+    }
+}
