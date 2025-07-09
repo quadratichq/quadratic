eb80b07aa310bfa1bf9062a4f23a390deb016520
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-07-10 02:36:43 +0530

    fix e2e and remove cancel button

--- quadratic-client/src/app/ui/menus/BottomBar/TopLeftPosition.tsx
+++ quadratic-client/src/app/ui/menus/BottomBar/TopLeftPosition.tsx
@@ -1,7 +1,6 @@
 import { events } from '@/app/events/events';
 import { sheets } from '@/app/grid/controller/Sheets';
 import { pixiApp } from '@/app/gridGL/pixiApp/PixiApp';
-import { WAIT_TO_SNAP_TIME } from '@/app/gridGL/pixiApp/viewport/Viewport';
 import { xyToA1 } from '@/app/quadratic-core/quadratic_core';
 import { useEffect, useState } from 'react';
 
@@ -9,27 +8,21 @@ export const TopLeftPosition = () => {
   const [topLeftCoordinate, SetTopLeftCoordinate] = useState('');
 
   useEffect(() => {
-    let timeoutId: NodeJS.Timeout | undefined;
     const updateTopLeftCoordinate = () => {
-      // need to wait for the animation to complete before querying the viewport location
-      timeoutId = setTimeout(() => {
-        timeoutId = undefined;
-        const topLeft = pixiApp.viewport.getVisibleBounds();
-        const gridHeadings = pixiApp.headings.headingSize;
-        const topLeftCoordinate = sheets.sheet.getColumnRowFromScreen(
-          topLeft.x + gridHeadings.width,
-          topLeft.y + gridHeadings.height
-        );
-        SetTopLeftCoordinate(xyToA1(topLeftCoordinate.column, topLeftCoordinate.row));
-      }, WAIT_TO_SNAP_TIME);
+      const topLeft = pixiApp.viewport.getVisibleBounds();
+      const gridHeadings = pixiApp.headings.headingSize;
+      const topLeftCoordinate = sheets.sheet.getColumnRowFromScreen(
+        topLeft.x + gridHeadings.width,
+        topLeft.y + gridHeadings.height
+      );
+      SetTopLeftCoordinate(xyToA1(topLeftCoordinate.column, topLeftCoordinate.row));
     };
+
     updateTopLeftCoordinate();
-    events.on('viewportChanged', updateTopLeftCoordinate);
+
+    events.on('viewportReadyAfterUpdate', updateTopLeftCoordinate);
     return () => {
-      events.off('viewportChanged', updateTopLeftCoordinate);
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
+      events.off('viewportReadyAfterUpdate', updateTopLeftCoordinate);
     };
   }, []);
 
