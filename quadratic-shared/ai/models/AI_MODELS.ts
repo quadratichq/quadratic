a95aaf2b225c3cad1803dfa104f18f7d282cf181
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-07-13 02:55:28 +0530

    name

--- quadratic-shared/ai/models/AI_MODELS.ts
+++ quadratic-shared/ai/models/AI_MODELS.ts
@@ -443,7 +443,7 @@ export const MODELS_CONFIGURATION: {
   },
   'azure-openai:gpt-4.1-mini': {
     model: 'gpt-4.1-mini',
-    displayName: '4.1 mini',
+    displayName: 'gpt 4.1 mini',
     temperature: 0,
     max_tokens: 32768,
     canStream: true,
