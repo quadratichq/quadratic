bdb857ebd70feb68e68ea2077bee3adcd35e370b
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-07-14 13:15:04 +0530

    click to copy

--- quadratic-client/src/app/ui/menus/AIAnalyst/AIAnalystDebugChatInput.tsx
+++ quadratic-client/src/app/ui/menus/AIAnalyst/AIAnalystDebugChatInput.tsx
@@ -1,6 +1,7 @@
 import { aiAnalystCurrentChatAtom, aiAnalystLoadingAtom } from '@/app/atoms/aiAnalystAtom';
 import { useDebugFlags } from '@/app/debugFlags/useDebugFlags';
 import { Textarea } from '@/shared/shadcn/ui/textarea';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/shared/shadcn/ui/tooltip';
 import { ChatMessagesSchema } from 'quadratic-shared/typesAndSchemasAI';
 import { memo, useCallback, useMemo, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
@@ -11,6 +12,7 @@ export const AIAnalystDebugChatInput = memo(() => {
   const loading = useRecoilValue(aiAnalystLoadingAtom);
   const [currentChat, setCurrentChat] = useRecoilState(aiAnalystCurrentChatAtom);
   const [error, setError] = useState<string | null>(null);
+  const [showCopied, setShowCopied] = useState(false);
 
   const chatString = useMemo(
     () => (currentChat.messages.length > 0 ? JSON.stringify(currentChat.messages) : ''),
@@ -39,22 +41,40 @@ export const AIAnalystDebugChatInput = memo(() => {
     [setCurrentChat]
   );
 
+  const handleCopy = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (!chatString || error !== null) {
+        return;
+      }
+      event.preventDefault();
+      navigator.clipboard.writeText(chatString);
+      setShowCopied(true);
+      setTimeout(() => setShowCopied(false), 1000);
+    },
+    [chatString, error]
+  );
+
   if (!debug || !debugAIAnalystDebugChatInput) {
     return null;
   }
 
   return (
     <div className="mb-2 border-b border-t border-border px-2 pt-2">
-      <Textarea
-        className="sticky top-0.5 z-10 bg-background"
-        autoComplete="off"
-        value={chatString}
-        onChange={handleChange}
-        disabled={loading}
-        placeholder="Chat String"
-        autoHeight={true}
-        maxHeight={'120px'}
-      />
+      <Tooltip open={showCopied}>
+        <TooltipTrigger onClick={handleCopy} asChild>
+          <Textarea
+            className="sticky top-0.5 z-10 bg-background"
+            autoComplete="off"
+            value={chatString}
+            onChange={handleChange}
+            disabled={loading}
+            placeholder="Chat String"
+            autoHeight={true}
+            maxHeight={'120px'}
+          />
+        </TooltipTrigger>
+        <TooltipContent>Copied</TooltipContent>
+      </Tooltip>
 
       {error && <p className="max-h-32 overflow-y-auto text-red-500">{error}</p>}
     </div>
