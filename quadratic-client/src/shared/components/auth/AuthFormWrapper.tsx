f494de32a6aeaa724b85611b2bd92e1b7f3f7bdf
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-08-12 14:18:27 +0530

    anchor tag for logo

--- quadratic-client/src/shared/components/auth/AuthFormWrapper.tsx
+++ quadratic-client/src/shared/components/auth/AuthFormWrapper.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/shared/shadcn/utils';
 import { forwardRef, memo } from 'react';
+import { Link } from 'react-router';
 
 export const AuthFormWrapper = memo(
   forwardRef<HTMLDivElement, React.PropsWithChildren<{ className?: string }>>((props, _ref) => {
@@ -9,7 +10,9 @@ export const AuthFormWrapper = memo(
       >
         <div className="relative flex w-[400px] flex-col overflow-hidden rounded-md bg-white font-sans text-base font-normal leading-4 text-gray-900 antialiased shadow-[0_12px_40px_0_rgba(0,0,0,0.12)]">
           <div className="flex flex-grow flex-col items-center justify-center gap-6 p-10">
-            <img src="/logo192.png" alt="Quadratic Logo" className="h-12 w-12" />
+            <Link to="/" reloadDocument>
+              <img src="/logo192.png" alt="Quadratic Logo" className="h-12 w-12" />
+            </Link>
 
             {props.children}
           </div>
