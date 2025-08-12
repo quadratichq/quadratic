b3210dcb11478d7581a815168a309fef4ca1dfd6
David Kircos <davidkircos@gmail.com>
2025-08-12 13:55:30 -0600

    fix mixpanel import

--- quadratic-api/src/analytics/index.ts
+++ quadratic-api/src/analytics/index.ts
@@ -1,8 +1,10 @@
-import mixpanel from 'mixpanel-browser';
+const Mixpanel = require('mixpanel');
 import { MIXPANEL_TOKEN } from '../env-vars';
 
+let mixpanel = null;
+
 if (MIXPANEL_TOKEN) {
-  mixpanel.init(MIXPANEL_TOKEN);
+  mixpanel = Mixpanel.init(MIXPANEL_TOKEN);
 }
 
 export const trackEvent = (event: string, properties: Record<string, any>) => {
