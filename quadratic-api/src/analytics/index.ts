5e5f89ecc8c08810c451d2d31b8a0d418df2cea7
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-08-13 02:14:57 +0530

    check for null

--- quadratic-api/src/analytics/index.ts
+++ quadratic-api/src/analytics/index.ts
@@ -1,14 +1,13 @@
-const Mixpanel = require('mixpanel');
+import Mixpanel from 'mixpanel';
 import { MIXPANEL_TOKEN } from '../env-vars';
 
 let mixpanel = null;
-
 if (MIXPANEL_TOKEN) {
   mixpanel = Mixpanel.init(MIXPANEL_TOKEN);
 }
 
 export const trackEvent = (event: string, properties: Record<string, any>) => {
-  if (MIXPANEL_TOKEN) {
+  if (MIXPANEL_TOKEN && mixpanel) {
     mixpanel.track(event, properties);
   }
 };
