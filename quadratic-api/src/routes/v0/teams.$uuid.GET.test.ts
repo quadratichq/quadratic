67b7ecc60439aad075ca4535379c680a3eba84c5
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-08-14 02:56:42 +0530

    fix test

--- quadratic-api/src/routes/v0/teams.$uuid.GET.test.ts
+++ quadratic-api/src/routes/v0/teams.$uuid.GET.test.ts
@@ -1,7 +1,36 @@
+import { auth0Mock } from '../../tests/auth0Mock';
+jest.mock('auth0', () =>
+  auth0Mock([
+    {
+      user_id: 'team_1_owner',
+      email: 'team_1_owner@example.com',
+      // picture: null,
+      name: 'Test User 1',
+    },
+    {
+      user_id: 'team_1_editor',
+      email: 'team_1_editor@example.com',
+      // picture: null,
+      name: 'Test User 2',
+    },
+    {
+      user_id: 'team_1_viewer',
+      email: 'team_1_viewer@example.com',
+      // picture: null,
+      name: 'Test User 3',
+    },
+    {
+      user_id: 'user_without_team',
+      email: 'user_without_team@example.com',
+      // picture: null,
+      name: 'Test User 4',
+    },
+  ])
+);
+
 import request from 'supertest';
 import { app } from '../../app';
 import dbClient from '../../dbClient';
-import { auth0Mock } from '../../tests/auth0Mock';
 import { clearDb, createFile, createTeam, createUser } from '../../tests/testDataGenerator';
 
 beforeEach(async () => {
@@ -36,35 +65,6 @@ beforeEach(async () => {
 });
 
 afterEach(clearDb);
-
-jest.mock('auth0', () =>
-  auth0Mock([
-    {
-      user_id: 'team_1_owner',
-      email: 'team_1_owner@example.com',
-      // picture: null,
-      name: 'Test User 1',
-    },
-    {
-      user_id: 'team_1_editor',
-      email: 'team_1_editor@example.com',
-      // picture: null,
-      name: 'Test User 2',
-    },
-    {
-      user_id: 'team_1_viewer',
-      email: 'team_1_viewer@example.com',
-      // picture: null,
-      name: 'Test User 3',
-    },
-    {
-      user_id: 'user_without_team',
-      email: 'user_without_team@example.com',
-      // picture: null,
-      name: 'Test User 4',
-    },
-  ])
-);
 
 describe('GET /v0/teams/:uuid', () => {
   describe('get a team you belong to', () => {
