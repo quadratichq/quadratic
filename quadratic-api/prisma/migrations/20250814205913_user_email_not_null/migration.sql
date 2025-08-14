520a73383c5ab7741ca177aab8d114daa6ecddf6
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-08-15 05:02:40 +0530

    update migration

--- quadratic-api/prisma/migrations/20250814205913_user_email_not_null/migration.sql
+++ quadratic-api/prisma/migrations/20250814205913_user_email_not_null/migration.sql
@@ -4,6 +4,9 @@
   - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
 
 */
+-- First update any NULL emails
+UPDATE "User" SET email = CONCAT('missing_email_-', "auth0_id", '@quadratichq.com') WHERE email IS NULL;
+
 -- AlterTable
 ALTER TABLE "User" ALTER COLUMN "email" SET NOT NULL;
 
