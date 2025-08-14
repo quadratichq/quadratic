bea36310d6fef756cc4c4a904a3489c040b3305e
David Figatner <david@yopeyopey.com>
2025-08-14 11:39:12 -0700

    adding SetCellVAlues tool card

--- quadratic-client/src/app/ai/toolCards/SetCellValues.tsx
+++ quadratic-client/src/app/ai/toolCards/SetCellValues.tsx
@@ -1,8 +1,13 @@
 import { getRowColSentence, ToolCard } from '@/app/ai/toolCards/ToolCard';
+import { aiViewAtom } from '@/app/atoms/gridSettingsAtom';
+import { sheets } from '@/app/grid/controller/Sheets';
+import { rectToA1 } from '@/app/quadratic-core/quadratic_core';
+import { AILightWeight } from '@/app/ui/menus/AIAnalyst/AILightWeight';
 import { TableRowsIcon } from '@/shared/components/Icons';
 import { AITool, aiToolsSpec } from 'quadratic-shared/ai/specs/aiToolsSpec';
 import type { AIToolCall } from 'quadratic-shared/typesAndSchemasAI';
 import { memo, useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 import type { z } from 'zod';
 
 type SetCellValuesResponse = z.infer<(typeof aiToolsSpec)[AITool.SetCellValues]['responseSchema']>;
@@ -10,6 +15,7 @@ type SetCellValuesResponse = z.infer<(typeof aiToolsSpec)[AITool.SetCellValues][
 export const SetCellValues = memo(
   ({ toolCall: { arguments: args, loading }, className }: { toolCall: AIToolCall; className: string }) => {
     const [toolArgs, setToolArgs] = useState<z.SafeParseReturnType<SetCellValuesResponse, SetCellValuesResponse>>();
+    const aiView = useRecoilValue(aiViewAtom);
 
     useEffect(() => {
       if (loading) {
@@ -42,13 +48,33 @@ export const SetCellValues = memo(
     const { top_left_position, cell_values } = toolArgs.data;
     const rows = cell_values.length;
     const cols = cell_values.reduce((max, row) => Math.max(max, row.length), 0);
+    let a1 = 'A1';
+    try {
+      const topLeftSelection = sheets.stringToSelection(top_left_position, sheets.current);
+      const start = topLeftSelection.getCursor();
+      a1 = rectToA1({
+        min: {
+          x: BigInt(start.x),
+          y: BigInt(start.y),
+        },
+        max: {
+          x: BigInt(start.x + rows - 1),
+          y: BigInt(start.y + cols - 1),
+        },
+      });
+    } catch {}
     return (
-      <ToolCard
-        icon={icon}
-        label={label}
-        description={`${getRowColSentence({ rows, cols })} at ${top_left_position}`}
-        className={className}
-      />
+      <>
+        <ToolCard
+          icon={icon}
+          label={label}
+          description={`${getRowColSentence({ rows, cols })} at ${top_left_position}`}
+          className={className}
+        />
+        {aiView && (
+          <AILightWeight height={100} a1={a1} uniqueName={`set-cell-values-${toolArgs.data.top_left_position}`} />
+        )}
+      </>
     );
   }
 );
