1e687b0abd7d2930f2c9057929a2c05ceddbedea
Jim Nielsen <jimniels@gmail.com>
2025-08-25 10:01:07 -0600

    Update feedback.POST.ts

--- quadratic-api/src/routes/v0/feedback.POST.ts
+++ quadratic-api/src/routes/v0/feedback.POST.ts
@@ -48,7 +48,7 @@ async function handler(req: RequestWithUser, res: express.Response) {
 
   // Post to Slack
   // SLACK_FEEDBACK_URL is the Quadratic product feedback slack app webhook URL
-  if (SLACK_FEEDBACK_URL) {
+  if (SLACK_FEEDBACK_URL && feedback.length >= 15) {
     const payload = {
       text: [
         `📣 ${NODE_ENV === 'production' ? '' : '[STAGING]'} New product feedback`,
