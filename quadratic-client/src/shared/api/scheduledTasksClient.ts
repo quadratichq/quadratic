f194fb6285fd698c7151ace894960a8875c2342d
David Figatner <david@yopeyopey.com>
2025-08-28 11:46:42 -0700

    remove console.log

--- quadratic-client/src/shared/api/scheduledTasksClient.ts
+++ quadratic-client/src/shared/api/scheduledTasksClient.ts
@@ -4,7 +4,6 @@ import { ApiSchemas, type ApiTypes } from 'quadratic-shared/typesAndSchemas';
 export const scheduledTasksAPI = {
   /// Creates a new scheduled task
   create: (fileId: string, body: ApiTypes['/v0/files/:uuid/scheduled_task.POST.request']) => {
-    console.log(fileId, body);
     return fetchFromApi(
       `/v0/files/${fileId}/scheduled_task`,
       { method: 'POST', body: JSON.stringify(body) },
