2f023d1040959363fbd668237ff82bde2297eeb5
David Figatner <david@yopeyopey.com>
2025-09-05 10:56:03 -0700

    cleaning up

--- quadratic-client/src/app/gridGL/UI/LineGraphics.ts
+++ quadratic-client/src/app/gridGL/UI/LineGraphics.ts
@@ -10,6 +10,9 @@ interface DrawRectOptions {
   strokeWidth?: number;
   strokeTint?: number;
   strokeAlpha?: number;
+
+  // 1 / viewport.scaled
+  scaled?: number;
 }
 
 interface DrawLineOptions {
@@ -17,16 +20,13 @@ interface DrawLineOptions {
   alpha?: number;
   alignment?: number;
   strokeWidth?: number;
+
+  // 1 / viewport.scaled
+  scaled?: number;
 }
 
 export class LineGraphics extends Container {
   private current = 0;
-  private scaled: boolean;
-
-  constructor(scaled: boolean) {
-    super();
-    this.scaled = scaled;
-  }
 
   private getSprite(): Sprite {
     if (this.current >= this.children.length) {
@@ -51,16 +51,14 @@ export class LineGraphics extends Container {
   }
 
   drawRect(x0: number, y0: number, x1: number, y1: number, options: DrawRectOptions = {}) {
-    const { fill, strokeWidth, alpha, strokeTint, strokeAlpha } = options;
+    const { fill, alpha, strokeTint, strokeAlpha, scaled } = options;
+    let strokeWidth = (options.strokeWidth ?? 1) * (scaled ?? 1);
     const sprite = this.getSprite();
-    if (this.scaled) {
-      strokeWidth = (strokeWidth ?? 1) / pixiApp.viewport.scaled;
-    }
-    this.drawHorizontalLine(x0, x1 - (strokeWidth ?? 1), y0, { tint: strokeTint, alpha: strokeAlpha, strokeWidth });
+    this.drawHorizontalLine(x0, x1 - strokeWidth, y0, { tint: strokeTint, alpha: strokeAlpha, strokeWidth });
     if (strokeWidth !== undefined) {
-      sprite.position.set(x0 + (strokeWidth ?? 1), y0 + (strokeWidth ?? 1));
-      sprite.width = x1 - x0 - (strokeWidth ?? 1) * 2;
-      sprite.height = y1 - y0 - (strokeWidth ?? 1) * 2;
+      sprite.position.set(x0 + strokeWidth, y0 + strokeWidth);
+      sprite.width = x1 - x0 - strokeWidth * 2;
+      sprite.height = y1 - y0 - strokeWidth * 2;
       sprite.tint = fill ?? 0;
       sprite.alpha = alpha ?? 1;
     } else {
@@ -73,19 +71,23 @@ export class LineGraphics extends Container {
   }
 
   drawHorizontalLine(x0: number, x1: number, y: number, options: DrawLineOptions = {}) {
-    const { tint, alpha, alignment, strokeWidth } = options;
+    const { tint, alpha, scaled } = options;
+    let alignment = options.alignment ?? 0.5;
+    let strokeWidth = (options.strokeWidth ?? 1) * (scaled ?? 1);
     const sprite = this.getSprite();
-    sprite.position.set(x0, y - (alignment ?? 0.5) * (strokeWidth ?? 1));
+    sprite.position.set(x0, y - alignment * strokeWidth);
     sprite.width = x1 - x0;
-    sprite.height = strokeWidth ?? 1;
+    sprite.height = strokeWidth;
     sprite.tint = tint ?? 0;
     sprite.alpha = alpha ?? 1;
   }
 
   drawVerticalLine(y0: number, y1: number, x: number, options: DrawLineOptions = {}) {
-    const { tint, alpha, alignment, strokeWidth } = options;
+    const { tint, alpha, scaled } = options;
+    let alignment = options.alignment ?? 0.5;
+    let strokeWidth = (options.strokeWidth ?? 1) * (scaled ?? 1);
     const sprite = this.getSprite();
-    sprite.position.set(x, y0 - (alignment ?? 0.5) * (strokeWidth ?? 1));
+    sprite.position.set(x, y0 - alignment * strokeWidth);
     sprite.width = 1;
     sprite.height = y1 - y0;
     sprite.tint = tint ?? 0;
