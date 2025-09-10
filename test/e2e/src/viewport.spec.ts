7e71101f56f342c2fdf28c6b2f6d74028dcf9f0a
David Figatner <david@yopeyopey.com>
2025-09-10 09:28:36 -0700

    add e2e test

--- test/e2e/src/viewport.spec.ts
+++ test/e2e/src/viewport.spec.ts
@@ -1,7 +1,7 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { logIn } from './helpers/auth.helpers';
-import { uploadFile } from './helpers/file.helpers';
-import { createNewTeamByURL } from './helpers/team.helper';
+import { cleanUpFiles, uploadFile } from './helpers/file.helpers';
+import { changeSheet } from './helpers/sheet.helper';
 
 test('Table floating headers', async ({ page }) => {
   // Constants
@@ -12,16 +12,39 @@ test('Table floating headers', async ({ page }) => {
   await logIn(page, { emailPrefix: `e2e_viewport` });
 
   // // Create a new team
-  const teamName = `Table floating headers in different sheets - ${Date.now()}`;
-  await createNewTeamByURL(page, { teamName });
+  // const teamName = `Table floating headers in different sheets - ${Date.now()}`;
+  // await createNewTeamByURL(page, { teamName });
 
   // Import file
   await uploadFile(page, { fileName, fileType });
+
+  await page.mouse.move(330, 249); // move mouse over viewport
+  await page.mouse.wheel(0, 200); // scroll down so table header hovers
+  await page.waitForTimeout(5 * 1000);
+
+  // await expect(page.locator('#QuadraticCanvasID')).toHaveScreenshot('viewport_headers_1.png', {
+  //   maxDiffPixelRatio: 0.001,
+  // });
+
+  await changeSheet(page, 'Sheet 2');
+  await page.mouse.move(330, 249); // move mouse over viewport
+  await page.mouse.wheel(0, 200); // scroll down so table header hovers
+  await page.waitForTimeout(5 * 1000);
+
+  await expect(page.locator('#QuadraticCanvasID')).toHaveScreenshot('viewport_headers_2.png', {
+    maxDiffPixelRatio: 0.001,
+  });
+
+  await changeSheet(page, 'Sheet 3');
+  await page.waitForTimeout(5 * 1000);
+  await expect(page.locator('#QuadraticCanvasID')).toHaveScreenshot('viewport_headers_3.png', {
+    maxDiffPixelRatio: 0.001,
+  });
 
   //--------------------------------
   // Clean up:
   //--------------------------------
   // Cleanup newly created files
   await page.locator(`nav a svg`).click({ timeout: 60 * 1000 });
-  // await cleanUpFiles(page, { fileName });
+  await cleanUpFiles(page, { fileName });
 });
