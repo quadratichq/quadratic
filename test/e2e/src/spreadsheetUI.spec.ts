ed42883f3a49a12f84894c733dad4fcf018360cb
David Figatner <david@yopeyopey.com>
2025-09-26 05:56:09 -0700

    add e2e test

--- test/e2e/src/spreadsheetUI.spec.ts
+++ test/e2e/src/spreadsheetUI.spec.ts
@@ -963,7 +963,7 @@ test('Resize Column width with Fill', async ({ page }) => {
   await cleanUpFiles(page, { fileName });
 });
 
-test('Right Click Actions', async ({ page }) => {
+test.only('Right Click Actions', async ({ page }) => {
   // Constants
   const fileName = 'RightClickActions';
   const fileType = 'csv';
@@ -1000,6 +1000,12 @@ test('Right Click Actions', async ({ page }) => {
   });
 
   expect(clipboardContainsPng).toBe(true); // Assert that the clipboard contains a PNG image
+
+  // ensure that the menu closes
+  // Take a screenshot to verify the UI state after copy as PNG
+  await expect(page).toHaveScreenshot('right-click-copy-as-png.png', {
+    maxDiffPixels: 1000,
+  });
 
   //--------------------------------
   // Download selection as CSV
