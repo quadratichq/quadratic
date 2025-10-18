08de6160f9ce2f93eef1a3d6b0a3c7a661dfca1a
David Figatner <david@yopeyopey.com>
2025-10-18 11:10:20 -0700

    remove test.only

--- test/e2e/src/auth.spec.ts
+++ test/e2e/src/auth.spec.ts
@@ -1,7 +1,7 @@
-import { expect, test } from '@playwright/test';
-import { logIn, signUp } from './helpers/auth.helpers';
+import { expect, test } from "@playwright/test";
+import { logIn, signUp } from "./helpers/auth.helpers";
 
-test.only('Log In', async ({ page }) => {
+test("Log In", async ({ page }) => {
   //--------------------------------
   // Log In
   //--------------------------------
@@ -12,11 +12,13 @@ test.only('Log In', async ({ page }) => {
   // Assert:
   //--------------------------------
   // brought to home page
-  await expect(page.locator(`:text("Shared with me")`)).toBeVisible({ timeout: 60 * 1000 });
+  await expect(page.locator(`:text("Shared with me")`)).toBeVisible({
+    timeout: 60 * 1000,
+  });
   await expect(page).toHaveURL(/teams/);
 });
 
-test('Log Out', async ({ page }) => {
+test("Log Out", async ({ page }) => {
   //--------------------------------
   // Log Out
   //--------------------------------
@@ -29,20 +31,24 @@ test('Log Out', async ({ page }) => {
   //--------------------------------
   // Click the Account Icon in the bottom left
   await page
-    .locator('button[aria-haspopup="menu"][data-state="closed"]:has(p:has-text("e2e_"))')
+    .locator(
+      'button[aria-haspopup="menu"][data-state="closed"]:has(p:has-text("e2e_"))'
+    )
     .click({ timeout: 60 * 1000 });
 
   // Click the Log out button
-  await page.getByText('logout', { exact: true }).click({ timeout: 60 * 1000 });
+  await page.getByText("logout", { exact: true }).click({ timeout: 60 * 1000 });
 
   //--------------------------------
   // Assert:
   //--------------------------------
   // Assert you're successfully logged out
-  await expect(page.getByText(`Log in to Quadratic`)).toBeVisible({ timeout: 2000 });
+  await expect(page.getByText(`Log in to Quadratic`)).toBeVisible({
+    timeout: 2000,
+  });
 });
 
-test('Sign Up', async ({ page }) => {
+test("Sign Up", async ({ page }) => {
   //--------------------------------
   // Sign Up
   //--------------------------------
@@ -56,15 +62,21 @@ test('Sign Up', async ({ page }) => {
 
   // Log Out
   await page
-    .locator('button[aria-haspopup="menu"][data-state="closed"]:has(p:has-text("e2e_signup_"))')
+    .locator(
+      'button[aria-haspopup="menu"][data-state="closed"]:has(p:has-text("e2e_signup_"))'
+    )
     .click({ timeout: 60 * 1000 });
-  await page.getByText('logout', { exact: true }).click({ timeout: 60 * 1000 });
-  await expect(page.getByText(`Log in to Quadratic`)).toBeVisible({ timeout: 2000 });
+  await page.getByText("logout", { exact: true }).click({ timeout: 60 * 1000 });
+  await expect(page.getByText(`Log in to Quadratic`)).toBeVisible({
+    timeout: 2000,
+  });
 
   // Log In
   await logIn(page, { emailPrefix });
 
   // Assert we can successfully log in
-  await expect(page.locator(`:text("Shared with me")`)).toBeVisible({ timeout: 60 * 1000 });
+  await expect(page.locator(`:text("Shared with me")`)).toBeVisible({
+    timeout: 60 * 1000,
+  });
   await expect(page).toHaveURL(/teams/);
 });
