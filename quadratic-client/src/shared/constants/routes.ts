5231fceeb16f5506843727281a1e2ff9f5cfe907
Jim Nielsen <jimniels@gmail.com>
2025-10-22 16:35:46 -0600

    Update routes.ts

--- quadratic-client/src/shared/constants/routes.ts
+++ quadratic-client/src/shared/constants/routes.ts
@@ -78,7 +78,6 @@ export const ROUTES = {
   LABS: '/labs',
   ONBOARDING_QUESTIONNAIRE: '/onboarding',
   ONBOARDING_VIDEO: '/onboarding-video',
-  UPGRADE: (teamUuid: string, redirectTo: string) => `/upgrade?team=${teamUuid}&redirect=${redirectTo}`,
 
   API: {
     FILE: (uuid: string) => `/api/files/${uuid}`,
