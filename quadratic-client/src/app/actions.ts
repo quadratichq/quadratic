e8dcf8d6349c7348b138feb6eececeeacc91cd76
David Figatner <david@yopeyopey.com>
2025-10-31 08:50:59 -0700

    fix duplicate file

--- quadratic-client/src/app/actions.ts
+++ quadratic-client/src/app/actions.ts
@@ -5,7 +5,7 @@ import { showUpgradeDialog } from '@/shared/atom/showUpgradeDialogAtom';
 import { ROUTES } from '@/shared/constants/routes';
 import type { ApiTypes, FilePermission, TeamPermission } from 'quadratic-shared/typesAndSchemas';
 import { FilePermissionSchema } from 'quadratic-shared/typesAndSchemas';
-import { matchPath, type SubmitFunction } from 'react-router';
+import { type SubmitFunction } from 'react-router';
 import { type SetterOrUpdater } from 'recoil';
 
 const { FILE_EDIT, FILE_DELETE } = FilePermissionSchema.enum;
@@ -85,8 +85,7 @@ export const duplicateFileAction = {
   isAvailable: ({ isAuthenticated, filePermissions }: IsAvailableArgs) =>
     isAuthenticated && filePermissions.includes('FILE_VIEW'),
   async run({ fileUuid, teamUuid }: { fileUuid: string; teamUuid: string }) {
-    const isTeamPrivateFilesRoute = Boolean(matchPath(ROUTES.TEAM_FILES_PRIVATE(teamUuid), window.location.pathname));
-    const { hasReachedLimit } = await apiClient.teams.fileLimit(teamUuid, isTeamPrivateFilesRoute);
+    const { hasReachedLimit } = await apiClient.teams.fileLimit(teamUuid, true);
     if (hasReachedLimit) {
       showUpgradeDialog('fileLimitReached');
       return;
