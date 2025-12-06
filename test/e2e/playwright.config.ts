90f4208e48eccde79e7e5644903f58591aaa9160
David Figatner <david@yopeyopey.com>
2025-12-06 10:10:31 -0800

    shrink the number of tests running at once

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests. */
-  workers: process.env.CI ? 1 : 10,
+  workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
     ? [['blob']]
