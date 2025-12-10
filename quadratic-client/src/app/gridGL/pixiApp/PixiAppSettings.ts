50f0ec90027a8b17cd42c80e69e45a339d61bbc0
David Kircos <davidkircos@gmail.com>
2025-12-10 11:05:15 -0700

    all code

--- quadratic-client/src/app/gridGL/pixiApp/PixiAppSettings.ts
+++ quadratic-client/src/app/gridGL/pixiApp/PixiAppSettings.ts
@@ -259,11 +259,7 @@ export class PixiAppSettings {
 
   // Check if the grid canvas has focus (or user is editing a cell, or context menu is open)
   isGridFocused(): boolean {
-    return (
-      document.activeElement === pixiApp.canvas ||
-      this.input.show ||
-      this.contextMenu.type !== undefined
-    );
+    return document.activeElement === pixiApp.canvas || this.input.show || this.contextMenu.type !== undefined;
   }
 
   setGlobalSnackbar(addGlobalSnackbar: GlobalSnackbar['addGlobalSnackbar'], closeGlobalSnackbar: () => void) {
