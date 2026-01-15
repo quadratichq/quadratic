cd32fcb8529a2e3bee5b11cb2a0f6f6c154f4cda
Jim Nielsen <jimniels@gmail.com>
2026-01-15 16:31:43 -0700

    fix vulnerability

--- quadratic-client/src/dashboard/components/FilesListItemCore.tsx
+++ quadratic-client/src/dashboard/components/FilesListItemCore.tsx
@@ -27,17 +27,13 @@ export function FilesListItemCore({
   actions?: ReactNode;
   children?: ReactNode;
 }) {
-  const __html = nameFilter ? highlightMatchingString(name, nameFilter) : name;
   const isGrid = viewPreferences.layout === Layout.Grid;
 
   return (
     <div className={`flex w-full items-center`}>
       <div className={`flex w-full items-center justify-between gap-3`}>
         <div className={cn(`flex-1 overflow-hidden`, isGrid ? 'flex-col' : 'flex-col gap-0.5')}>
-          <h2
-            className={cn(isGrid ? 'truncate text-sm' : 'text-md flex-1 leading-tight')}
-            dangerouslySetInnerHTML={{ __html }}
-          />
+          <h2 className={cn(isGrid ? 'truncate text-sm' : 'text-md flex-1 leading-tight')}>{name}</h2>
 
           <div className="flex h-5 items-center gap-1">
             {children}
@@ -61,28 +57,4 @@ export function FilesListItemCore({
       {actions && <div className="flex-none">{actions}</div>}
     </div>
   );
-}
-
-function escapeHtml(str: string): string {
-  return str
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#039;');
-}
-
-function highlightMatchingString(str: string, search: string) {
-  // Escape HTML to prevent XSS
-  const escapedStr = escapeHtml(str);
-  const escapedSearch = escapeHtml(search);
-
-  // Escape all regex special characters in the search term
-  const searchWithEscapedRegex = escapedSearch.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-  const regex = new RegExp(searchWithEscapedRegex, 'gi');
-
-  const highlightedString = escapedStr.replace(regex, (match: string) => {
-    return `<b class="bg-yellow-100 dark:bg-yellow-700">${match}</b>`;
-  });
-  return highlightedString;
 }
