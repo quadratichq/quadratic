0803f9c6a24fcd6ea7df0d0f86251460fba0065d
David DiMaria <info@collectivesessions.com>
2026-01-19 11:52:41 -0700

    Moved the files service into api-vpc and set up VPC peering with the shared VPC

--- infra/index.ts
+++ infra/index.ts
@@ -1,6 +1,7 @@
 // This file is the entry point for the infra package.
 
 import { redisHost, redisPort } from "./shared/redis";
+import "./shared/vpc_peering"; // VPC peering between api-vpc and shared VPC
 
 import { apiPublicDns } from "./api/api";
 
--- infra/shared/securityGroups.ts
+++ infra/shared/securityGroups.ts
@@ -3,6 +3,9 @@ import * as aws from "@pulumi/aws";
 import { apiVPC } from "../api/api_network";
 import { cloudControllerEc2SecurityGroup } from "../cloud-controller/cloud_controller_network";
 
+// api-vpc CIDR block (defined in api_network.ts)
+const API_VPC_CIDR = "10.0.0.0/16";
+
 // Create a Security Group for the Files EC2 instance
 export const filesEc2SecurityGroup = new aws.ec2.SecurityGroup("files-sg", {
   vpcId: apiVPC.id,
@@ -57,17 +60,31 @@ export const multiplayerEc2SecurityGroup = new aws.ec2.SecurityGroup(
 );
 
 // Redis Security Group
+// Note: Files service is in api-vpc (10.0.0.0/16) while Redis is in the shared VPC.
+// We use CIDR block for files since security groups can't be referenced across VPCs.
+// VPC peering is configured in vpc_peering.ts to enable connectivity.
 export const redisSecurityGroup = new aws.ec2.SecurityGroup("redis-sg", {
   ingress: [
     {
+      description: "Allow cloud controller to access Redis",
       protocol: "tcp",
       fromPort: 6379,
       toPort: 6379,
-      securityGroups: [
-        cloudControllerEc2SecurityGroup.id,
-        filesEc2SecurityGroup.id,
-        multiplayerEc2SecurityGroup.id,
-      ],
+      securityGroups: [cloudControllerEc2SecurityGroup.id],
+    },
+    {
+      description: "Allow multiplayer to access Redis",
+      protocol: "tcp",
+      fromPort: 6379,
+      toPort: 6379,
+      securityGroups: [multiplayerEc2SecurityGroup.id],
+    },
+    {
+      description: "Allow files service (in api-vpc) to access Redis via VPC peering",
+      protocol: "tcp",
+      fromPort: 6379,
+      toPort: 6379,
+      cidrBlocks: [API_VPC_CIDR],
     },
   ],
 });
