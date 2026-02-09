3cf419bd76f7ee7fd7863f9fb55cd600e252dc29
Jim Nielsen <jimniels@gmail.com>
2026-02-09 09:18:59 -0700

    add event tracking

--- quadratic-client/src/shared/components/ShareDialog.tsx
+++ quadratic-client/src/shared/components/ShareDialog.tsx
@@ -1139,6 +1139,12 @@ function ListItemTeamFile({
       // checked = false means make it a personal file (ownerUserId: loggedInUserId)
       const newOwnerUserId = checked ? null : loggedInUserId;
 
+      if (newOwnerUserId === null) {
+        trackEvent('[FileSharing].moveFileToTeam');
+      } else {
+        trackEvent('[FileSharing].moveFileToPersonal');
+      }
+
       // Submit via fetcher (handles API call)
       const data = getActionFileMove(newOwnerUserId);
       fetcher.submit(data, {
